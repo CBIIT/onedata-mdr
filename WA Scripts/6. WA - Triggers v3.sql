6514a3aa5fd45b02666df1cee0f33a399b699f62
deepalimanathara <deepali.manathara@gmail.com>
2020-06-04 14:37:34 -0400

    Update 6. WA - Triggers v3.sql
    This did not include v2 triggers. Combined and deleted v2 .

--- WA Scripts/6. WA - Triggers v3.sql
+++ WA Scripts/6. WA - Triggers v3.sql
@@ -11,3 +11,113 @@ end if;
 
 END ;
 /
+create or replace trigger TR_NCI_AI_DENORM_INS
+  for  insert or update   on ADMIN_ITEM
+compound trigger
+TYPE r_change_row is RECORD (
+  item_id   ADMIN_ITEM.ITEM_ID%TYPE,
+  VER_NR   ADMIN_ITEM.VER_NR%TYPE,
+  REGSTR_STUS_ID ADMIN_ITEM.REGSTR_STUS_ID%TYPE,
+  ADMIN_STUS_ID  ADMIN_ITEM.ADMIN_STUS_ID%TYPE,
+  CNTXT_ITEM_ID  ADMIN_ITEM.CNTXT_ITEM_ID%TYPE,
+  ORIGIN_ID  ADMIN_ITEM.ORIGIN_ID%TYPE,
+  OLD_REGSTR_STUS_ID ADMIN_ITEM.REGSTR_STUS_ID%TYPE,
+  OLD_ADMIN_STUS_ID  ADMIN_ITEM.ADMIN_STUS_ID%TYPE,
+  OLD_CNTXT_ITEM_ID  ADMIN_ITEM.CNTXT_ITEM_ID%TYPE,
+  OLD_ORIGIN_ID  ADMIN_ITEM.ORIGIN_ID%TYPE
+);
+
+TYPE t_change_row is TABLE of r_change_row
+INDEX by PLS_INTEGER;
+
+t_change t_change_row;
+
+AFTER EACH ROW IS
+BEGIN
+  t_change(t_change.count).ITEM_ID := :new.ITEM_ID;
+  t_change(t_change.count).VER_NR := :new.VER_NR;
+  t_change(t_change.count).REGSTR_STUS_ID := nvl(:new.REGSTR_STUS_ID,9999);
+  t_change(t_change.count).ADMIN_STUS_ID := nvl(:new.ADMIN_STUS_ID,9999);
+  t_change(t_change.count).CNTXT_ITEM_ID := nvl(:new.CNTXT_ITEM_ID,9999);
+  t_change(t_change.count).ORIGIN_ID := nvl(:new.ORIGIN_ID,9999);
+  t_change(t_change.count).OLD_REGSTR_STUS_ID := nvl(:old.REGSTR_STUS_ID,9999);
+  t_change(t_change.count).OLD_ADMIN_STUS_ID := nvl(:old.ADMIN_STUS_ID,9999);
+  t_change(t_change.count).OLD_CNTXT_ITEM_ID := nvl(:old.CNTXT_ITEM_ID,9999);
+  t_change(t_change.count).OLD_ORIGIN_ID := nvl(:old.ORIGIN_ID,9999);
+
+END AFTER EACH ROW;
+
+AFTER STATEMENT IS
+s_REGSTR_STUS_NM_DN  ADMIN_ITEM.REGSTR_STUS_NM_DN%TYPE;
+s_ADMIN_STUS_NM_DN  ADMIN_ITEM.ADMIN_STUS_NM_DN%TYPE;
+s_CNTXT_NM_DN   ADMIN_ITEM.CNTXT_NM_DN%TYPE;
+s_ORIGIN_ID_DN  ADMIN_ITEM.ORIGIN_ID_DN%TYPE;
+
+BEGIN
+for indx in 1..t_change.count
+loop
+
+if ( t_change(indx).REGSTR_STUS_ID <> t_change(indx).OLD_REGSTR_STUS_ID) then
+select STUS_NM into s_REGSTR_STUS_NM_DN from STUS_MSTR where stus_typ_id = 1 and stus_id = t_change(indx).REGSTR_STUS_ID;
+update ADMIN_ITEM set REGSTR_STUS_NM_DN = s_REGSTR_STUS_NM_DN where ITEM_ID = t_change(indx).item_id
+and ver_nr = t_change(indx).ver_nr;
+end if;
+
+if ( t_change(indx).ADMIN_STUS_ID <> t_change(indx).OLD_ADMIN_STUS_ID) then
+select STUS_NM into s_ADMIN_STUS_NM_DN from STUS_MSTR where stus_typ_id = 2 and stus_id = t_change(indx).ADMIN_STUS_ID;
+update ADMIN_ITEM set ADMIN_STUS_NM_DN = s_ADMIN_STUS_NM_DN where ITEM_ID = t_change(indx).item_id
+and ver_nr = t_change(indx).ver_nr;
+end if;
+
+
+if ( t_change(indx).CNTXT_ITEM_ID <> t_change(indx).OLD_CNTXT_ITEM_ID) then
+select ITEM_NM into s_CNTXT_NM_DN from ADMIN_ITEM where ITEM_ID= t_change(indx).CNTXT_ITEM_ID;
+update ADMIN_ITEM set CNTXT_NM_DN = s_CNTXT_NM_DN where ITEM_ID = t_change(indx).item_id
+and ver_nr = t_change(indx).ver_nr;
+end if;
+
+if ( t_change(indx).ORIGIN_ID <> t_change(indx).OLD_ORIGIN_ID) then
+select OBJ_KEY_DESC into s_ORIGIN_ID_DN from OBJ_KEY where obj_key_id = t_change(indx).ORIGIN_ID;
+update ADMIN_ITEM set ORIGIN_ID_DN = s_ORIGIN_ID_DN, ORIGIN = s_ORIGIN_ID_DN where ITEM_ID = t_change(indx).item_id
+and ver_nr = t_change(indx).ver_nr;
+end if;
+
+end loop;
+END AFTER STATEMENT;
+
+END;
+/
+
+
+    CREATE OR REPLACE TRIGGER OD_TR_ALT_KEY  BEFORE INSERT  on NCI_ADMIN_ITEM_REL_ALT_KEY
+  for each row
+         BEGIN    IF (:NEW.NCI_PUB_ID<= 0  or :NEW.NCI_PUB_ID is null)  THEN 
+         select od_seq_ADMIN_ITEM.nextval
+    into :new.NCI_PUB_ID  from  dual ;   
+if (:new.nci_idseq is null) then 
+ :new.nci_idseq := nci_11179.cmr_guid();
+end if;
+END IF; END ;
+/
+
+CREATE or REPLACE TRIGGER OD_TR_QUEST_VV  BEFORE INSERT  on NCI_QUEST_VALID_VALUE
+  for each row
+         BEGIN    IF (:NEW.NCI_PUB_ID<= 0  or :NEW.NCI_PUB_ID is null)  THEN 
+         select od_seq_ADMIN_ITEM.nextval
+    into :new.NCI_PUB_ID  from  dual ;   END IF;
+if (:new.nci_idseq is null) then 
+ :new.nci_idseq := nci_11179.cmr_guid();
+end if;
+ END ;
+
+create or replace trigger TR_AI_EXT_TAB_INS
+  AFTER INSERT
+  on ADMIN_ITEM
+  for each row
+BEGIN
+
+insert into NCI_ADMIN_ITEM_EXT (ITEM_ID, VER_NR)
+select :new.ITEM_ID, :new.VER_NR from dual;
+
+END;
+/
