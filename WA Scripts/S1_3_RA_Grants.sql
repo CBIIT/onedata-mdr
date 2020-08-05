57dc9053e6dca0e79fae22edbf7fe4c75b41da68
asafieva <asafieva@gmail.com>
2020-08-04 23:16:52 -0400

    Renamed WA Scripts to WA_Scripts not to use spaces in directory path

--- WA Scripts/S1_3_RA_Grants.sql
+++
@@ -1,6 +0,0 @@
-grant select on NCI_AI_TYP_VALID_STUS to onedata_wa;
-grant select on NCI_ENTTY to onedata_wa;
-grant select on NCI_ORG to onedata_wa;
-grant select on  NCI_PRSN to onedata_wa;
-grant select on NCI_ENTTY_COMM to onedata_wa;
-grant select on NCI_ENTTY_ADDR to onedata_wa;
