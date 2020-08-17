87356bc0ec45d32aec2122325b48eb07fbd0b3f6
deepalimanathara <deepali.manathara@gmail.com>
2020-08-17 16:59:21 -0400

    Update S3_2-1_SBR_Grants.sql
    Added disable trigger.

--- WA_Scripts/s3/S3_2-1_SBR_Grants.sql
+++ WA_Scripts/s3/S3_2-1_SBR_Grants.sql
@@ -2,3 +2,4 @@ grant select on administered_components to onedata_md;
 grant select on definitions to onedata_md;
 grant select on designations to onedata_md;
 grant select on user_accounts to oneData_md;
+alter trigger AC_BIUD_ROW disable;
