13699a17684a66755dc302f37e8f7fb8a018a515
deepalimanathara <deepali.manathara@gmail.com>
2020-08-20 13:14:52 -0400

    Update S3_6_MD_SP.sql

--- WA_Scripts/s3/S3_6_MD_SP.sql
+++ WA_Scripts/s3/S3_6_MD_SP.sql
@@ -1,4 +1,5 @@
-update OD_MD_OBJTYPPROP set PROP_VAL = CONCAT((Select PROP_VAL from OD_MD_OBJTYPPROP where PROP_ID = 3037), ',customHtml') where PROP_ID = 3037;
+update OD_MD_OBJTYPPROP set PROP_VAL = CONCAT((Select PROP_VAL from OD_MD_OBJTYPPROP where PROP_ID = 3037), ',customHtml') where PROP_ID = 3037 and
+prop_val not like '%customHtml';
 commit;
 
 
