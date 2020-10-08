ab6283767d912acf9f05554342bbe4da406052a6
asafieva <asafieva@gmail.com>
2020-10-08 14:32:41 -0400

    DSRMWS-51 Custom portal ONEDATA_MD DB script

--- od-db-scripts/S00_6_MD_custom_portal.sql
+++ od-db-scripts/S00_6_MD_custom_portal.sql
@@ -1,4 +1,4 @@
 update OD_MD_OBJTYPPROP set PROP_VAL = CONCAT((Select PROP_VAL from OD_MD_OBJTYPPROP where PROP_ID = 3037), ',customHtml') 
-where PROP_ID = 3037 and PROP_VAL not like '%customHtml';
+where PROP_ID = 3037 and PROP_VAL not like '%,customHtml';
 commit;
-select PROP_VAL from OD_MD_OBJTYPPROP where PROP_ID = 3037 and PROP_VAL not like '%,customHtml';
+select PROP_VAL from OD_MD_OBJTYPPROP where PROP_ID = 3037 and PROP_VAL like '%,customHtml';
