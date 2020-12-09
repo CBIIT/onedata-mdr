f87148660c29391d8c5174123ca8d4612b6555a2
asafieva <asafieva@gmail.com>
2020-12-08 21:22:30 -0500

    spAISTUpdPost Sprint 6 Version from DEV

--- od-db-sprints/sprint6/S6_SPAISTUPDPOST.sql
+++ od-db-sprints/sprint6/S6_SPAISTUPDPOST.sql
@@ -1,110 +1,183 @@
-create or replace PROCEDURE spAISTUpdPost
-  (
-    v_data_in IN CLOB,
-    v_data_out OUT CLOB)
+create or replace PROCEDURE            spAISTUpdPost (
+    v_data_in    IN     CLOB,
+    v_data_out      OUT CLOB)
 AS
-  hookInput t_hookInput;
-  hookOutput t_hookOutput := t_hookOutput();
-   actions t_actions := t_actions();
-  action t_actionRowset;
-  row t_row;
-  rows  t_rows;
-    row_ori t_row;
-  action_rows       t_rows := t_rows();
-  action_row		    t_row;
-  rowset            t_rowset;
-  v_add integer :=0;
-  v_action_typ varchar2(30);
-  v_temp  int;
-  v_item_id  number;
-  v_ver_nr  number(4,2);
-  v_nm_id number;
-  v_cntxt_id number;
-  v_cntxt_ver_nr number(4,2);
-  i integer := 0;
-  column  t_column;
-  v_item_nm   varchar2(255);
-  v_item_def varchar2(4000);
-  msg varchar2(4000);
+    hookInput        t_hookInput;
+    hookOutput       t_hookOutput := t_hookOutput ();
+    actions          t_actions := t_actions ();
+    action           t_actionRowset;
+    row              t_row;
+    rows             t_rows;
+    row_ori          t_row;
+    action_rows      t_rows := t_rows ();
+    action_row       t_row;
+    rowset           t_rowset;
+    v_add            INTEGER := 0;
+    v_action_typ     VARCHAR2 (30);
+    v_temp           INT;
+    v_item_id        NUMBER;
+    v_ver_nr         NUMBER (4, 2);
+    v_nm_id          NUMBER;
+    v_cntxt_id       NUMBER;
+    v_cntxt_ver_nr   NUMBER (4, 2);
+    i                INTEGER := 0;
+    column           t_column;
+    v_item_nm        VARCHAR2 (255);
+    v_item_def       VARCHAR2 (4000);
+    msg              VARCHAR2 (4000);
 BEGIN
-  hookinput                    := Ihook.gethookinput (v_data_in);
-  hookoutput.invocationnumber  := hookinput.invocationnumber;
-  hookoutput.originalrowset    := hookinput.originalrowset;
-    rows := t_rows();  
+    hookinput := Ihook.gethookinput (v_data_in);
+    hookoutput.invocationnumber := hookinput.invocationnumber;
+    hookoutput.originalrowset := hookinput.originalrowset;
+    rows := t_rows ();
 
-    row_ori := hookInput.originalRowset.rowset(1);
-    v_item_id := ihook.getColumnValue(row_ori, 'ITEM_ID');
-    v_ver_nr := ihook.getColumnValue(row_ori, 'VER_NR');
+    row_ori := hookInput.originalRowset.rowset (1);
+    v_item_id := ihook.getColumnValue (row_ori, 'ITEM_ID');
+    v_ver_nr := ihook.getColumnValue (row_ori, 'VER_NR');
 
-    if (hookinput.originalRowset.tablename = 'DE') then
+    IF (hookinput.originalRowset.tablename = 'DE')
+    THEN
+        FOR cur
+            IN (SELECT ai.item_id
+                  FROM admin_item ai, de de
+                 WHERE     ai.item_id = de.item_id
+                       AND ai.ver_nr = de.ver_nr
+                       AND de.de_conc_item_id =
+                           ihook.getColumnValue (row_ori, 'DE_CONC_ITEM_ID')
+                       AND de.de_conc_ver_nr =
+                           ihook.getColumnValue (row_ori, 'DE_CONC_VER_NR')
+                       AND de.val_dom_item_id =
+                           ihook.getColumnValue (row_ori, 'VAL_DOM_ITEM_ID')
+                       AND de.val_dom_ver_nr =
+                           ihook.getColumnValue (row_ori, 'VAL_DOM_VER_NR')
+                       AND de.item_id <> v_item_id
+                       AND (ai.cntxt_item_id, ai.cntxt_ver_nr) IN
+                               (SELECT ai1.cntxt_item_id, ai1.cntxt_ver_nr
+                                  FROM admin_item ai1
+                                 WHERE     item_id = v_item_id
+                                       AND ver_nr = v_ver_nr))
+        LOOP
+            raise_application_error (-20000,
+                                     'Duplicate DE found. ' || cur.item_id);
+            RETURN;
+        END LOOP;
+
+        IF (   ihook.getColumnValue (row_ori, 'DE_CONC_ITEM_ID') <>
+               ihook.getColumnOldValue (row_ori, 'DE_CONC_ITEM_ID')
+            OR ihook.getColumnValue (row_ori, 'DE_CONC_VER_NR') <>
+               ihook.getColumnOldValue (row_ori, 'DE_CONC_VER_NR')
+            OR ihook.getColumnValue (row_ori, 'VAL_DOM_ITEM_ID') <>
+               ihook.getColumnOldValue (row_ori, 'VAL_DOM_ITEM_ID')
+            OR ihook.getColumnValue (row_ori, 'VAL_DOM_VER_NR') <>
+               ihook.getColumnOldValue (row_ori, 'VAL_DOM_VER_NR'))
+        THEN
+            SELECT SUBSTR (dec.item_nm || ' ' || vd.item_nm, 1, 255),
+                   SUBSTR (dec.item_desc || '_' || vd.item_desc, 1, 4000)
+              INTO v_item_nm, v_item_def
+              FROM admin_item dec, admin_item vd
+             WHERE     dec.item_id =
+                       ihook.getColumnValue (row_ori, 'DE_CONC_ITEM_ID')
+                   AND dec.ver_nr =
+                       ihook.getColumnValue (row_ori, 'DE_CONC_VER_NR')
+                   AND vd.item_id =
+                       ihook.getColumnValue (row_ori, 'VAL_DOM_ITEM_ID')
+                   AND vd.ver_nr =
+                       ihook.getColumnValue (row_ori, 'VAL_DOM_VER_NR');
+
+            row := t_row ();
+            ihook.setColumnValue (row, 'ITEM_ID', v_item_id);
+            ihook.setColumnValue (row, 'VER_NR', v_ver_nr);
+            ihook.setColumnValue (row, 'ITEM_NM', v_item_nm);
+            ihook.setColumnValue (row, 'ITEM_DESC', v_item_def);
+            rows.EXTEND;
+            rows (rows.LAST) := row;
+            action :=
+                t_actionrowset (rows,
+                                'Administered Item',
+                                2,
+                                0,
+                                'update');
+            actions.EXTEND;
+            actions (actions.LAST) := action;
+        END IF;
+
+        spDEPrefQuestPost2 (row_ori, actions);
+    END IF;
+
+    IF (hookinput.originalRowset.tablename = 'DE_CONC')
+    THEN
+        FOR cur
+            IN (SELECT ai.item_id
+                  FROM admin_item ai, de_conc dec
+                 WHERE     ai.item_id = dec.item_id
+                       AND ai.ver_nr = dec.ver_nr
+                       AND dec.obj_cls_item_id =
+                           ihook.getColumnValue (row_ori, 'OBJ_CLS_ITEM_ID')
+                       AND dec.obj_cls_ver_nr =
+                           ihook.getColumnValue (row_ori, 'OBJ_CLS_VER_NR')
+                       AND dec.prop_item_id =
+                           ihook.getColumnValue (row_ori, 'PROP_ITEM_ID')
+                       AND dec.prop_ver_nr =
+                           ihook.getColumnValue (row_ori, 'PROP_VER_NR')
+                       AND dec.item_id <> v_item_id
+                       AND (ai.cntxt_item_id, ai.cntxt_ver_nr) IN
+                               (SELECT ai1.cntxt_item_id, ai1.cntxt_ver_nr
+                                  FROM admin_item ai1
+                                 WHERE     item_id = v_item_id
+                                       AND ver_nr = v_ver_nr))
+        LOOP
+            raise_application_error (-20000,
+                                     'Duplicate DEC found. ' || cur.item_id);
+            RETURN;
+        END LOOP;
+
+        IF (   ihook.getColumnValue (row_ori, 'OBJ_CLS_ITEM_ID') <>
+               ihook.getColumnOldValue (row_ori, 'OBJ_CLS_ITEM_ID')
+            OR ihook.getColumnValue (row_ori, 'OBJ_CLS_VER_NR') <>
+               ihook.getColumnOldValue (row_ori, 'OBJ_CLS_VER_NR')
+            OR ihook.getColumnValue (row_ori, 'PROP_ITEM_ID') <>
+               ihook.getColumnOldValue (row_ori, 'PROP_ITEM_ID')
+            OR ihook.getColumnValue (row_ori, 'PROP_VER_NR') <>
+               ihook.getColumnOldValue (row_ori, 'PROP_VER_NR'))
+        THEN
+            SELECT SUBSTR (oc.item_nm || ' ' || prop.item_nm, 1, 255),
+                   SUBSTR (oc.item_desc || ' ' || prop.item_desc, 1, 4000)
+              INTO v_item_nm, v_item_def
+              FROM admin_item oc, admin_item prop
+             WHERE     oc.item_id =
+                       ihook.getColumnValue (row_ori, 'OBJ_CLS_ITEM_ID')
+                   AND oc.ver_nr =
+                       ihook.getColumnValue (row_ori, 'OBJ_CLS_VER_NR')
+                   AND prop.item_id =
+                       ihook.getColumnValue (row_ori, 'PROP_ITEM_ID')
+                   AND prop.ver_nr =
+                       ihook.getColumnValue (row_ori, 'PROP_VER_NR');
+
+            row := t_row ();
+            ihook.setColumnValue (row, 'ITEM_ID', v_item_id);
+            ihook.setColumnValue (row, 'VER_NR', v_ver_nr);
+            ihook.setColumnValue (row, 'ITEM_NM', v_item_nm);
+            ihook.setColumnValue (row, 'ITEM_DESC', v_item_def);
+            rows.EXTEND;
+            rows (rows.LAST) := row;
+            action :=
+                t_actionrowset (rows,
+                                'Administered Item',
+                                2,
+                                0,
+                                'update');
+            actions.EXTEND;
+            actions (actions.LAST) := action;
+        END IF;
+    END IF;
 
 
-            for cur in (select ai.item_id from admin_item ai, de de
-            where ai.item_id = de.item_id and ai.ver_nr = de.ver_nr and de.de_conc_item_id = ihook.getColumnValue(row_ori, 'DE_CONC_ITEM_ID') and de.de_conc_ver_nr =  ihook.getColumnValue(row_ori, 'DE_CONC_VER_NR')
-            and de.val_dom_item_id =  ihook.getColumnValue(row_ori, 'VAL_DOM_ITEM_ID') and de.val_dom_ver_nr =  ihook.getColumnValue(row_ori, 'VAL_DOM_VER_NR') and de.item_id <> v_item_id 
-                and (ai.cntxt_item_id, ai.cntxt_ver_nr) in (Select ai1.cntxt_item_id, ai1.cntxt_ver_nr from admin_item ai1 where item_id = v_item_id and ver_nr = v_ver_nr)) loop
-               raise_application_error(-20000, 'Duplicate DE found. ' || cur.item_id);
-               return;
-            end loop;
+    IF actions.COUNT > 0
+    THEN
+        hookoutput.actions := actions;
+    END IF;
 
-       if(ihook.getColumnValue(row_ori, 'DE_CONC_ITEM_ID') <> ihook.getColumnOldValue(row_ori, 'DE_CONC_ITEM_ID') or
-       ihook.getColumnValue(row_ori, 'DE_CONC_VER_NR') <> ihook.getColumnValue(row_ori, 'DE_CONC_VER_NR') or
-       ihook.getColumnValue(row_ori, 'VAL_DOM_ITEM_ID') <> ihook.getColumnValue(row_ori, 'VAL_DOM_ITEM_ID') or
-       ihook.getColumnValue(row_ori, 'VAL_DOM_VER_NR') <> ihook.getColumnValue(row_ori, 'VAL_DOM_VER_NR')) then
-            select substr(dec.item_nm || ' ' || vd.item_nm,1,255) ,substr(dec.item_desc || ' ' || vd.item_desc,  1, 4000) into v_item_nm, v_item_def from admin_item dec, admin_item vd
-            where dec.item_id = ihook.getColumnValue(row_ori, 'DE_CONC_ITEM_ID') and dec.ver_nr =  ihook.getColumnValue(row_ori, 'DE_CONC_VER_NR')
-            and vd.item_id =  ihook.getColumnValue(row_ori, 'VAL_DOM_ITEM_ID') and vd.ver_nr = ihook.getColumnValue(row_ori, 'VAL_DOM_VER_NR');
-        row := t_row ();
-        ihook.setColumnValue(row,'ITEM_ID',v_item_id );  
-        ihook.setColumnValue(row,'VER_NR',v_ver_nr );  
-        ihook.setColumnValue(row,'ITEM_NM',v_item_nm );  
-        ihook.setColumnValue(row,'ITEM_DESC',v_item_def );                                 
-        rows.extend;
-        rows(rows.last) := row;
-        action := t_actionrowset(rows, 'Administered Item', 2,0,'update');
-        actions.extend;
-        actions(actions.last) := action;
-        end if;
-        spDEPrefQuestPost2(row_ori, actions);
-
-    end if;
-
-   if (hookinput.originalRowset.tablename = 'DE_CONC') then
-
-            for cur in (select  ai.item_id from admin_item ai, de_conc dec
-            where ai.item_id = dec.item_id and ai.ver_nr = dec.ver_nr and dec.obj_cls_item_id = ihook.getColumnValue(row_ori, 'OBJ_CLS_ITEM_ID')
-            and dec.obj_cls_ver_nr =  ihook.getColumnValue(row_ori, 'OBJ_CLS_VER_NR')
-            and dec.prop_item_id =  ihook.getColumnValue(row_ori, 'PROP_ITEM_ID') and dec.prop_ver_nr =  ihook.getColumnValue(row_ori, 'PROP_VER_NR') and dec.item_id <> v_item_id 
-            and (ai.cntxt_item_id, ai.cntxt_ver_nr) in (Select ai1.cntxt_item_id, ai1.cntxt_ver_nr from admin_item ai1 where item_id = v_item_id and ver_nr = v_ver_nr)) loop
-               raise_application_error(-20000, 'Duplicate DEC found. ' || cur.item_id);
-               return;
-            end loop;
-
-       if(ihook.getColumnValue(row_ori, 'OBJ_CLS_ITEM_ID') <> ihook.getColumnOldValue(row_ori, 'OBJ_CLS_ITEM_ID') or
-       ihook.getColumnValue(row_ori, 'OBJ_CLS_VER_NR') <> ihook.getColumnValue(row_ori, 'OBJ_CLS_VER_NR') or
-       ihook.getColumnValue(row_ori, 'PROP_ITEM_ID') <> ihook.getColumnValue(row_ori, 'PROP_ITEM_ID') or
-       ihook.getColumnValue(row_ori, 'PROP_VER_NR') <> ihook.getColumnValue(row_ori, 'PROP_VER_NR')) then
-            select substr(oc.item_nm || ' ' || prop.item_nm,1,255) ,substr(oc.item_desc || ' ' || prop.item_desc,  1, 4000) into v_item_nm, v_item_def from admin_item oc, admin_item prop
-            where oc.item_id = ihook.getColumnValue(row_ori, 'OBJ_CLS_ITEM_ID') and oc.ver_nr =  ihook.getColumnValue(row_ori, 'OBJ_CLS_VER_NR')
-            and prop.item_id =  ihook.getColumnValue(row_ori, 'PROP_ITEM_ID') and prop.ver_nr = ihook.getColumnValue(row_ori, 'PROP_VER_NR');
-        row := t_row ();
-        ihook.setColumnValue(row,'ITEM_ID',v_item_id );  
-        ihook.setColumnValue(row,'VER_NR',v_ver_nr );  
-        ihook.setColumnValue(row,'ITEM_NM',v_item_nm );  
-        ihook.setColumnValue(row,'ITEM_DESC',v_item_def );                                 
-        rows.extend;
-        rows(rows.last) := row;
-        action := t_actionrowset(rows, 'Administered Item', 2,0,'update');
-        actions.extend;
-        actions(actions.last) := action;
-         end if;
-
-    end if;
-
-
-   if actions.count > 0 then      
-    hookoutput.actions := actions;
-  end if;
-   -- end if;
-  V_DATA_OUT := IHOOK.GETHOOKOUTPUT (HOOKOUTPUT);
+    -- end if;
+    V_DATA_OUT := IHOOK.GETHOOKOUTPUT (HOOKOUTPUT);
 END;
+/
