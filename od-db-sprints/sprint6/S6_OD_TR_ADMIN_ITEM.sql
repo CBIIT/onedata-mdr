f2d58aab21037dd576ca000537019d9aa2c679b2
asafieva <asafieva@gmail.com>
2020-12-11 23:18:01 -0500

    QA version before Sprint 6 OD_TR_ADMIN_ITEM

---
+++ od-db-sprints/sprint6/S6_OD_TR_ADMIN_ITEM.sql
@@ -0,0 +1,30 @@
+create or replace trigger  OD_TR_ADMIN_ITEM  BEFORE INSERT  on ADMIN_ITEM  for each row
+     BEGIN    IF (:NEW.ITEM_ID = -1  or :NEW.ITEM_ID is null)  THEN select od_seq_ADMIN_ITEM.nextval
+ into :new.ITEM_ID  from  dual ;   END IF;
+if (:new.nci_idseq is null) then 
+ :new.nci_idseq := nci_11179.cmr_guid();
+end if; 
+if (:new.admin_item_typ_id  in (4,3,2,1,54) and :new.ver_nr = 1) then -- draft new
+:new.admin_stus_id := 66;
+:new.regstr_stus_id := 9; -- not sure if rules are changed
+end if;
+if (:new.admin_item_typ_id  in (4,3,2,1,54) and :new.ver_nr > 1) then -- draft mod
+:new.admin_stus_id := 65;
+end if;
+if (:new.admin_item_typ_id  in (5,6,49,53,7)) then -- Released
+:new.admin_stus_id := 75;
+end if;
+if (:new.admin_item_typ_id  in (5,6)) then -- Set the default context
+ :new.cntxt_item_id := 20000000024;
+:new.cntxt_ver_nr := 1;
+end if;
+if (:new.ITEM_LONG_NM is null) then
+:new.ITEM_LONG_NM := :new.ITEM_ID || 'v' || :new.ver_nr;
+end if;
+:new.creat_usr_id_x := :new.creat_usr_id;
+:new.lst_upd_usr_id_x := :new.lst_upd_usr_id;
+if (:new.lst_upd_dt is null) then
+:new.lst_upd_dt := sysdate;
+end if; 
+END ;
+/
