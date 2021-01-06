8a95568cee97276f5c50bd1a4d6b99e57490f998
trushi2 <irina.trush@nih.gov>
2021-01-06 10:47:59 -0500

    Update S7_PACKAGE_BODY_nci_11179.sql

--- od-db-sprints/sprint7/S7_PACKAGE_BODY_nci_11179.sql
+++ od-db-sprints/sprint7/S7_PACKAGE_BODY_nci_11179.sql
@@ -1268,8 +1268,8 @@ begin
             ihook.setColumnValue(row, 'VER_NR', v_version);
             ihook.setColumnValue(row, 'CREAT_USR_ID', hookInput.userId);
             ihook.setColumnValue(row, 'LST_UPD_USR_ID', hookInput.userId);
-            ihook.setColumnValue(row, 'ADMIN_STUS_ID', 65 );
-            ihook.setColumnValue(row, 'REGSTR_STUS_ID','' );
+            ihook.setColumnValue(row, 'ADMIN_STUS_ID', 65 );           
+	    ihook.setColumnValue(row, 'REGSTR_STUS_ID',9 ); –- Application			    
             ihook.setColumnValue(row, 'NCI_IDSEQ', '');
 
             ai_insert_action_rows.extend; ai_insert_action_rows(ai_insert_action_rows.last) := row;
