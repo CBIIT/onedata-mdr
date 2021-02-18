59b2734291a1ec77817f4a65995d13e632ac0acc
trushi2 <irina.trush@nih.gov>
2021-02-18 13:24:11 -0500

    Add files via upload

---
+++ od-db-sprints/sprint8/S8_DROP_PROCEDURES.sql
@@ -0,0 +1,13 @@
+select *from all_source where name='NCI_11179';
+
+DROP PROCEDURE hr.remove_emp; 
+DROP PROCEDURE hr.remove_emp; 
+DROP PROCEDURE hr.remove_emp; 
+DROP PROCEDURE hr.remove_emp; 
+DROP PROCEDURE hr.remove_emp; 
+DROP PROCEDURE onedata_wa.spFormRelatedPost; 
+DROP PROCEDURE onedata_wa.spModUpdPost; 
+DROP PROCEDURE onedata_wa.spInsRefBlobPost; 
+DROP PROCEDURE onedata_wa.spDelRefBlobPost; 
+DROP PROCEDURE onedata_wa.spUpdRefBlobPost; 
+DROP PROCEDURE onedata_wa.spAltDefPost; 
