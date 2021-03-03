070bdb157fb7383100150ff744e3b6a1d246e39d
trushi2 <irina.trush@nih.gov>
2021-03-03 13:54:13 -0500

    Add files via upload

--- od-db-sprints/sprint9/S9_1_WA_RA_DDL_DM.sql
+++ od-db-sprints/sprint9/S9_1_WA_RA_DDL_DM.sql
@@ -1,6 +1,10 @@
 set serveroutput on size 1000000
 SPOOL S9_WA_RA_DDL_DM.log
 
+truncate table nci_admin_item_rel; 
+truncate table DE_CONC; 
+truncate table NCI_QUEST_VV_REP; 
+
 create unique index idx_Admin_item_nci_uni on admin_item (ADMIN_ITEM_TYP_ID, ITEM_LONG_NM, CNTXT_ITEM_ID, CNTXT_VER_NR, VER_NR);
 
 create unique index idxQuestRepUni on nci_quest_vv_rep (QUEST_PUB_ID, QUEST_VER_NR, REP_SEQ);
@@ -13,25 +17,105 @@ create unique index idxNCiAIRelUni on nci_admin_item_rel
 create index idxQuestVV2 on nci_quest_valid_value (Q_PUB_ID, Q_VER_NR, VALUE);
 
 
-  CREATE OR REPLACE  VIEW VW_DE AS
-  SELECT ADMIN_ITEM.ITEM_ID, ADMIN_ITEM.VER_NR, ADMIN_ITEM.ITEM_NM, 
-ADMIN_ITEM.ITEM_LONG_NM, ADMIN_ITEM.ITEM_DESC, ADMIN_ITEM.CNTXT_NM_DN, 
-ADMIN_ITEM.CURRNT_VER_IND, ADMIN_ITEM.REGSTR_STUS_NM_DN, ADMIN_ITEM.ADMIN_STUS_NM_DN,
-ADMIN_ITEM.REGSTR_STUS_ID, ADMIN_ITEM.ADMIN_STUS_ID,
- DE.DE_CONC_VER_NR, DE.DE_CONC_ITEM_ID, DE.VAL_DOM_VER_NR, DE.VAL_DOM_ITEM_ID, DE.REP_CLS_VER_NR, DE.REP_CLS_ITEM_ID, 
-DE.CREAT_USR_ID, DE.LST_UPD_USR_ID, DE.FLD_DELETE, DE.LST_DEL_DT, DE.S2P_TRN_DT, DE.LST_UPD_DT, DE.CREAT_DT,
-DE.PREF_QUEST_TXT, VALUE_DOM.NON_ENUM_VAL_DOM_DESC, VALUE_DOM.DTTYPE_ID, VAL_DOM_MAX_CHAR, VAL_DOM_TYP_ID, VALUE_DOM.UOM_ID,
-VAL_DOM_MIN_CHAR, VAL_DOM_FMT_ID, CHAR_SET_ID, VAL_DOM_HIGH_VAL_NUM, VAL_DOM_LOW_VAL_NUM, FMT_NM, DTTYPE_NM, UOM_NM,
-decode(VALUE_DOM.VAL_DOM_TYP_ID,17 ,'Enumerated', 18,'Non-Enumerated')
-   FROM ADMIN_ITEM, DE, VALUE_DOM, DATA_TYP, FMT, UOM
-       WHERE ADMIN_ITEM.ADMIN_ITEM_TYP_ID = 4
-AND DE.ITEM_ID = ADMIN_ITEM.ITEM_ID
-and DE.VER_NR = ADMIN_ITEM.VER_NR
-and DE.VAL_DOM_ITEM_ID = VALUE_DOM.ITEM_ID
-and DE.VAL_DOM_VER_NR = VALUE_DOM.VER_NR
-and VALUE_DOM.VAL_DOM_FMT_ID = FMT.FMT_ID (+)
-and VALUE_DOM.DTTYPE_ID = DATA_TYP.DTTYPE_ID (+)
-and VALUE_DOM.UOM_ID = UOM.UOM_ID (+);
+CREATE OR REPLACE FORCE VIEW VW_DE
+(
+    ITEM_ID,
+    VER_NR,
+    ITEM_NM,
+    ITEM_LONG_NM,
+    ITEM_DESC,
+    CNTXT_NM_DN,
+    CURRNT_VER_IND,
+    REGSTR_STUS_NM_DN,
+    ADMIN_STUS_NM_DN,
+    REGSTR_STUS_ID,
+    ADMIN_STUS_ID,
+    DE_CONC_VER_NR,
+    DE_CONC_ITEM_ID,
+    VAL_DOM_VER_NR,
+    VAL_DOM_ITEM_ID,
+    REP_CLS_VER_NR,
+    REP_CLS_ITEM_ID,
+    CREAT_USR_ID,
+    LST_UPD_USR_ID,
+    FLD_DELETE,
+    LST_DEL_DT,
+    S2P_TRN_DT,
+    LST_UPD_DT,
+    CREAT_DT,
+    PREF_QUEST_TXT,
+    NON_ENUM_VAL_DOM_DESC,
+    DTTYPE_ID,
+    VAL_DOM_MAX_CHAR,
+    VAL_DOM_TYP_ID,
+    UOM_ID,
+    VAL_DOM_MIN_CHAR,
+    VAL_DOM_FMT_ID,
+    CHAR_SET_ID,
+    VAL_DOM_HIGH_VAL_NUM,
+    VAL_DOM_LOW_VAL_NUM,
+    FMT_NM,
+    DTTYPE_NM,
+    UOM_NM,
+    VAL_DOM_TYP
+)
+BEQUEATH DEFINER
+AS
+    SELECT ADMIN_ITEM.ITEM_ID,
+           ADMIN_ITEM.VER_NR,
+           ADMIN_ITEM.ITEM_NM,
+           ADMIN_ITEM.ITEM_LONG_NM,
+           ADMIN_ITEM.ITEM_DESC,
+           ADMIN_ITEM.CNTXT_NM_DN,
+           ADMIN_ITEM.CURRNT_VER_IND,
+           ADMIN_ITEM.REGSTR_STUS_NM_DN,
+           ADMIN_ITEM.ADMIN_STUS_NM_DN,
+           ADMIN_ITEM.REGSTR_STUS_ID,
+           ADMIN_ITEM.ADMIN_STUS_ID,
+           DE.DE_CONC_VER_NR,
+           DE.DE_CONC_ITEM_ID,
+           DE.VAL_DOM_VER_NR,
+           DE.VAL_DOM_ITEM_ID,
+           DE.REP_CLS_VER_NR,
+           DE.REP_CLS_ITEM_ID,
+           DE.CREAT_USR_ID,
+           DE.LST_UPD_USR_ID,
+           DE.FLD_DELETE,
+           DE.LST_DEL_DT,
+           DE.S2P_TRN_DT,
+           DE.LST_UPD_DT,
+           DE.CREAT_DT,
+           DE.PREF_QUEST_TXT,
+           VALUE_DOM.NON_ENUM_VAL_DOM_DESC,
+           VALUE_DOM.DTTYPE_ID,
+           VAL_DOM_MAX_CHAR,
+           VAL_DOM_TYP_ID,
+           VALUE_DOM.UOM_ID,
+           VAL_DOM_MIN_CHAR,
+           VAL_DOM_FMT_ID,
+           CHAR_SET_ID,
+           VAL_DOM_HIGH_VAL_NUM,
+           VAL_DOM_LOW_VAL_NUM,
+           FMT_NM,
+           DTTYPE_NM,
+           UOM_NM,
+           DECODE (VALUE_DOM.VAL_DOM_TYP_ID,
+                   17, 'Enumerated',
+                   18, 'Non-Enumerated')    VAL_DOM_TYP
+      FROM ADMIN_ITEM,
+           DE,
+           VALUE_DOM,
+           DATA_TYP,
+           FMT,
+           UOM
+     WHERE     ADMIN_ITEM.ADMIN_ITEM_TYP_ID = 4
+           AND DE.ITEM_ID = ADMIN_ITEM.ITEM_ID
+           AND DE.VER_NR = ADMIN_ITEM.VER_NR
+           AND DE.VAL_DOM_ITEM_ID = VALUE_DOM.ITEM_ID
+           AND DE.VAL_DOM_VER_NR = VALUE_DOM.VER_NR
+           AND VALUE_DOM.VAL_DOM_FMT_ID = FMT.FMT_ID(+)
+           AND VALUE_DOM.DTTYPE_ID = DATA_TYP.DTTYPE_ID(+)
+           AND VALUE_DOM.UOM_ID = UOM.UOM_ID(+);
 
 
 create unique index idxDEC on DE_CONC 
@@ -56,7 +140,7 @@ FROM ADMIN_ITEM, NCI_CLSFCTN_SCHM_ITEM csi
        WHERE ADMIN_ITEM_TYP_ID = 51 and ADMIN_ITEM.ITEM_ID = CSI.ITEM_ID and ADMIN_ITEM.VER_NR = CSI.VER_NR;
 
 
-CREATE OR REPLACE FORCE VIEW ONEDATA_WA.VW_NCI_CSI_NODE
+CREATE OR REPLACE FORCE VIEW VW_NCI_CSI_NODE
 (
     ITEM_ID,
     VER_NR,
@@ -128,30 +212,63 @@ AS
            AND CSI.p_item_ver_nr = pcsi.ver_nr(+);
 
 
-  CREATE OR REPLACE VIEW VW_NCI_CSI_DE AS
-  select  aim.c_item_id item_id, aim.c_item_ver_nr ver_nr, aim.p_item_id csi_item_id, aim.p_item_ver_nr csi_ver_nr, 
-  csi.item_nm csi_ITEM_NM, csi.cs_item_id CS_ITEM_ID, csi.CS_ITEM_VER_NR CS_VER_NR, aim.rel_typ_id,
- csi.item_long_nm  csi_item_long_nm, csi.item_desc csi_item_desc, csi.p_item_Id p_CS_ITEM_ID, csi.P_ITEM_VER_NR p_CS_VER_NR,
-aim.CREAT_DT, aim.CREAT_USR_ID, aim.LST_UPD_USR_ID, aim.FLD_DELETE, aim.LST_DEL_DT, aim.S2P_TRN_DT, aim.LST_UPD_DT,
- csi.item_nm || ' ' || cs.ITEM_NM CSI_SEARCH_STR, cs.ITEM_NM  CS_ITEM_NM, cs.ITEM_LONG_NM CS_ITEM_LONG_NM
-from vw_CLSFCTN_SCHM_ITEM csi, nci_admin_item_rel aim, vw_CLSFCTN_SCHM cs 
-where csi.item_id = aim.p_item_id and csi.ver_nr = aim.p_item_ver_nr 
-and aim.rel_typ_id = 65 
-and csi.cs_item_id = cs.item_id and csi.cs_item_ver_nr = cs.ver_nr;
-
-
-
-  CREATE OR REPLACE VIEW VW_NCI_CSI_DE AS
-  select  aim.NCI_PUB_ID, AIM.NCI_VER_NR,  aim.c_item_id item_id, aim.c_item_ver_nr ver_nr, ai.item_id csi_item_id, ai.ver_nr csi_ver_nr, 
-  ai.item_nm csi_ITEM_NM, air.cntxt_cs_item_id CS_ITEM_ID, air.CNTXT_CS_VER_NR CS_VER_NR, aim.rel_typ_id,
- ai.item_long_nm  csi_item_long_nm, ai.item_desc csi_item_desc, air.p_item_Id p_CS_ITEM_ID, air.P_ITEM_VER_NR p_CS_VER_NR,
-aim.CREAT_DT, aim.CREAT_USR_ID, aim.LST_UPD_USR_ID, aim.FLD_DELETE, aim.LST_DEL_DT, aim.S2P_TRN_DT, aim.LST_UPD_DT,
- ai.item_nm || ' ' || cs.ITEM_NM CSI_SEARCH_STR, cs.ITEM_NM  CS_ITEM_NM, cs.ITEM_LONG_NM CS_ITEM_LONG_NM
-from admin_item ai, CLSFCTN_SCHM_ITEM air, nci_admin_item_rel aim, admin_item cs 
-where ai.item_id = air.c_item_id and ai.ver_nr = air.c_item_ver_nr 
-and air.rel_typ_id = 64
-and aim.nci_pub_id = air.nci_pub_id and aim.nci_ver_nr = air.nci_ver_nr
-and air.cntxt_cs_item_id = cs.item_id and air.cntxt_cs_ver_nr = cs.ver_nr and cs.admin_item_typ_id = 9;
+CREATE OR REPLACE FORCE VIEW VW_NCI_CSI_DE
+(
+    ITEM_ID,
+    VER_NR,
+    CSI_ITEM_ID,
+    CSI_VER_NR,
+    CSI_ITEM_NM,
+    CS_ITEM_ID,
+    CS_VER_NR,
+    REL_TYP_ID,
+    CSI_ITEM_LONG_NM,
+    CSI_ITEM_DESC,
+    P_CS_ITEM_ID,
+    P_CS_VER_NR,
+    CREAT_DT,
+    CREAT_USR_ID,
+    LST_UPD_USR_ID,
+    FLD_DELETE,
+    LST_DEL_DT,
+    S2P_TRN_DT,
+    LST_UPD_DT,
+    CSI_SEARCH_STR,
+    CS_ITEM_NM,
+    CS_ITEM_LONG_NM
+)
+BEQUEATH DEFINER
+AS
+    SELECT aim.c_item_id                        item_id,
+           aim.c_item_ver_nr                    ver_nr,
+           aim.p_item_id                        csi_item_id,
+           aim.p_item_ver_nr                    csi_ver_nr,
+           csi.item_nm                          csi_ITEM_NM,
+           csi.cs_item_id                       CS_ITEM_ID,
+           csi.CS_ITEM_VER_NR                   CS_VER_NR,
+           aim.rel_typ_id,
+           csi.item_long_nm                     csi_item_long_nm,
+           csi.item_desc                        csi_item_desc,
+           csi.p_item_Id                        p_CS_ITEM_ID,
+           csi.P_ITEM_VER_NR                    p_CS_VER_NR,
+           aim.CREAT_DT,
+           aim.CREAT_USR_ID,
+           aim.LST_UPD_USR_ID,
+           aim.FLD_DELETE,
+           aim.LST_DEL_DT,
+           aim.S2P_TRN_DT,
+           aim.LST_UPD_DT,
+           csi.item_nm || ' ' || cs.ITEM_NM     CSI_SEARCH_STR,
+           cs.ITEM_NM                           CS_ITEM_NM,
+           cs.ITEM_LONG_NM                      CS_ITEM_LONG_NM
+      FROM vw_CLSFCTN_SCHM_ITEM  csi,
+           nci_admin_item_rel    aim,
+           vw_CLSFCTN_SCHM       cs
+     WHERE     csi.item_id = aim.p_item_id
+           AND csi.ver_nr = aim.p_item_ver_nr
+           AND aim.rel_typ_id = 65
+           AND csi.cs_item_id = cs.item_id
+           AND csi.cs_item_ver_nr = cs.ver_nr;
 
 
 
