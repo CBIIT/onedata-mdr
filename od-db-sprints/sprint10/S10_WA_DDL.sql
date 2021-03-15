d22ea20abfb862bfb0289fcefb98a98cefa06c4b
deepalimanathara <deepali.manathara@gmail.com>
2021-03-15 16:23:56 -0400

    Create S10_WA_DDL.sql

---
+++ od-db-sprints/sprint10/S10_WA_DDL.sql
@@ -0,0 +1,11 @@
+create or replace TRIGGER TR_AI_EXT_TAB_INS
+  AFTER INSERT
+  on ADMIN_ITEM
+  for each row
+BEGIN
+
+if (:new.admin_item_typ_id not in (5,6,7)) then
+insert into NCI_ADMIN_ITEM_EXT (ITEM_ID, VER_NR)
+select :new.ITEM_ID, :new.VER_NR from dual;
+end if;
+END;
