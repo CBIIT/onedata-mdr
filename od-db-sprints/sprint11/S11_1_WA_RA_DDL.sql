d2522fdb47f2acae099d511f53efc05eeb335c30
deepalimanathara <deepali.manathara@gmail.com>
2021-04-19 12:14:42 -0400

    Update S11_1_WA_RA_DDL.sql
    Added VW_NCI_MODULE_DE

--- od-db-sprints/sprint11/S11_1_WA_RA_DDL.sql
+++ od-db-sprints/sprint11/S11_1_WA_RA_DDL.sql
@@ -184,11 +184,11 @@ alter table DATA_TYP ADD (NCI_DFLT_LEGCY_ID number);
 			 
 			 
 
-CREATE UNIQUE INDEX ONEDATA_WA.IDX_NCI_QUEST_VV_IDSEQ ON ONEDATA_WA.NCI_QUEST_VALID_VALUE
+CREATE UNIQUE INDEX IDX_NCI_QUEST_VV_IDSEQ ON NCI_QUEST_VALID_VALUE
 (NCI_IDSEQ);
-CREATE UNIQUE INDEX ONEDATA_WA.IDXQUESTVV ON ONEDATA_WA.NCI_QUEST_VALID_VALUE
+CREATE UNIQUE INDEX IDXQUESTVV ON NCI_QUEST_VALID_VALUE
 (NCI_PUB_ID);
-CREATE INDEX ONEDATA_WA.IDX_NCI_QUEST_VV ON ONEDATA_WA.NCI_QUEST_VALID_VALUE
+CREATE INDEX IDX_NCI_QUEST_VV ON .NCI_QUEST_VALID_VALUE
 (Q_PUB_ID, Q_VER_NR);
 
 			 
@@ -203,3 +203,25 @@ nvl(ADMIN_ITEM.DEF_SRC, 'No Def Source') DEF_SRC, OBJ_KEY.OBJ_KEY_DESC EVS_SRC
        FROM ADMIN_ITEM, CNCPT, OBJ_KEY
        WHERE ADMIN_ITEM_TYP_ID = 49 and ADMIN_ITEM.ITEM_ID = CNCPT.ITEM_ID and ADMIN_ITEM.VER_NR = CNCPT.VER_NR
 	and cncpt.EVS_SRC_ID = OBJ_KEY.OBJ_KEY_ID (+);
+
+			 
+			 
+  CREATE OR REPLACE  VIEW VW_NCI_MODULE_DE AS
+  select frm.item_long_nm frm_item_long_nm, frm.item_nm frm_item_nm, frm.item_id frm_item_id, frm.ver_nr frm_ver_nr, 
+air.p_item_id mod_item_id, air.p_item_ver_nr mod_ver_nr,
+ air.c_item_id de_item_id, air.c_item_ver_nr de_ver_nr, air.disp_ord,
+aim.item_nm MOD_ITEM_NM,
+ aim.item_long_nm  mod_item_long_nm, aim.item_desc mod_item_desc,
+'QUESTION' admin_item_typ_nm, air.nci_pub_id,air.nci_ver_nr, 
+de.CNTXT_NM_DN,
+air.CREAT_DT, air.CREAT_USR_ID, air.LST_UPD_USR_ID, air.FLD_DELETE, air.LST_DEL_DT, air.S2P_TRN_DT, air.LST_UPD_DT, air.item_long_nm QUEST_LONG_TXT, air.item_nm QUEST_TXT
+from  nci_admin_item_rel_alt_key air, admin_item aim,
+admin_item frm, nci_admin_item_rel frm_mod , admin_item de
+where  air.rel_typ_id = 63
+and aim.item_id = air.p_item_id and aim.ver_nr = air.p_item_ver_nr
+and frm.item_id = frm_mod.p_item_id and frm.ver_nr = frm_mod.p_item_ver_nr and aim.item_id = frm_mod.c_item_id and aim.ver_nr = frm_mod.c_item_ver_nr
+and frm_mod.rel_typ_id in (61,62) 
+and frm.admin_item_typ_id in ( 54,55)
+and air.c_item_id = de.item_id
+and air.c_item_ver_nr = de.ver_nr;
+
