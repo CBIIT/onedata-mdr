2850c92d09f39aefb49d9fa85b25e40fdff09836
deepalimanathara <deepali.manathara@gmail.com>
2021-04-21 14:35:46 -0400

    Update S12_2_WA_DDL.sql

--- od-db-sprints/sprint12/S12_2_WA_DDL.sql
+++ od-db-sprints/sprint12/S12_2_WA_DDL.sql
@@ -7,8 +7,6 @@
  CREATE OR REPLACE TRIGGER OD_TR_CDE_CREAT  BEFORE INSERT  on NCI_STG_CDE_CREAT  for each row
      BEGIN    IF (:NEW.STG_AI_ID = -1  or :NEW.STG_AI_ID is null)  THEN select od_seq_CDE_IMPORT.nextval
  into :new.STG_AI_ID  from  dual ;   END IF;
-
+ END;
 /
-
-
 
