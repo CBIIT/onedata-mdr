8d8245c5ce604ee1ad4a8ae0e2afab7b38918256
asafieva <asafieva@gmail.com>
2021-04-26 21:39:44 -0400

    DSRMWS-780 RETIRED WFS IDs corrections 77 - 81 range

--- od-db-sprints/sprint12/S12_WA_TR_AI_WFS.sql
+++ od-db-sprints/sprint12/S12_WA_TR_AI_WFS.sql
@@ -2,10 +2,10 @@ CREATE OR REPLACE TRIGGER ONEDATA_WA.TR_AI_WFS
   BEFORE UPDATE or INSERT ON ONEDATA_WA.ADMIN_ITEM
   FOR EACH ROW
   when ( -- WFS is like retired requires End Date --
-  ((DECODE(new.ADMIN_STUS_ID,old.ADMIN_STUS_ID,0,1)=1 and (new.ADMIN_STUS_ID > 70 and new.ADMIN_STUS_ID < 82)) and --WFS is changed to like Retired
+  ((DECODE(new.ADMIN_STUS_ID,old.ADMIN_STUS_ID,0,1)=1 and (new.ADMIN_STUS_ID > 76 and new.ADMIN_STUS_ID < 82)) and --WFS is changed to like Retired
    (new.UNTL_DT is null)) --new End Date is null error
     or --WFS was like Retired and is not changed, End Date shall not be removed
-  (DECODE(new.ADMIN_STUS_ID,old.ADMIN_STUS_ID,0,1)=0 and (old.ADMIN_STUS_ID is not null and old.ADMIN_STUS_ID > 70 and old.ADMIN_STUS_ID < 82) and 
+  (DECODE(new.ADMIN_STUS_ID,old.ADMIN_STUS_ID,0,1)=0 and (old.ADMIN_STUS_ID is not null and old.ADMIN_STUS_ID > 76 and old.ADMIN_STUS_ID < 82) and 
   (DECODE(new.UNTL_DT, old.UNTL_DT,0,1)=1 and new.UNTL_DT is null) ---changed End Data to n errorull
   ))
 BEGIN
