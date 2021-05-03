5592e45d9e5c7e8905e4051bd655e5cb0967cde7
asafieva <asafieva@gmail.com>
2021-05-03 17:58:43 -0400

    DSRMWS-748 set default Origin_ID 69942 for Concepts in OD_TR_ADMIN_ITEM

---
+++ od-db-sprints/sprint12/S12_OD_TR_ADMIN_ITEM.sql
@@ -0,0 +1,48 @@
+create or replace TRIGGER OD_TR_ADMIN_ITEM  BEFORE INSERT ON ADMIN_ITEM for each row
+BEGIN    IF (:NEW.ITEM_ID = -1  or :NEW.ITEM_ID is null)  THEN select od_seq_ADMIN_ITEM.nextval
+ into :new.ITEM_ID  from  dual ;   END IF;
+if (:new.nci_idseq is null) then 
+ :new.nci_idseq := nci_11179.cmr_guid();
+end if; 
+if (:new.admin_item_typ_id  in (4,3,2,1,54) and :new.ver_nr = 1 and :new.admin_stus_id is null) then -- draft new
+:new.admin_stus_id := 66;
+end if;
+--if (:new.admin_item_typ_id  in (4,3,2) and :new.ver_nr = 1 and :new.regstr_stus_id is null) then -- default new reg status Application
+--:new.regstr_stus_id := 9; -- not sure if rules are changed
+--end if;
+ -- Tracker 806
+if (:new.regstr_stus_id is null) then -- default new reg status Application
+:new.regstr_stus_id := 9; -- not sure if rules are changed
+end if;
+
+if (:new.admin_item_typ_id  in (4,3,2,1) and :new.ver_nr > 1) then -- draft mod
+:new.admin_stus_id := 65;
+end if;
+if (:new.admin_item_typ_id  in (5,6,49,53,7)) then -- Released
+:new.admin_stus_id := 75;
+end if;
+if (:new.admin_item_typ_id  in (49) and :new.cntxt_item_id is null) then -- Set the default context for concept if empty
+ :new.cntxt_item_id := 20000000024;
+ :new.cntxt_ver_nr := 1;
+end if;
+if (:new.admin_item_typ_id  in (49) and :new.ORIGIN_ID is null) then -- Set the default origin for concept if empty
+ :new.ORIGIN_ID := 69942; --NCI Thesaurus DSRMWS-748
+end if;
+if (:new.admin_item_typ_id  in (5,6)) then -- Set the default context
+ :new.cntxt_item_id := 20000000024;
+:new.cntxt_ver_nr := 1;
+end if;
+if (:new.ITEM_LONG_NM is null) then
+if instr(to_char(:new.ver_nr),'.')=0 then
+:new.ITEM_LONG_NM := :new.ITEM_ID || 'v' || :new.ver_nr||'.0';
+else
+:new.ITEM_LONG_NM := :new.ITEM_ID || 'v' || :new.ver_nr;
+end if;
+end if;
+:new.creat_usr_id_x := :new.creat_usr_id;
+:new.lst_upd_usr_id_x := :new.lst_upd_usr_id;
+if (:new.lst_upd_dt is null) then
+:new.lst_upd_dt := sysdate;
+end if; 
+END ;
+/
