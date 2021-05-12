102f681720c5dbcba26d1ed92ebb438a073f598e
deepalimanathara <deepali.manathara@gmail.com>
2021-05-11 21:51:37 -0400

    Update S13_WA_RA_DDL.sql

--- od-db-sprints/sprint13/S13_WA_RA_DDL.sql
+++ od-db-sprints/sprint13/S13_WA_RA_DDL.sql
@@ -2,3 +2,5 @@ alter table 	NCI_STG_CDE_CREAT add (
 VD_CNTXT_ITEM_ID  number, 
 VD_CNTXT_VER_NR number(4,2));
 
+alter table NCI_STG_AI_CNCPT_CREAT add (CTL_VAL_STUS varchar2(100));
+                                                              
