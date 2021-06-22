c280bbf325de9c51765fc5823f1b5857aa0020ca
deepalimanathara <deepali.manathara@gmail.com>
2021-06-22 13:51:16 -0400

    Update S15_1_WA_RA_DDL.sql

--- od-db-sprints/sprint15/S15_1_WA_RA_DDL.sql
+++ od-db-sprints/sprint15/S15_1_WA_RA_DDL.sql
@@ -5,8 +5,11 @@ create or replace TRIGGER TR_DE_RULE
   on DE
   for each row
 BEGIN
-  if (:new.DERV_TYP_ID is not null) then
+  if (:new.DERV_TYP_ID is not null and nvl(:new.DERV_DE_IND,0) = 0) then
     :new.DERV_DE_IND = 1;
+  end if;
+    if (:new.DERV_TYP_ID is null and nvl(:new.DERV_DE_IND,0) = 1) then
+    :new.DERV_DE_IND = 0;
   end if;
 END ;
 /
