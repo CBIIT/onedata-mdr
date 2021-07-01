bdc81988a0baf5c852722a853fde32246b52e152
deepalimanathara <deepali.manathara@gmail.com>
2021-07-01 18:21:05 -0400

    Update S15_1_WA_RA_DDL.sql

--- od-db-sprints/sprint15/S15_1_WA_RA_DDL.sql
+++ od-db-sprints/sprint15/S15_1_WA_RA_DDL.sql
@@ -465,6 +465,7 @@ from NCI_ADMIN_ITEM_REL ak, ADMIN_ITEM ai
 where ak.C_ITEM_ID  = ai.ITEM_ID and ak.C_ITEM_VER_NR  = ai.VER_NR and ai.ADMIN_ITEM_TYP_ID = 4;
 
 
+-- Tracker 1016
 
   CREATE OR REPLACE  VIEW VW_VAL_MEAN AS
   SELECT ADMIN_ITEM.ITEM_ID, ADMIN_ITEM.VER_NR, ADMIN_ITEM.ITEM_NM, ADMIN_ITEM.ITEM_LONG_NM,  ADMIN_ITEM.ITEM_DESC, 
@@ -474,3 +475,18 @@ ADMIN_ITEM.LST_UPD_DT, decode(ext.cncpt_concat, ext.cncpt_concat_nm, null, ext.c
 FROM ADMIN_ITEM, NCI_ADMIN_ITEM_EXT ext
        WHERE ADMIN_ITEM_TYP_ID = 53 and admin_item.item_id = ext.item_id and admin_item.ver_nr = ext.ver_nr;
 
+
+  CREATE OR REPLACE  VIEW VW_NCI_DE_PV AS
+  SELECT PV.PERM_VAL_BEG_DT, PERM_VAL_END_DT, PERM_VAL_NM, PERM_VAL_DESC_TXT, 
+              DE.ITEM_ID  DE_ITEM_ID, 
+		DE.VER_NR DE_VER_NR, VM.ITEM_NM, VM.ITEM_LONG_NM, 
+                VM.ITEM_DESC, NCI_VAL_MEAN_ITEM_ID, NCI_VAL_MEAN_VER_NR,  decode(e.cncpt_concat, e.cncpt_concat_nm, null, e.cncpt_concat)  cncpt_concat, e.CNCPT_CONCAT_NM,
+		PV.CREAT_DT, PV.CREAT_USR_ID, PV.LST_UPD_USR_ID, PV.FLD_DELETE, PV.LST_DEL_DT, PV.S2P_TRN_DT, PV.LST_UPD_DT,
+                PV.PRNT_CNCPT_ITEM_ID, PV.PRNT_CNCPT_VER_NR, VM.ITEM_NM || ' ' || VM.ITEM_DESC  VM_SEARCH_STR
+       FROM  DE, PERM_VAL PV, ADMIN_ITEM VM, NCI_ADMIN_ITEM_EXT e where 
+	PV.VAL_DOM_ITEM_ID = DE.VAL_DOM_ITEM_ID and
+	PV.VAL_DOM_VER_NR = DE.VAL_DOM_VER_NR and
+	PV.NCI_VAL_MEAN_ITEM_ID = vm.ITEM_ID and
+	PV.NCI_VAL_MEAN_VER_NR = vm.VER_NR and
+        VM.ITEM_ID = e.ITEM_ID and
+        VM.VER_NR = e.VER_NR;
