dc62d87951a1af2c443f4daaf907423559450497
asafieva <asafieva@gmail.com>
2021-07-08 18:16:30 -0400

    DSRMWS-1038 Remove Template from AI types

--- od-db-sprints/sprint16/S16_2_WA_DDL.sql
+++ od-db-sprints/sprint16/S16_2_WA_DDL.sql
@@ -1,2 +1,8 @@
-update OBJ_KEY set FLD_DELETE = 1 WHERE OBJ_KEY_DESC ='Template' and OBJ_TYP_ID = 4 and FLD_DELETE <> 1;
-commit;
+create or replace TRIGGER TR_ALT_NMS_POST
+  AFTER INSERT OR UPDATE
+  on ALT_NMS
+  for each row
+BEGIN
+    update ADMIN_ITEM set LST_UPD_DT = sysdate, LST_UPD_USR_ID = :new.LST_UPD_USR_ID where ITEM_ID = :new.item_id and VER_NR = :new.VER_NR;
+END;
+/
---
+++ od-db-sprints/sprint16/S16_3_WA_RA_MISC.sql
@@ -0,0 +1,2 @@
+update OBJ_KEY set FLD_DELETE = 1 WHERE OBJ_KEY_DESC ='Template' and OBJ_TYP_ID = 4 and FLD_DELETE <>1;
+ommit;
