33fcdb89e1160236cf43a6414d99d7005f068575
deepalimanathara <deepali.manathara@gmail.com>
2021-07-12 16:58:46 -0400

    Update S16_2_WA_DDL.sql

--- od-db-sprints/sprint16/S16_2_WA_DDL.sql
+++ od-db-sprints/sprint16/S16_2_WA_DDL.sql
@@ -70,3 +70,35 @@ if (:new.lst_upd_dt is null) then
 end if; 
 END ;
 /
+
+create or replace TRIGGER TR_ALT_DEF_POST
+  AFTER INSERT OR UPDATE
+  on ALT_DEF
+  for each row
+BEGIN
+    update ADMIN_ITEM set LST_UPD_DT = sysdate, LST_UPD_USR_ID = :new.LST_UPD_USR_ID where ITEM_ID = :new.item_id and VER_NR = :new.VER_NR;
+END;
+/
+
+
+create or replace TRIGGER TR_REF_POST
+  AFTER INSERT OR UPDATE
+  on REF
+  for each row
+BEGIN
+    update ADMIN_ITEM set LST_UPD_DT = sysdate, LST_UPD_USR_ID = :new.LST_UPD_USR_ID where ITEM_ID = :new.item_id and VER_NR = :new.VER_NR;
+END;
+/
+
+
+create or replace TRIGGER TR_CSI_DE_POST
+  AFTER INSERT OR UPDATE
+  on 
+NCI_ADMIN_ITEM_REL
+  for each row
+BEGIN
+if (:new.rel_typ_id = 65) then
+    update ADMIN_ITEM set LST_UPD_DT = sysdate, LST_UPD_USR_ID = :new.LST_UPD_USR_ID where ITEM_ID = :new.c_item_id and VER_NR = :new.c_item_VER_NR;
+    end if;
+END;
+/
