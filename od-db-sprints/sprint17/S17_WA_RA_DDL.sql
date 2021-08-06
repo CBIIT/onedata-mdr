a9ed015d62bb5544d859d5c8bf207aba904e5f16
deepalimanathara <deepali.manathara@gmail.com>
2021-08-06 14:33:57 -0400

    Update S17_WA_RA_DDL.sql

--- od-db-sprints/sprint17/S17_WA_RA_DDL.sql
+++ od-db-sprints/sprint17/S17_WA_RA_DDL.sql
@@ -114,7 +114,8 @@ FROM ADMIN_ITEM, NCI_CLSFCTN_SCHM_ITEM csi, vw_clsfctn_schm cs
   SELECT ADMIN_ITEM.ITEM_ID, ADMIN_ITEM.VER_NR, ADMIN_ITEM.ITEM_NM, ADMIN_ITEM.ITEM_LONG_NM,  ADMIN_ITEM.ITEM_DESC,
 ADMIN_ITEM.CNTXT_NM_DN, ADMIN_ITEM.CURRNT_VER_IND, ADMIN_ITEM.REGSTR_STUS_NM_DN, ADMIN_ITEM.ADMIN_STUS_NM_DN, ADMIN_ITEM.CREAT_DT,
 ADMIN_ITEM.CREAT_USR_ID, ADMIN_ITEM.LST_UPD_USR_ID, ADMIN_ITEM.FLD_DELETE, ADMIN_ITEM.LST_DEL_DT, ADMIN_ITEM.S2P_TRN_DT,
-ADMIN_ITEM.LST_UPD_DT, decode(ext.cncpt_concat, ext.cncpt_concat_nm, null, admin_item.item_long_nm, null, ext.cncpt_concat)  cncpt_concat, ext.cncpt_concat_nm, nvl(admin_item.origin_id_dn, origin) ORIGIN_NM
+ADMIN_ITEM.LST_UPD_DT, decode(ext.cncpt_concat, ext.cncpt_concat_nm, null, admin_item.item_long_nm, null, admin_item.item_id, null, ext.cncpt_concat)  cncpt_concat, ext.cncpt_concat_nm, nvl(admin_item.origin_id_dn, origin) ORIGIN_NM
 FROM ADMIN_ITEM, NCI_ADMIN_ITEM_EXT ext
        WHERE ADMIN_ITEM_TYP_ID = 53 and admin_item.item_id = ext.item_id and admin_item.ver_nr = ext.ver_nr;
+
 
