509e9193fe0e9d8d6c386ec16bf13bdc0b33f333
trushi2 <irina.trush@nih.gov>
2021-08-11 12:07:58 -0400

    Add files via upload

---
+++ od-db-sprints/sprint17/SP17_WA_FORM_ELEMENT_AU_TRIGGERS.sql
@@ -0,0 +1,55 @@
+--VV modifyes Question
+CREATE OR REPLACE TRIGGER TRG_NCI_QVV_POST
+  AFTER INSERT OR UPDATE
+  on NCI_QUEST_VALID_VALUE
+  for each row
+
+BEGIN   
+
+                      
+    update NCI_ADMIN_ITEM_REL_ALT_KEY set LST_UPD_DT = :new.LST_UPD_DT, LST_UPD_USR_ID = :new.LST_UPD_USR_ID     
+    where NCI_PUB_ID=:NEW.Q_PUB_ID and NCI_VER_NR= :NEW.Q_VER_NR;
+ 
+           
+END;
+/
+
+--Qustion updates Module
+CREATE OR REPLACE TRIGGER TRG_NCI_QS_MOD_POST
+  AFTER INSERT OR UPDATE
+  on NCI_ADMIN_ITEM_REL_ALT_KEY
+  for each row
+DECLARE
+
+BEGIN   
+                      
+    update NCI_ADMIN_ITEM_REL set LST_UPD_DT = :new.LST_UPD_DT, LST_UPD_USR_ID = :new.LST_UPD_USR_ID     
+    where  C_ITEM_ID=:new.P_ITEM_ID
+           AND C_ITEM_VER_NR=:new.P_ITEM_VER_NR ;   
+
+
+END;
+/
+--MOD or PROTOCOL UPDATING from, MOD updates ADMIN_Item for MODE 
+CREATE OR REPLACE TRIGGER TRG_NCI_MOD_PROT_POST
+  AFTER INSERT OR UPDATE
+  on NCI_ADMIN_ITEM_REL
+  for each row
+BEGIN
+
+IF :new.REL_TYP_ID in (60,61) then
+    update NCI_FORM set LST_UPD_DT = :new.LST_UPD_DT, LST_UPD_USR_ID = :new.LST_UPD_USR_ID where ITEM_ID = :new.P_item_id and VER_NR = :new.P_ITEM_VER_NR;
+end IF;
+IF :new.REL_TYP_ID =61 then
+    update ADMIN_ITEM set LST_UPD_DT = :new.LST_UPD_DT, LST_UPD_USR_ID = :new.LST_UPD_USR_ID where ITEM_ID = :new.P_item_id and VER_NR = :new.P_ITEM_VER_NR;
+end IF;
+END;
+/
+--from updating its ADMIN_ITEM
+CREATE OR REPLACE TRIGGER TRG_NCI_FORM_POST
+  AFTER INSERT or UPDATE
+  on NCI_FORM
+  for each row
+BEGIN
+    update ADMIN_ITEM set LST_UPD_DT = :new.LST_UPD_DT, LST_UPD_USR_ID = :new.LST_UPD_USR_ID where ITEM_ID = :new.item_id and VER_NR = :new.VER_NR;
+END;
