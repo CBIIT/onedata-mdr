9b16f5023b1c6dc59b896ba668239b4c9eb7a03c
deepalimanathara <deepali.manathara@gmail.com>
2021-08-18 13:01:16 -0400

    Update S18_WA_DDL.sql

--- od-db-sprints/sprint18/S18_WA_DDL.sql
+++ od-db-sprints/sprint18/S18_WA_DDL.sql
@@ -8,3 +8,25 @@ if (
 END;
 /
 
+  
+  create or replace TRIGGER TR_NCI_DLOAD_DTL_TS
+  BEFORE  UPDATE
+  on NCI_DLOAD_DTL
+  for each row
+BEGIN
+  :new.LST_UPD_DT := SYSDATE;
+  update NCI_DLOAD_HDR set LST_UPD_DT := sysdate, lst_upd_usr_id :=:new.lst_upd_usr_id;
+  commit;
+END;
+/
+  create or replace TRIGGER TR_NCI_DLOAD_ALS_TS
+  BEFORE  UPDATE
+  on NCI_DLOAD_ALS
+  for each row
+BEGIN
+  :new.LST_UPD_DT := SYSDATE;
+  update NCI_DLOAD_HDR set LST_UPD_DT := sysdate, lst_upd_usr_id :=:new.lst_upd_usr_id;
+  commit;
+END;
+/
+  
