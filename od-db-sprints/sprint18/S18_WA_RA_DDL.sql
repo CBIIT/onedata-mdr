59675c9f8575a4acaf0e7f3dc50729d030bef522
deepalimanathara <deepali.manathara@gmail.com>
2021-08-19 12:41:09 -0400

    Update S18_WA_RA_DDL.sql

--- od-db-sprints/sprint18/S18_WA_RA_DDL.sql
+++ od-db-sprints/sprint18/S18_WA_RA_DDL.sql
@@ -32,3 +32,58 @@ nvl(decode(trim(ADMIN_ITEM.DEF_SRC), 'NCI', '1-NCI', ADMIN_ITEM.DEF_SRC), 'No De
 	and cncpt.EVS_SRC_ID = OBJ_KEY.OBJ_KEY_ID (+) and admin_item.admin_stus_nm_dn = 'RELEASED';
 
 
+
+  CREATE OR REPLACE  VIEW VW_NCI_USED_BY AS
+  select ITEM_ID, VER_NR , CNTXT_ITEM_ID, CNTXT_VER_NR, max(owned_by) OWNED_BY, sysdate CREAT_DT, 'ONEDATA' CREAT_USR_ID, 'ONEDATA' LST_UPD_USR_ID, 0 FLD_DELETE, sysdate LST_DEL_DE, sysdate S2P_TRN_DT, sysdate LST_UPD_DT
+from (
+	SELECT distinct DE.ITEM_ID, DE.VER_NR, de.CNTXT_ITEM_ID, de.CNTXT_VER_NR, 1 OWNED_BY, sysdate CREAT_DT, 'ONEDATA' CREAT_USR_ID,
+	'ONEDATA' LST_UPD_USR_ID, de.FLD_DELETE, de.LST_DEL_DT, sysdate S2P_TRN_DT, sysdate LST_UPD_DT
+       FROM ADMIN_ITEM DE
+       WHERE nvl(de.fld_delete,0) = 0
+      and de.admin_item_typ_id = 4
+	union
+  SELECT distinct DE.ITEM_ID, DE.VER_NR, a.CNTXT_ITEM_ID, a.CNTXT_VER_NR, 0, sysdate CREAT_DT, 'ONEDATA' CREAT_USR_ID, 'ONEDATA' LST_UPD_USR_ID, a.FLD_DELETE, a.LST_DEL_DT, sysdate S2P_TRN_DT, sysdate LST_UPD_DT
+       FROM DE, ALT_NMS a
+       WHERE DE.ITEM_ID = a.ITEM_ID and DE.VER_NR = a.VER_NR
+       and nvl(a.fld_delete,0) = 0
+       and nvl(de.fld_delete,0) = 0
+      and a.CNTXT_ITEM_ID is not null
+union
+  SELECT distinct DE.ITEM_ID, DE.VER_NR, a.NCI_CNTXT_ITEM_ID, a.NCI_CNTXT_VER_NR, 0, sysdate CREAT_DT, 'ONEDATA' CREAT_USR_ID, 'ONEDATA' LST_UPD_USR_ID, a.FLD_DELETE, a.LST_DEL_DT, sysdate S2P_TRN_DT, sysdate LST_UPD_DT
+       FROM DE, REF a
+       WHERE DE.ITEM_ID = a.ITEM_ID and DE.VER_NR = a.VER_NR
+       and nvl(a.fld_delete,0) = 0
+       and nvl(de.fld_delete,0) = 0
+      and a.NCI_CNTXT_ITEM_ID is not null
+union
+  SELECT distinct DE.C_ITEM_ID, DE.C_ITEM_VER_NR, cs.CNTXT_ITEM_ID, cs.CNTXT_VER_NR, 0, sysdate CREAT_DT, 'ONEDATA' CREAT_USR_ID, 'ONEDATA' LST_UPD_USR_ID, cscsi.FLD_DELETE, cscsi.LST_DEL_DT, sysdate S2P_TRN_DT,
+  sysdate LST_UPD_DT
+       FROM NCI_ALT_KEY_ADMIN_ITEM_REL de, NCI_ADMIN_ITEM_REL_ALT_KEY cscsi, admin_item cs
+       WHERE DE.NCI_PUB_ID = cscsi.NCI_PUB_ID and de.NCI_VER_NR = cscsi.NCI_VER_NR
+       and nvl(cscsi.fld_delete,0) = 0
+       and nvl(de.fld_delete,0) = 0
+       and nvl(cs.fld_delete,0) = 0
+       and cscsi.CNTXT_CS_ITEM_ID = cs.item_id and
+       cscsi.CNTXT_CS_VER_NR = cs.ver_nr and
+       cs.admin_item_typ_id = 9       and cs.CNTXT_ITEM_ID is not null
+union
+  SELECT distinct quest.C_ITEM_ID, quest.C_ITEM_VER_NR, ai.CNTXT_ITEM_ID, ai.CNTXT_VER_NR, 0, sysdate CREAT_DT, 'ONEDATA' CREAT_USR_ID, 'ONEDATA' LST_UPD_USR_ID, quest.FLD_DELETE, quest.LST_DEL_DT, sysdate S2P_TRN_DT, sysdate LST_UPD_DT
+       FROM NCI_ADMIN_ITEM_REL_ALT_KEY quest, ADMIN_ITEM ai, NCI_ADMIN_ITEM_REL rel
+       WHERE AI.ITEM_ID = rel.P_ITEM_ID and AI.VER_NR = rel.P_ITEM_VER_NR
+       and nvl(ai.fld_delete,0) = 0
+       and nvl(rel.fld_delete,0) = 0
+       and nvl(quest.fld_delete,0) = 0
+       and rel.C_ITEM_ID = quest.P_ITEM_ID
+       and rel.C_ITEM_VER_NR = quest.P_ITEM_VER_NR
+       and ai.admin_stus_id = 75 -- only for released forms.
+union
+  SELECT distinct quest.C_ITEM_ID, quest.C_ITEM_VER_NR, cs.CNTXT_ITEM_ID, cs.CNTXT_VER_NR, 0, sysdate CREAT_DT, 'ONEDATA' CREAT_USR_ID, 'ONEDATA' LST_UPD_USR_ID, quest.FLD_DELETE, quest.LST_DEL_DT, sysdate S2P_TRN_DT, sysdate LST_UPD_DT
+       FROM NCI_ADMIN_ITEM_REL quest, ADMIN_ITEM cs, NCI_CLSFCTN_SCHM_ITEM csi
+       WHERE cs.ITEM_ID = csi.cs_ITEM_ID and cs.VER_NR = csi.CS_ITEM_VER_NR
+       and nvl(cs.fld_delete,0) = 0
+       and nvl(csi.fld_delete,0) = 0
+       and nvl(quest.fld_delete,0) = 0
+       and csi.ITEM_ID = quest.P_ITEM_ID
+       and cs.VER_NR = quest.P_ITEM_VER_NR
+      )
+      group by ITEM_ID, VER_NR , CNTXT_ITEM_ID, CNTXT_VER_NR;
