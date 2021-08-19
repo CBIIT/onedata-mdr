c4ecf8b72d62b84cabac012fe483350642b838dc
deepalimanathara <deepali.manathara@gmail.com>
2021-08-19 12:21:46 -0400

    Update S18_WA_DDL.sql

--- od-db-sprints/sprint18/S18_WA_DDL.sql
+++ od-db-sprints/sprint18/S18_WA_DDL.sql
@@ -3,29 +3,30 @@ create or replace TRIGGER TR_NCI_DLOAD_HDR_TS
   on NCI_DLOAD_HDR
   for each row
 BEGIN
-if (
+
   :new.LST_UPD_DT := SYSDATE;
 END;
 /
 
   
   create or replace TRIGGER TR_NCI_DLOAD_DTL_TS
-  BEFORE  UPDATE
+  before  INSERT OR UPDATE
   on NCI_DLOAD_DTL
   for each row
 BEGIN
   :new.LST_UPD_DT := SYSDATE;
-  update NCI_DLOAD_HDR set LST_UPD_DT := sysdate, lst_upd_usr_id :=:new.lst_upd_usr_id;
+  update NCI_DLOAD_HDR set LST_UPD_DT = sysdate, lst_upd_usr_id =:new.lst_upd_usr_id where hdr_id = :new.hdr_id;
   commit;
 END;
 /
+
   create or replace TRIGGER TR_NCI_DLOAD_ALS_TS
   BEFORE  UPDATE
   on NCI_DLOAD_ALS
   for each row
 BEGIN
   :new.LST_UPD_DT := SYSDATE;
-  update NCI_DLOAD_HDR set LST_UPD_DT := sysdate, lst_upd_usr_id :=:new.lst_upd_usr_id;
+  update NCI_DLOAD_HDR set LST_UPD_DT = sysdate, lst_upd_usr_id =:new.lst_upd_usr_id where hdr_id = :new.hdr_id;
   commit;
 END;
 /
