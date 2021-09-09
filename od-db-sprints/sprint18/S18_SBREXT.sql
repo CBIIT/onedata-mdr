d8c29bc1dc95ee71e3bc7e00d2106c74b58be7bc
deepalimanathara <deepali.manathara@gmail.com>
2021-09-09 18:32:09 -0400

    Update S18_SBREXT.sql

--- od-db-sprints/sprint18/S18_SBREXT.sql
+++ od-db-sprints/sprint18/S18_SBREXT.sql
@@ -1,8 +1,3 @@
 drop index CSI2_UK;
 drop index CSI_CSI_2_UK;
 
-alter trigger SBREXT.CC_BIU_ROW enable;
-alter trigger SBR.AR_BIU_ROW_ASSIGN_VALUES enable;
-alter trigger SBR.ACCSI_BIU_ROW_ASSIGN_VALUES enable;
-alter trigger sbrext.AAI_BIU_ROW enable;
-
