984cad9bc76d269939a1c9440817154bfc9354d4
deepalimanathara <deepali.manathara@gmail.com>
2021-09-12 14:32:23 -0400

    Create S19_WA_RA_DDL.sql

---
+++ od-db-sprints/sprint19/S19_WA_RA_DDL.sql
@@ -0,0 +1,7 @@
+alter table NCI_STG_AI_CNCPT_CREAT
+ ADD ( CNCPT_INT_1_1 integer, CNCPT_INT_1_2 integer, CNCPT_INT_1_3 integer, CNCPT_INT_1_4 integer, CNCPT_INT_1_5 integer, 
+ CNCPT_INT_1_6 integer, CNCPT_INT_1_7 integer, CNCPT_INT_1_8 integer, CNCPT_INT_1_9 integer, CNCPT_INT_1_10 integer, 
+ CNCPT_INT_2_1 integer, CNCPT_INT_2_2 integer, CNCPT_INT_2_3 integer, CNCPT_INT_2_4 integer, CNCPT_INT_2_5 integer, 
+ CNCPT_INT_2_6 integer, CNCPT_INT_2_7 integer, CNCPT_INT_2_8 integer, CNCPT_INT_2_9 integer, CNCPT_INT_2_10 integer);
+ 
+ 
