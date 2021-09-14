e1e8475738ba3af5f4962db977c5471a7d9a3594
deepalimanathara <deepali.manathara@gmail.com>
2021-09-14 19:58:29 -0400

    Update S19_WA_RA_DDL.sql

--- od-db-sprints/sprint19/S19_WA_RA_DDL.sql
+++ od-db-sprints/sprint19/S19_WA_RA_DDL.sql
@@ -4,4 +4,10 @@ alter table NCI_STG_AI_CNCPT_CREAT
  CNCPT_INT_2_1 integer, CNCPT_INT_2_2 integer, CNCPT_INT_2_3 integer, CNCPT_INT_2_4 integer, CNCPT_INT_2_5 integer, 
  CNCPT_INT_2_6 integer, CNCPT_INT_2_7 integer, CNCPT_INT_2_8 integer, CNCPT_INT_2_9 integer, CNCPT_INT_2_10 integer);
  
+ alter table 
+ NCI_STG_PV_VM_BULK
+ ADD ( CNCPT_INT_1_1 integer, CNCPT_INT_1_2 integer, CNCPT_INT_1_3 integer, CNCPT_INT_1_4 integer, CNCPT_INT_1_5 integer, 
+ CNCPT_INT_1_6 integer, CNCPT_INT_1_7 integer, CNCPT_INT_1_8 integer, CNCPT_INT_1_9 integer, CNCPT_INT_1_10 integer, 
+ STR_DESC_1  varchar2(4000),STR_DESC_2  varchar2(4000),STR_DESC_3  varchar2(4000),STR_DESC_4  varchar2(4000),STR_DESC_5  varchar2(4000),
+     STR_DESC_6  varchar2(4000),STR_DESC_7  varchar2(4000),STR_DESC_8  varchar2(4000),STR_DESC_9  varchar2(4000),STR_DESC_10  varchar2(4000));
  
