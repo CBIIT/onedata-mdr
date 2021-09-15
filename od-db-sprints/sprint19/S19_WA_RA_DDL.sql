23a4df067d18d9648e9875845561b685690ab365
deepalimanathara <deepali.manathara@gmail.com>
2021-09-14 20:42:16 -0400

    Update S19_WA_RA_DDL.sql

--- od-db-sprints/sprint19/S19_WA_RA_DDL.sql
+++ od-db-sprints/sprint19/S19_WA_RA_DDL.sql
@@ -11,3 +11,7 @@ alter table NCI_STG_AI_CNCPT_CREAT
  STR_DESC_1  varchar2(4000),STR_DESC_2  varchar2(4000),STR_DESC_3  varchar2(4000),STR_DESC_4  varchar2(4000),STR_DESC_5  varchar2(4000),
      STR_DESC_6  varchar2(4000),STR_DESC_7  varchar2(4000),STR_DESC_8  varchar2(4000),STR_DESC_9  varchar2(4000),STR_DESC_10  varchar2(4000));
  
+  alter table 
+ NCI_STG_PV_VM_BULK
+ ADD ( blank_1 char(1));
+ 
