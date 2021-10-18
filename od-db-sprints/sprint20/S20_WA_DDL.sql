d395d00dbe84d5ea5a1a91133be66055f23e72b9
deepalimanathara <deepali.manathara@gmail.com>
2021-10-18 01:58:56 -0400

    Update S20_WA_DDL.sql

--- od-db-sprints/sprint20/S20_WA_DDL.sql
+++ od-db-sprints/sprint20/S20_WA_DDL.sql
@@ -41,3 +41,22 @@ END AFTER STATEMENT;
 END;
 
 /
+
+CREATE OR REPLACE TRIGGER TR_AI_EXT_TAB_INS
+  AFTER INSERT
+  on ADMIN_ITEM
+  for each row
+BEGIN
+
+if (:new.admin_item_typ_id not in (5,6,7, 53)) then
+insert into NCI_ADMIN_ITEM_EXT (ITEM_ID, VER_NR)
+select :new.ITEM_ID, :new.VER_NR from dual;
+end if;
+
+if (:new.admin_item_typ_id =52) then
+insert into NCI_MODULE (ITEM_ID, VER_NR)
+select :new.ITEM_ID, :new.VER_NR from dual;
+end if;
+
+END;
+/
