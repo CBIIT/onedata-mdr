8b45a822e1b5acfa37878a08635169062da85934
deepalimanathara <deepali.manathara@gmail.com>
2021-10-19 00:32:06 -0400

    Update S20_WA_DDL.sql

--- od-db-sprints/sprint20/S20_WA_DDL.sql
+++ od-db-sprints/sprint20/S20_WA_DDL.sql
@@ -81,7 +81,7 @@ end IF;
 END;
 /
 
---VV modifyes Form
+--VV modifyes Form. module, question
 CREATE OR REPLACE TRIGGER TRG_NCI_QVV_POST
   AFTER INSERT OR UPDATE OR DELETE
   on NCI_QUEST_VALID_VALUE
@@ -93,10 +93,21 @@ BEGIN
                                 where q.NCI_PUB_ID=:NEW.Q_PUB_ID and q.NCI_VER_NR= :NEW.Q_VER_NR and q.P_ITEM_ID = ak.C_ITEM_ID
                                 and q.P_ITEM_VER_NR = ak.C_ITEM_VER_NR and ak.rel_typ_id = 61);
            
+
+
+    update NCI_ADMIN_ITEM_REL_ALT_KEY set LST_UPD_DT = :new.LST_UPD_DT, LST_UPD_USR_ID = :new.LST_UPD_USR_ID     
+    where  C_ITEM_ID = :new.Q_PUB_ID
+           AND C_ITEM_VER_NR=:new.Q_VER_NR;
+           
+    update NCI_ADMIN_ITEM_REL set LST_UPD_DT = :new.LST_UPD_DT, LST_UPD_USR_ID = :new.LST_UPD_USR_ID     
+    where  (C_ITEM_ID, C_ITEM_VER_NR) in (select p_item_id, p_item_ver_nr from nci_admin_item_rel_alt_key where c_item_id = :new.q_pub_id and 
+                                          c_item_ver_nr = :new.q_ver_nr)
+    and rel_typ_id = 61;
+    
 END;
 /
 
---Qustion updates Form
+--Qustion updates Form and module
 CREATE OR REPLACE TRIGGER TRG_NCI_QS_MOD_POST
   AFTER INSERT OR UPDATE OR DELETE
   on NCI_ADMIN_ITEM_REL_ALT_KEY
@@ -109,6 +120,10 @@ BEGIN
     where  (ITEM_ID, VER_NR) in (select P_ITEM_ID, P_ITEM_VER_NR from NCI_ADMIN_ITEM_REL where C_ITEM_ID=:new.P_ITEM_ID
            AND C_ITEM_VER_NR=:new.P_ITEM_VER_NR  and rel_typ_id = 61);   
 
+
+    update NCI_ADMIN_ITEM_REL set LST_UPD_DT = :new.LST_UPD_DT, LST_UPD_USR_ID = :new.LST_UPD_USR_ID     
+    where  C_ITEM_ID = :new.P_ITEM_ID
+           AND C_ITEM_VER_NR=:new.P_ITEM_VER_NR  and rel_typ_id = 61;   
 END;
 /
 
