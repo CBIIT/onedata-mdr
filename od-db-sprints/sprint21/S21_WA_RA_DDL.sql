339043b79547e5bca81d512687ae6424c7414da0
deepalimanathara <deepali.manathara@gmail.com>
2021-11-03 10:38:13 -0400

    Update S21_WA_RA_DDL.sql

--- od-db-sprints/sprint21/S21_WA_RA_DDL.sql
+++ od-db-sprints/sprint21/S21_WA_RA_DDL.sql
@@ -7,7 +7,27 @@ ADMIN_ITEM.CNTXT_NM_DN, ADMIN_ITEM.CURRNT_VER_IND, ADMIN_ITEM.REGSTR_STUS_NM_DN,
 ADMIN_ITEM.CREAT_USR_ID, ADMIN_ITEM.LST_UPD_USR_ID, ADMIN_ITEM.FLD_DELETE, ADMIN_ITEM.LST_DEL_DT, ADMIN_ITEM.S2P_TRN_DT,
 ADMIN_ITEM.LST_UPD_DT, 
 decode(ext.cncpt_concat, ext.cncpt_concat_nm, null, admin_item.item_long_nm, null, admin_item.item_id, null, ext.cncpt_concat)  cncpt_concat,
-ext.cncpt_concat_nm, nvl(admin_item.origin_id_dn, origin) ORIGIN_NM, ok.nm_desc ALT_NM
-FROM ADMIN_ITEM, NCI_ADMIN_ITEM_EXT ext, (select item_id, ver_nr, NM_DESC from ALT_NMS a, OBJ_KEY o where a.nm_typ_id = o.obj_key_id and o.obj_typ_id = 11 and o.obj_key_desc = 'Biomarker Synonym') ok
-       WHERE ADMIN_ITEM_TYP_ID = 53 and admin_item.item_id = ext.item_id and admin_item.ver_nr = ext.ver_nr
-       and admin_item.item_id = ok.item_id (+) and admin_item.ver_nr = ok.ver_nr (+);
+ext.cncpt_concat_nm, nvl(admin_item.origin_id_dn, origin) ORIGIN_NM
+FROM ADMIN_ITEM, NCI_ADMIN_ITEM_EXT ext
+       WHERE ADMIN_ITEM_TYP_ID = 53 and admin_item.item_id = ext.item_id and admin_item.ver_nr = ext.ver_nr;
+       
+       
+  CREATE OR REPLACE VIEW VW_NCI_MODULE_DE AS
+  select frm.item_long_nm frm_item_long_nm, frm.item_nm frm_item_nm, frm.item_id frm_item_id, frm.ver_nr frm_ver_nr,  frm.cntxt_nm_dn FRM_CNTXT_NM_DN,
+  frm.CNTXT_ITEM_ID FRM_CNTXT_ITEM_ID, frm.CNTXT_VER_NR FRM_CNTXT_VER_NR,
+air.p_item_id mod_item_id, air.p_item_ver_nr mod_ver_nr,
+ air.c_item_id de_item_id, air.c_item_ver_nr de_ver_nr, air.disp_ord quest_disp_ord,frm_mod.disp_ord mod_disp_ord,
+aim.item_nm MOD_ITEM_NM, air.instr,
+ aim.item_long_nm  mod_item_long_nm, aim.item_desc mod_item_desc,
+'QUESTION' admin_item_typ_nm, air.nci_pub_id,air.nci_ver_nr,
+de.CNTXT_NM_DN, de.item_nm DE_ITEM_NM, de.cntxt_item_id DE_CNTXT_ITEM_ID, de.cntxt_VER_NR DE_CNTXT_VER_NR, de.ADMIN_STUS_NM_DN DE_ADMIN_STUS_NM_DN, de.REGSTR_STUS_NM_DN DE_REGSTR_STUS_NM_DN,
+air.CREAT_DT, air.CREAT_USR_ID, air.LST_UPD_USR_ID, air.FLD_DELETE, air.LST_DEL_DT, air.S2P_TRN_DT, air.LST_UPD_DT, air.item_long_nm QUEST_LONG_TXT, air.item_nm QUEST_TXT
+from  nci_admin_item_rel_alt_key air, admin_item aim,
+admin_item frm, nci_admin_item_rel frm_mod , admin_item de
+where  air.rel_typ_id = 63
+and aim.item_id = air.p_item_id and aim.ver_nr = air.p_item_ver_nr
+and frm.item_id = frm_mod.p_item_id and frm.ver_nr = frm_mod.p_item_ver_nr and aim.item_id = frm_mod.c_item_id and aim.ver_nr = frm_mod.c_item_ver_nr
+and frm_mod.rel_typ_id in (61,62)
+and frm.admin_item_typ_id in ( 54,55)
+and air.c_item_id = de.item_id
+and air.c_item_ver_nr = de.ver_nr;
