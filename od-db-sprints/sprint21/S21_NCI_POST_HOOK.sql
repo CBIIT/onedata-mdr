5cee9b964c3a877751977ba802b3730fc927b853
deepalimanathara <deepali.manathara@gmail.com>
2021-11-08 01:41:41 -0500

    Update S21_NCI_POST_HOOK.sql

--- od-db-sprints/sprint21/S21_NCI_POST_HOOK.sql
+++ od-db-sprints/sprint21/S21_NCI_POST_HOOK.sql
@@ -10,11 +10,88 @@ procedure spQuestVVRestore ( v_data_in in clob, v_data_out out clob, v_user_id v
 procedure spAIIns ( v_data_in in clob, v_data_out out clob);
 procedure spPVUpd ( v_data_in in clob, v_data_out out clob, v_user_id varchar2);
 procedure spCSIUpd ( v_data_in in clob, v_data_out out clob, v_user_id varchar2);
-
+procedure            sp_postprocess ( v_data_in in clob, v_data_out out clob);
 
 END;
 /
 create or replace PACKAGE BODY            NCI_POST_HOOK AS
+
+
+procedure            sp_postprocess ( v_data_in in clob, v_data_out out clob)
+as
+ hookInput        t_hookInput;
+    hookOutput       t_hookOutput := t_hookOutput ();
+    row_ori          t_row;
+  v_temp integer;
+  actions t_actions := t_actions();
+    action t_actionRowset;
+    rows  t_rows;
+  BEGIN
+    hookinput := Ihook.gethookinput (v_data_in);
+    hookoutput.invocationnumber := hookinput.invocationnumber;
+    hookoutput.originalrowset := hookinput.originalrowset;
+
+delete from NCI_ADMIN_ITEM_EXT;
+commit;
+
+delete from onedata_ra.NCI_ADMIN_ITEM_EXT;
+commit;
+
+--- Data Element
+insert into NCI_ADMIN_ITEM_EXT (ITEM_ID, VER_NR, USED_BY)
+select ai.item_id, ai.ver_nr, a.used_by
+from  admin_item ai,
+(SELECT item_id, ver_nr, LISTAGG(item_nm, ',') WITHIN GROUP (ORDER by ITEM_ID) AS USED_BY
+FROM (select distinct ub.item_id,ub.ver_nr, c.item_nm from vw_nci_used_by ub, vw_cntxt c where ub.cntxt_item_id = c.item_id and ub.cntxt_ver_nr = c.ver_nr and ub.owned_by=0 )
+GROUP BY item_id, ver_nr) a
+where ai.item_id = a.item_id (+) and ai.ver_nr = a.ver_nr(+)
+and ai.admin_item_typ_id = 4;
+commit;
+
+
+insert into NCI_ADMIN_ITEM_EXT (ITEM_ID, VER_NR,  CNCPT_CONCAT, CNCPT_CONCAT_NM, cncpt_concat_def, CNCPT_CONCAT_WITH_INT)
+select ai.item_id, ai.ver_nr, b.cncpt_cd, b.CNCPT_nm, b.cncpt_def, b.cncpt_cd_int
+from  admin_item ai,
+(SELECT cai.item_id, cai.ver_nr, LISTAGG(ai.item_long_nm, ':')WITHIN GROUP (ORDER by cai.nci_ord desc) as CNCPT_CD ,
+LISTAGG(decode(ai.item_long_nm, 'C45255',decode( cai.nci_cncpt_val,null, ai.item_nm, ai.item_nm || '::' || cai.NCI_CNCPT_VAL),ai.item_nm), ' ')WITHIN GROUP (ORDER by cai.nci_ord desc) as CNCPT_NM ,
+ LISTAGG(substr(decode(ai.item_long_nm, 'C45255',decode( cai.nci_cncpt_val,null, ai.item_desc, ai.item_desc || '::' || cai.NCI_CNCPT_VAL),ai.item_desc),1, 750), '_')WITHIN GROUP (ORDER by cai.nci_ord desc) as CNCPT_DEF,
+ LISTAGG(decode(ai.item_long_nm, 'C45255',decode( cai.nci_cncpt_val,null, ai.item_long_nm, ai.item_long_nm || '::' || cai.NCI_CNCPT_VAL),ai.item_long_nm), ':')WITHIN GROUP (ORDER by cai.nci_ord desc) as CNCPT_CD_INT
+from cncpt_admin_item cai, admin_item ai where ai.item_id = cai.cncpt_item_id and ai.ver_nr = cai.cncpt_ver_nr
+group by cai.item_id, cai.ver_nr) b
+where ai.item_id = b.item_id (+) and ai.ver_nr = b.ver_nr (+)
+and ai.admin_item_typ_id in (1,2,3,5,6,7,53);
+commit;
+
+
+insert into NCI_ADMIN_ITEM_EXT (ITEM_ID, VER_NR,  CNCPT_CONCAT, CNCPT_CONCAT_NM, cncpt_concat_def)
+select ai.item_id, ai.ver_nr, ai.item_long_nm, ai.item_nm, ai.item_desc
+from admin_item ai where ai.admin_item_typ_id = 49;
+commit;
+
+
+insert into NCI_ADMIN_ITEM_EXT (ITEM_ID, VER_NR)
+select ai.item_id, ai.ver_nr from  admin_item ai
+where ai.admin_item_typ_id in (8,9,50,51,52,54,56);
+commit;
+
+
+update nci_admin_item_ext e set (cncpt_concat, cncpt_concat_nm) = (select item_nm, item_nm from admin_item ai where ai.item_id = e.item_id and ai.ver_nr = e.ver_nr and ai.admin_item_typ_id = 53)
+where e.cncpt_concat is null and
+(e.item_id, e.ver_nr) in (select item_id, ver_nr from admin_item where admin_item_typ_id= 53);
+
+commit;
+
+insert into onedata_ra.NCI_ADMIN_ITEM_EXT select * from NCI_ADMIN_ITEM_EXT;
+commit;
+
+delete from nci_usr_cart where CNTCT_SECU_ID = 'GUEST';
+commit;
+
+delete from onedata_ra.nci_usr_cart where CNTCT_SECU_ID = 'GUEST';
+commit;
+ V_DATA_OUT := IHOOK.GETHOOKOUTPUT (HOOKOUTPUT);
+
+end;
 
 procedure spModRestore ( v_data_in in clob, v_data_out out clob, v_user_id varchar2, v_mode in varchar2)
 as
