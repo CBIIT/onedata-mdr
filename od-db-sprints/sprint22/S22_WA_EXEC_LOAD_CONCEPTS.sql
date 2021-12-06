9e97356e3326e441ef2813f73abe960954424b3e
asafieva <asafieva@gmail.com>
2021-12-06 13:34:18 -0500

    Load concepts clean up idseq from a previous load

--- od-db-sprints/sprint22/S22_WA_EXEC_LOAD_CONCEPTS.sql
+++ od-db-sprints/sprint22/S22_WA_EXEC_LOAD_CONCEPTS.sql
@@ -1,5 +1,7 @@
 set serveroutput on size 1000000
 SPOOL S22_LOAD_CONCEPTS.log
+update SAG_LOAD_CONCEPTS_EVS set con_idseq = null where con_idseq is not null;
+commit;
 --load concepts
 set timing on
 exec SAG_LOAD_CONCEPT_ADMIN_ITEM_DESIG;
