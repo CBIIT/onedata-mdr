3ae07002207c970306807f5aca1eb0c003eb6d7d
asafieva <asafieva@gmail.com>
2021-12-06 17:29:33 -0500

    Separated concepts load to RA schema to a separate script

---
+++ od-db-sprints/sprint22/S22_RA_EXEC_LOAD_CONCEPTS.sql
@@ -0,0 +1,18 @@
+set serveroutput on size 1000000
+SPOOL S22_RA_LOAD_CONCEPTS.log
+--insert concepts from ONEDATA_WA to ONEDATA_RA
+insert into onedata_ra.admin_item ra select * from onedata_wa.admin_item wa
+where wa.admin_item_typ_id = 49
+and creat_usr_id = 'ONEDATA' and lst_upd_usr_id = 'ONEDATA'
+and (wa.item_id, wa.ver_nr) not in (select item_id, ver_nr from onedata_ra.admin_item);
+commit;
+--insert concepts synonyms to ONEDATA_RA
+insert into onedata_ra.alt_nms 
+select * from onedata_wa.alt_nms where creat_usr_id = 'ONEDATA' and lst_upd_usr_id = 'ONEDATA' and nm_typ_id = 1064
+and (item_id, ver_nr) not in (select item_id, ver_nr from onedata_ra.alt_nms);
+commit;
+begin
+dbms_output.put_line('ONEDATA_RA load concepts is completed');
+end;
+/
+SPOOL OFF
