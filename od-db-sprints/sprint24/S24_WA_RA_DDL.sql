ccb69c5e666a4cae66a040dcd9cf8ee957aa0bab
deepalimanathara <deepali.manathara@gmail.com>
2022-01-04 12:07:32 -0500

    Update S24_WA_RA_DDL.sql

--- od-db-sprints/sprint24/S24_WA_RA_DDL.sql
+++ od-db-sprints/sprint24/S24_WA_RA_DDL.sql
@@ -7,3 +7,12 @@ UC.FLD_DELETE, UC.LST_DEL_DT, UC.S2P_TRN_DT, UC.LST_UPD_DT, UC.GUEST_USR_NM
 FROM NCI_USR_CART UC, ADMIN_ITEM AI WHERE AI.ITEM_ID = UC.ITEM_ID AND AI.VER_NR = UC.VER_NR
 and admin_item_typ_id in (4,52,54,2,3);
 
+
+-- Tracker
+delete from obj_key where obj_key_id in (90, 108, 110);
+commit;
+ insert into obj_key (obj_typ_id, obj_key_Desc, obj_key_id, obj_key_def, NCI_CD) values (31, 'RAVE ALS CDE', 90, 'RAVE ALS CDE', 'RAVE ALS CDE');
+ insert into obj_key (obj_typ_id, obj_key_Desc, obj_key_id, obj_key_def, NCI_CD) values (31, 'REDCap DD Form', 109, 'REDCap DD Form', 'REDCap DD Form');
+insert into obj_key (obj_typ_id, obj_key_Desc, obj_key_id, obj_key_def, NCI_CD) values (31, 'REDCap DD CDE', 108, 'REDCap DD CDE', 'REDCap DD CDE');
+insert into obj_key (obj_typ_id, obj_key_Desc, obj_key_id, obj_key_def, NCI_CD) values (31, 'RAVE ALS Form', 110, 'RAVE ALS Form', 'RAVE ALS Form');
+commit;
