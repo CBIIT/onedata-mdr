4b5d4a294c683dde7c590922b8f5f9d3675f923d
deepalimanathara <deepali.manathara@gmail.com>
2022-01-08 18:58:18 -0500

    Update S24_WA_RA_DDL.sql

--- od-db-sprints/sprint24/S24_WA_RA_DDL.sql
+++ od-db-sprints/sprint24/S24_WA_RA_DDL.sql
@@ -11,16 +11,19 @@ and admin_item_typ_id in (4,52,54,2,3);
 
 
 -- Tracker
-delete from obj_key where obj_key_id in (90, 108, 110, 105);
+delete from obj_key where obj_key_id in (90, 108, 110, 105, 111);
 commit;
  insert into obj_key (obj_typ_id, obj_key_Desc, obj_key_id, obj_key_def, NCI_CD) values (31, 'RAVE ALS CDE', 90, 'RAVE ALS CDE', 'RAVE ALS CDE');
  insert into obj_key (obj_typ_id, obj_key_Desc, obj_key_id, obj_key_def, NCI_CD) values (31, 'REDCap DD Form', 109, 'REDCap DD Form', 'REDCap DD Form');
 insert into obj_key (obj_typ_id, obj_key_Desc, obj_key_id, obj_key_def, NCI_CD) values (31, 'REDCap DD CDE', 108, 'REDCap DD CDE', 'REDCap DD CDE');
 insert into obj_key (obj_typ_id, obj_key_Desc, obj_key_id, obj_key_def, NCI_CD) values (31, 'RAVE ALS Form', 110, 'RAVE ALS Form', 'RAVE ALS Form');
 insert into obj_key (obj_typ_id, obj_key_Desc, obj_key_id, obj_key_def, NCI_CD) values (31, 'Form Excel', 105, 'Form Excel', 'Form Excel');
+insert into obj_key (obj_typ_id, obj_key_Desc, obj_key_id, obj_key_def, NCI_CD) values (31, 'Legacy CDE XML', 110, 'Legacy CDE XML', 'Legacy CDE XML');
 
 commit;
 
+  
+ 
 
 alter table nci_dload_hdr modify (DLOAD_TYP_ID integer null);
 
