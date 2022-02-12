1b7e84b166f6bd03d02770e92b710482c07afb1a
deepalimanathara <deepali.manathara@gmail.com>
2022-02-12 15:36:49 -0500

    Update S26_WA_RA_DDL.sql

--- od-db-sprints/sprint26/S26_WA_RA_DDL.sql
+++ od-db-sprints/sprint26/S26_WA_RA_DDL.sql
@@ -1,6 +1,13 @@
 create table nci_job_log 
 (log_id integer not null primary key, 
- start_dt datetime default sysdate,
+ start_dt timestamp default sysdate,
  job_step_desc  varchar2(255) not null,
- end_dt datetime,
+ end_dt timestamp,
+ "CREAT_DT" DATE DEFAULT sysdate, 
+	"CREAT_USR_ID" VARCHAR2(50 BYTE) COLLATE "USING_NLS_COMP" DEFAULT user, 
+	"LST_UPD_USR_ID" VARCHAR2(50 BYTE) COLLATE "USING_NLS_COMP" DEFAULT user, 
+	"FLD_DELETE" NUMBER(1,0) DEFAULT 0, 
+	"LST_DEL_DT" DATE DEFAULT sysdate, 
+	"S2P_TRN_DT" DATE DEFAULT sysdate, 
+	"LST_UPD_DT" DATE DEFAULT sysdate);
  
