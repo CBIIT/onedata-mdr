f3d78c1414662dbd542268ca277572b4fb10c9f3
trushi2 <irina.trush@nih.gov>
2022-02-14 14:13:40 -0500

    Add files via upload

--- xcustomize/Create S25_EXEC_LOAD_CONCEPTS_JOB/S25_RA_EXEC_LOAD_CONCEPTS_JOB.sql
+++ xcustomize/Create S25_EXEC_LOAD_CONCEPTS_JOB/S25_RA_EXEC_LOAD_CONCEPTS_JOB.sql
@@ -10,20 +10,17 @@ insert into onedata_ra.alt_nms
 select * from onedata_wa.alt_nms where creat_usr_id = 'ONEDATA' and lst_upd_usr_id = 'ONEDATA' and nm_typ_id = 1064
 and (item_id, ver_nr) not in (select item_id, ver_nr from onedata_ra.alt_nms);
 commit;
-
 dbms_output.put_line('ONEDATA_RA load concepts is completed');
-
-
 DBMS_MVIEW.REFRESH('VW_CNCPT'); 
-
 END;
-
+/
 BEGIN
 DBMS_SCHEDULER.CREATE_JOB (
-   job_name           =>  'ONEDATA_RA.EXEC_LOAD_CONCEPTS_JOB_RA',
+   job_name           =>  'LOAD_CONCEPTS_JOB_RA_S25',
    job_type           =>  'STORED_PROCEDURE',
    job_action         =>  'EXEC_LOAD_CONCEPTS_RA',
    start_date         =>  '22-OCT-22 10.00.00 PM',
    repeat_interval    =>  'FREQ=DAILY',
-   enabled            =>   TRUE);
+   enabled            =>   FALSE);
 END;
+/
--- xcustomize/Create S25_EXEC_LOAD_CONCEPTS_JOB/S25_WA_EXEC_LOAD_CONCEPTS_JOB.sql
+++ xcustomize/Create S25_EXEC_LOAD_CONCEPTS_JOB/S25_WA_EXEC_LOAD_CONCEPTS_JOB.sql
@@ -1,11 +1,9 @@
 CREATE OR REPLACE PROCEDURE EXEC_LOAD_CONCEPTS AS 
 BEGIN
-
 SAG_LOAD_CONCEPT_ADMIN_ITEM_DESIG;
 DBMS_MVIEW.REFRESH('VW_CNCPT'); 
-
 END;
-
+/
 BEGIN
 DBMS_SCHEDULER.CREATE_JOB (
    job_name           =>  'ONEDATA_WA.EXEC_LOAD_CONCEPTS_JOB_WA',
@@ -13,5 +11,6 @@ DBMS_SCHEDULER.CREATE_JOB (
    job_action         =>  'EXEC_LOAD_CONCEPTS',
    start_date         =>  '22-OCT-22 10.00.00 PM',
    repeat_interval    =>  'FREQ=DAILY',
-   enabled            =>   TRUE);
+   enabled            =>   FALSE);
 END;
+/
