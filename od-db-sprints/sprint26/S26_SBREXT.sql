dc4bed0edaf79e4ea14dfc87599c1bedfe54494c
deepalimanathara <deepali.manathara@gmail.com>
2022-02-21 13:06:28 -0500

    Update S26_SBREXT.sql

--- od-db-sprints/sprint26/S26_SBREXT.sql
+++ od-db-sprints/sprint26/S26_SBREXT.sql
@@ -1,3 +1,4 @@
 alter table quest_attributes_ext drop constraint QAT_QC_FK;
 drop index QAT_QC_VV_FK;
 alter table quest_attributes_ext drop constraint QAT_QC_FK2;
+alter table quest_vv_ext drop constraint QVT_QVT_FK;
