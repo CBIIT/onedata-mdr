f3458b3d05a25cfa77243ffe12438c9320f5b573
deepalimanathara <deepali.manathara@gmail.com>
2022-02-23 11:43:55 -0500

    Update S26_WA_DDL.sql

--- od-db-sprints/sprint26/S26_WA_DDL.sql
+++ od-db-sprints/sprint26/S26_WA_DDL.sql
@@ -7,3 +7,13 @@ drop sequence od_seq_lang;
  CREATE SEQUENCE od_seq_lang
           INCREMENT BY 1
           START WITH 2000;
+
+CREATE OR REPLACE TRIGGER OD_TR_REF_DOC  BEFORE INSERT  on REF_DOC  for each row
+                  BEGIN    IF (:NEW.REF_DOC_ID<= 0  or :NEW.REF_DOC_ID is null)  THEN 
+                  select od_seq_REF_DOC.nextval
+            into :new.REF_DOC_ID  from  dual ;   END IF; 
+  
+             IF (new.nci_idseq is null)  THEN 
+                  :new.nci_idseq := nci_11179.cmr_guid();  END IF; 
+                  END ;          
+/
