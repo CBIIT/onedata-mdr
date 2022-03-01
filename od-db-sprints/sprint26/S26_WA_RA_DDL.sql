fe2a3099218549eccfd7f0242aa0c5e4da6bb6d2
asafieva <asafieva@gmail.com>
2022-03-01 11:01:17 -0500

    Added missing semicolon after create view

--- od-db-sprints/sprint26/S26_WA_RA_DDL.sql
+++ od-db-sprints/sprint26/S26_WA_RA_DDL.sql
@@ -5,13 +5,13 @@ create table nci_job_log
  run_param varchar2(255),
  end_dt timestamp,
  exec_by varchar2(50),
- "CREAT_DT" DATE DEFAULT sysdate, 
-	"CREAT_USR_ID" VARCHAR2(50 BYTE) COLLATE "USING_NLS_COMP" DEFAULT user, 
-	"LST_UPD_USR_ID" VARCHAR2(50 BYTE) COLLATE "USING_NLS_COMP" DEFAULT user, 
-	"FLD_DELETE" NUMBER(1,0) DEFAULT 0, 
-	"LST_DEL_DT" DATE DEFAULT sysdate, 
-	"S2P_TRN_DT" DATE DEFAULT sysdate, 
-	"LST_UPD_DT" DATE DEFAULT sysdate);
+ CREAT_DT DATE DEFAULT sysdate, 
+	CREAT_USR_ID VARCHAR2(50 BYTE) COLLATE "USING_NLS_COMP" DEFAULT user, 
+	LST_UPD_USR_ID VARCHAR2(50 BYTE) COLLATE "USING_NLS_COMP" DEFAULT user, 
+	FLD_DELETE NUMBER(1,0) DEFAULT 0, 
+	LST_DEL_DT DATE DEFAULT sysdate, 
+	S2P_TRN_DT DATE DEFAULT sysdate, 
+	LST_UPD_DT DATE DEFAULT sysdate);
  
 alter table NCI_STG_CDE_CREAT add (DE_CONC_VER_NR_CREAT number(4,2), VAL_DOM_VER_NR_CREAT number(4,2));
 
@@ -68,7 +68,7 @@ and frm_mod.rel_typ_id in (61,62)
 and frm.admin_item_typ_id in ( 54,55)
 and air.c_item_id = de.item_id (+)
 and air.c_item_ver_nr = de.ver_nr (+)
-and air.DEFLT_VAL_ID = vv.nci_pub_id (+)
+and air.DEFLT_VAL_ID = vv.nci_pub_id (+);
 
 
 drop materialized view VW_CNCPT;
