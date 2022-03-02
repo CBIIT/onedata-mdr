6c56b8267bf94ee9ff4e9e59bcba8f6c9032cf42
trushi2 <irina.trush@nih.gov>
2022-03-02 10:29:09 -0500

    Add files via upload

--- od-db-sprints/sprint26/S26_WA_VW_ALS.sql
+++ od-db-sprints/sprint26/S26_WA_VW_ALS.sql
@@ -1,4 +1,213 @@
 /* Formatted on 2/11/2022 11:45:25 AM (QP5 v5.354) */
+CREATE OR REPLACE FORCE VIEW VW_ALS_FIELD_RAW_DATA
+(
+    PROTOCOL,
+    FORM_LONG_NAME,
+    FORM_ID,
+    FORM_VERSION,
+    MODULE_DISPLAY_ORDER,
+    MODULE_LONG_NAME,
+    QUEST_VERSION,
+    QUEST_ID,
+    QUEST_DISP_ORD,
+    QUEST_LONG_NAME,
+    QUEST_SHORT_NAME,
+    ISMANDATORY,
+    QUEST_INSTR,
+    DE_PUB_ID,
+    DE_VERSION,
+    CDE_SHORT_NAME,
+    CDE_LONG_NAME,
+    VD_SHORT_NAME,
+    VD_LONG_NAME,
+    VAL_DOM_MAX_CHAR,
+    VAL_DOM_TYP_ID,
+    VD_MAX_LENGTH,
+    VD_MIN_LENGTH,
+    VD_DISPLAY_FORMAT,
+    VD_DATA_TYPE_NCI,
+    VD_DATA_TYPE_MAP,
+    VD_ID,
+    VD_VERSION,
+    UOM_ID
+)
+BEQUEATH DEFINER
+AS
+    SELECT /*+ PARALLEL(4) */
+           DISTINCT REL.PROTO_ID,
+                    FR.ITEM_NM     Form_Long_Name,         --rownum Ordinal,--
+                    FR.ITEM_ID     FORM_ID,
+                    FR.VER_NR      Form_Version,
+                    NULL           Module,
+                    NULL           MODULE_LONG_NAME,
+                    NULL           QUESTION_Version,
+                    NULL           QUESTION_ID,
+                    NULL           QUEST_DISP_ORD,
+                    'FORM_OID'     QUEST_Long_Name,
+                    NULL           QUEST_SORT_Name,
+                    NULL           isMandatory,
+                    NULL           QUEST_Inst,
+                    NULL           DE_PUB_ID,
+                    NULL           DE_VERSION,
+                    'FORM_OID'     CDE_Sort_Name,
+                    NULL           CDE_Long_Name,
+                    NULL           VD_Short_Name,
+                    NULL           VD_Long_Name,
+                    NULL           VAL_DOM_MAX_CHAR,
+                    NULL           VAL_DOM_TYP_ID,
+                    NULL           VD_Max_Length,
+                    NULL           VD_Min_Length,
+                    NULL           VD_Display_Format,
+                    NULL           VD_DATA_TYPE,
+                    NULL           VD_DATA_TYPE_MAP,
+                    NULL           VD_ID,
+                    NULL           VD_Version,
+                    NULL           UOM_ID
+      FROM ADMIN_ITEM  FR,
+           --           (  SELECT C_ITEM_ID,
+           --                     C_ITEM_VER_NR,
+           --                     LISTAGG (P_ITEM_ID || 'v' || P_ITEM_VER_NR,
+           --                              ':'
+           --                              ON OVERFLOW TRUNCATE)
+           --                     WITHIN GROUP (ORDER BY DISP_ORD)    PROTO_ID
+           --                FROM NCI_ADMIN_ITEM_REL
+           --            GROUP BY C_ITEM_ID, C_ITEM_VER_NR)
+            (  SELECT /*+ PARALLEL(4) */
+                      C_ITEM_ID,
+                      C_ITEM_VER_NR,
+                      LISTAGG (p.ITEM_LONG_NM, ':' ON OVERFLOW TRUNCATE)
+                          WITHIN GROUP (ORDER BY DISP_ORD)    PROTO_ID
+                 FROM NCI_ADMIN_ITEM_REL r, ADMIN_ITEM p
+                WHERE     P.ADMIN_ITEM_TYP_ID = 50
+                      AND p.ITEM_ID = r.P_ITEM_ID
+                      AND p.VER_NR = r.P_ITEM_VER_NR
+                      AND NVL (p.FLD_DELETE, 0) = 0
+             GROUP BY C_ITEM_ID, C_ITEM_VER_NR) REL
+     WHERE     FR.ADMIN_ITEM_TYP_ID = 54
+           AND NVL (FR.FLD_DELETE, 0) = 0
+           AND FR.ITEM_ID = REL.C_ITEM_ID(+)
+           AND FR.VER_NR = REL.C_ITEM_VER_NR(+)
+    UNION
+    SELECT /*+ PARALLEL(4) */
+           DISTINCT
+           REL.PROTO_ID,
+           FR.ITEM_NM
+               Form_Long_Name,                             --rownum Ordinal,--
+           FR.ITEM_ID
+               FORM_ID,
+           FR.VER_NR
+               Form_Version,
+           module.DISP_ORD
+               Module,
+           MOD.ITEM_NM
+               MODULE_LONG_NAME,
+           QUESTION.NCI_VER_NR
+               QUEST_Version,
+           QUESTION.NCI_PUB_ID
+               QUEST_ID,
+           QUESTION.DISP_ORD
+               QUEST_DISP_ORD,
+           QUESTION.ITEM_LONG_NM
+               QUEST_Long_Name,
+           QUESTION.ITEM_NM
+               QUEST_SHORT_Name,
+           DECODE (QUESTION.req_IND,  1, 'TRUE',  0, 'FALSE')
+               isMandatory,
+           QUESTION.INSTR
+               QUEST_INST,
+           DE.ITEM_ID
+               DE_PUB_ID,
+           DE.VER_NR
+               DE_VERSION,
+           DE.ITEM_LONG_NM
+               CDE_Sort_Name,
+           DE.ITEM_NM
+               CDE_Long_Name,
+           VD.ITEM_LONG_NM
+               VD_Sort_Name,
+           VD.ITEM_NM
+               VD_Long_Name,
+           VALUE_DOM.VAL_DOM_MAX_CHAR,
+           DECODE (VAL_DOM_TYP_ID,  17, 'E',  18, 'N')
+               VAL_DOM_TYP_ID,
+           VALUE_DOM.VAL_DOM_HIGH_VAL_NUM
+               VD_Max_Length,
+           VALUE_DOM.VAL_DOM_MIN_CHAR
+               VD_Min_Length,
+           FMT.FMT_NM
+               VD_Display_Format,
+           dt.NCI_CD
+               VD_DATA_TYPE_NCI,
+           dt.NCI_DTTYPE_MAP
+               VD_DATA_TYPE_MAP,
+           VD.ITEM_id
+               VD_ID,
+           VD.VER_NR
+               VD_Version,
+           VALUE_DOM.UOM_ID
+      FROM ADMIN_ITEM                  de,
+           de                          de2,
+           NCI_FORM                    FORM,
+           ADMIN_ITEM                  FR,
+           NCI_ADMIN_ITEM_REL          MODULE,
+           ADMIN_ITEM                  MOD,
+           NCI_ADMIN_ITEM_REL_ALT_KEY  QUESTION,
+           ADMIN_ITEM                  VD,
+           VALUE_DOM,
+           FMT,
+           DATA_TYP                    DT,
+           (  SELECT /*+ PARALLEL(4) */
+                     C_ITEM_ID,
+                     C_ITEM_VER_NR,
+                     LISTAGG (p.ITEM_LONG_NM, ':' ON OVERFLOW TRUNCATE)
+                         WITHIN GROUP (ORDER BY DISP_ORD)    PROTO_ID
+                FROM NCI_ADMIN_ITEM_REL r, ADMIN_ITEM p
+               WHERE     P.ADMIN_ITEM_TYP_ID = 50
+                     AND p.ITEM_ID = r.P_ITEM_ID
+                     AND p.VER_NR = r.P_ITEM_VER_NR
+                     AND NVL (p.FLD_DELETE, 0) = 0
+            GROUP BY C_ITEM_ID, C_ITEM_VER_NR) REL,
+           (SELECT /*+ PARALLEL(4) */
+                   REF.REF_DESC, REF.ITEM_ID, REF.VER_NR
+              FROM REF, OBJ_KEY
+             WHERE     REF.REF_TYP_ID = OBJ_KEY.OBJ_KEY_ID
+                   AND OBJ_KEY.OBJ_KEY_DESC = 'Preferred Question Text'
+                   AND NVL (REF.FLD_DELETE, 0) = 0) REF
+     WHERE     DE.ADMIN_ITEM_TYP_ID = 4                                -- (DE)
+           AND VD.ADMIN_ITEM_TYP_ID = 3                                 --(VD)
+           AND FR.ADMIN_ITEM_TYP_ID = 54
+           AND FORM.ITEM_ID = module.P_ITEM_ID
+           AND FORM.VER_NR = module.P_ITEM_VER_NR
+           AND MOD.ITEM_ID = module.C_ITEM_ID
+           AND MOD.VER_NR = module.C_ITEM_VER_NR
+           AND FR.ITEM_ID = REL.C_ITEM_ID(+)
+           AND FR.VER_NR = REL.C_ITEM_VER_NR(+)
+           AND NVL (FR.FLD_DELETE, 0) = 0
+           AND module.REL_TYP_ID = 61
+           AND NVL (module.FLD_DELETE, 0) = 0
+           AND QUESTION.REL_TYP_ID = 63
+           AND NVL (QUESTION.FLD_DELETE, 0) = 0
+           AND QUESTION.P_ITEM_ID = module.C_ITEM_ID
+           AND QUESTION.P_ITEM_VER_NR = module.C_ITEM_VER_NR
+           AND QUESTION.C_ITEM_ID = de.ITEM_ID
+           AND NVL (DE.FLD_DELETE, 0) = 0
+           AND de.ITEM_ID = de2.ITEM_ID
+           AND de.ver_nr = de2.VER_NR
+           AND QUESTION.C_ITEM_VER_NR = de.VER_NR
+           AND DE.ITEM_ID = REF.ITEM_ID(+)
+           AND DE.VER_NR = REF.VER_NR(+)                                  /**/
+           AND VALUE_DOM.VAL_DOM_FMT_ID = FMT.FMT_ID(+)
+           AND VALUE_DOM.dttype_id = DT.dttype_id
+           AND de2.VAL_DOM_ITEM_ID = VALUE_DOM.ITEM_ID
+           AND de2.VAL_DOM_VER_NR = VALUE_DOM.VER_NR
+           AND NVL (VD.FLD_DELETE, 0) = 0
+           AND VD.ITEM_ID = VALUE_DOM.ITEM_ID
+           AND VD.VER_NR = VALUE_DOM.VER_NR
+           AND FORM.ITEM_ID = FR.ITEM_ID
+           AND FORM.VER_NR = FR.VER_NR
+    ORDER BY 3, 5, 7;
+
+
 CREATE OR REPLACE FORCE VIEW VW_ALS_FIELD_FORM_DD
 (
     COLLECT_ID,
