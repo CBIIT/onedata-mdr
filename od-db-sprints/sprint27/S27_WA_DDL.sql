998b92e147d5937cb61e57f239a479c75a4fb374
deepalimanathara <deepali.manathara@gmail.com>
2022-03-17 12:38:09 -0400

    Update S27_WA_DDL.sql

--- od-db-sprints/sprint27/S27_WA_DDL.sql
+++ od-db-sprints/sprint27/S27_WA_DDL.sql
@@ -79,7 +79,7 @@ end if;
 :new.creat_usr_id_x := :new.creat_usr_id;
 :new.lst_upd_usr_id_x := :new.lst_upd_usr_id;
 if (:new.admin_item_typ_id = 53) then -- Value Meaning
-:new.ITEM_NM_ID_VER := :new.ITEM_ID || ' | ' || :new.ver_nd || ' | ' || :new.item_nm;
+:new.ITEM_NM_ID_VER := :new.ITEM_ID || ' | ' || :new.ver_nr || ' | ' || :new.item_nm;
 else
 :new.ITEM_NM_ID_VER :=  :new.item_nm;
 
@@ -88,4 +88,44 @@ if (:new.lst_upd_dt is null) then
 :new.lst_upd_dt := sysdate;
 end if;
 END ;
+
+CREATE OR REPLACE TRIGGER TR_AI_AUD_TS
+  BEFORE UPDATE ON ADMIN_ITEM
+   FOR EACH ROW
+BEGIN
+  :new.LST_UPD_DT := SYSDATE;
+  :new.LST_UPD_USR_ID_X := :new.LST_UPD_USR_ID;
+
+if (upper(:new.ITEM_LONG_NM) = 'SYSGEN' and :new.ADMIN_ITEM_TYP_ID =4) then
+for cur in (select * from de where item_id = :new.ITEM_ID and ver_nr = :new.ver_nr) loop
+:new.ITEM_LONG_NM := cur.DE_CONC_ITEM_ID || 'v' || trim(to_char(cur.DE_CONC_VER_NR, '9999.99')) || ':' || cur.VAL_DOM_ITEM_ID || 'v' || trim(to_char(cur.VAL_DOM_VER_NR, '9999.99'));
+end loop;
+end if;
+
+
+if (upper(:new.ITEM_LONG_NM) = 'SYSGEN' and :new.ADMIN_ITEM_TYP_ID =2) then
+for cur in (select * from de_conc where item_id = :new.ITEM_ID and ver_nr = :new.ver_nr) loop
+:new.ITEM_LONG_NM := cur.OBJ_CLS_ITEM_ID || 'v' || trim(to_char(cur.OBJ_CLS_VER_NR, '9999.99')) || ':' || cur.PROP_ITEM_ID || 'v' || trim(to_char(cur.PROP_VER_NR, '9999.99'));
+end loop;
+end if;
+
+if (upper(:new.ITEM_LONG_NM) = 'SYSGEN' and :new.ADMIN_ITEM_TYP_ID not in (2,4)) then
+:new.ITEM_LONG_NM := :new.ITEM_ID || 'v' || trim(to_char(:new.ver_nr, '9999.99'));
+end if;
+
+if (:new.CREAT_USR_ID_X is null) then
+:new.CREAT_USR_ID_X := :new.CREAT_USR_ID;
+end if;
+if (:new.LST_UPD_USR_ID_X is null) then
+:new.LST_UPD_USR_ID_X := :new.LST_UPD_USR_ID;
+end if;
+
+if (:new.admin_item_typ_id = 53) then -- Value Meaning
+:new.ITEM_NM_ID_VER := :new.ITEM_ID || ' | ' || :new.ver_nr || ' | ' || :new.item_nm;
+else
+:new.ITEM_NM_ID_VER :=  :new.item_nm;
+
+end if;
+
+END TR_AI_AUD_TS;
 
