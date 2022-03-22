199cbaf40d78b57f83f1c8e1cd90277e4fe8ace6
deepalimanathara <deepali.manathara@gmail.com>
2022-03-22 13:23:48 -0400

    Update S28_WA_DDL.sql

--- od-db-sprints/sprint28/S28_WA_DDL.sql
+++ od-db-sprints/sprint28/S28_WA_DDL.sql
@@ -4,10 +4,10 @@ CREATE OR REPLACE TRIGGER TR_NCI_DLOAD_DTL_TS
   for each row
 BEGIN
   :new.LST_UPD_DT := SYSDATE;
-  update NCI_DLOAD_HDR set LST_UPD_DT = sysdate, lst_upd_usr_id =:new.lst_upd_usr_id where hdr_id = :new.hdr_id;
   if (nvl(:new.fld_delete,0)  <> nvl(:old.fld_delete,0)) then
   update NCI_DLOAD_HDR set LST_UPD_DT = sysdate, file_blob = null, lst_upd_usr_id =:new.lst_upd_usr_id where hdr_id = :new.hdr_id;
  else
   update NCI_DLOAD_HDR set LST_UPD_DT = sysdate, lst_upd_usr_id =:new.lst_upd_usr_id where hdr_id = :new.hdr_id;
+  end if;
  END;
 /
