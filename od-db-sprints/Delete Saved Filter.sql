cce04e8bb8ffed4a4850d7fac1f06f761ac9f407
asafieva <asafieva@gmail.com>
2022-03-23 13:03:12 -0400

    Update Delete Saved Filter.sql
    a semicolon was missed on delete statement

--- od-db-sprints/Delete Saved Filter.sql
+++ od-db-sprints/Delete Saved Filter.sql
@@ -1,6 +1,6 @@
 
 delete from od_md_objrel where obj_depn_id in
-(select obj_id from od_md_obj where obj_typ_id = 71)
+(select obj_id from od_md_obj where obj_typ_id = 71);
 commit;
 
 delete from od_md_objprop where obj_id  in (
