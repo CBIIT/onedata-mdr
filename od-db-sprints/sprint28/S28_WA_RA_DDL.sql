2e12db574a648f2c6cdf0441385abb5950560a1e
deepalimanathara <deepali.manathara@gmail.com>
2022-03-24 14:28:24 -0400

    Update S28_WA_RA_DDL.sql

--- od-db-sprints/sprint28/S28_WA_RA_DDL.sql
+++ od-db-sprints/sprint28/S28_WA_RA_DDL.sql
@@ -1,4 +1,6 @@
 --drop trigger TR_NCI_ALT_NMS_DENORM_INS;
 
 
-create unique index uni_alt_nms on alt_nms (item_id, ver_nr, nm_typ_id, cntxt_item_id, cntxt_ver_nr, nm_desc);
+--create unique index uni_alt_nms on alt_nms (item_id, ver_nr, nm_typ_id, cntxt_item_id, cntxt_ver_nr, nm_desc);
+alter table alt_nms add constraint uni_alt_nms unique(item_id, ver_nr, nm_typ_id, cntxt_item_id, cntxt_ver_nr, nm_desc);
+
