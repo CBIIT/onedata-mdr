756fb4d5de864cdc10b3951007834e4603a6c5c7
deepalimanathara <deepali.manathara@gmail.com>
2022-03-29 14:10:06 -0400

    Update S28_WA_RA_DDL.sql

--- od-db-sprints/sprint28/S28_WA_RA_DDL.sql
+++ od-db-sprints/sprint28/S28_WA_RA_DDL.sql
@@ -4,3 +4,4 @@
 --create unique index uni_alt_nms on alt_nms (item_id, ver_nr, nm_typ_id, cntxt_item_id, cntxt_ver_nr, nm_desc);
 alter table alt_nms add constraint uni_alt_nms unique(item_id, ver_nr, nm_typ_id, cntxt_item_id, cntxt_ver_nr, nm_desc);
 
+alter table test_results add (diff_cnt number);
