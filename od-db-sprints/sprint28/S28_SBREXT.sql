fa3affeaabe69cc2b62b5270388d8f6b36119687
deepalimanathara <deepali.manathara@gmail.com>
2022-04-05 15:13:42 -0400

    Update S28_SBREXT.sql

--- od-db-sprints/sprint28/S28_SBREXT.sql
+++ od-db-sprints/sprint28/S28_SBREXT.sql
@@ -5,3 +5,6 @@ alter table sbrext.triggered_actions_ext drop constraint TAT_QC_FK;
 alter table sbrext.triggered_actions_ext drop constraint TAT_QC_FK2;
 alter table sbrext.triggered_actions_ext drop constraint TAT_QVT_FK;
 alter table sbrext.triggered_actions_ext drop constraint TAT_QVT_FK2;
+
+alter table sbrext.VALID_VALUES_ATT_EXT drop constraint VVT_QC_FK;
+
