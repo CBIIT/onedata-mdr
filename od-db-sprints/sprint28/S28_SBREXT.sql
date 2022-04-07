7697c04e5844c6ca35d63d5cd57620dcea9538f1
deepalimanathara <deepali.manathara@gmail.com>
2022-04-07 10:08:25 -0400

    Update S28_SBREXT.sql

--- od-db-sprints/sprint28/S28_SBREXT.sql
+++ od-db-sprints/sprint28/S28_SBREXT.sql
@@ -9,3 +9,11 @@ alter table sbrext.quest_contents_ext drop constraint QC_VPV_FK;
 
 alter table sbrext.VALID_VALUES_ATT_EXT drop constraint VVT_QC_FK;
 
+create index nci_const_vv on sbrext.quest_contents_ext (p_val_idseq);
+
+create index nci_const_qst on sbrext.quest_contents_ext (p_qst_idseq);
+
+create index nci_const_mod on sbrext.quest_contents_ext (p_mod_idseq);
+
+
+
