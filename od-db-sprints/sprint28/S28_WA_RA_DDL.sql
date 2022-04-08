856fb0c2e6f5557470121066f81ee87b1118d2b3
deepalimanathara <deepali.manathara@gmail.com>
2022-04-08 13:55:16 -0400

    Update S28_WA_RA_DDL.sql

--- od-db-sprints/sprint28/S28_WA_RA_DDL.sql
+++ od-db-sprints/sprint28/S28_WA_RA_DDL.sql
@@ -14,3 +14,7 @@ ai.CREAT_DT, ai.CREAT_USR_ID, ai.LST_UPD_USR_ID, air.FLD_DELETE, ai.LST_DEL_DT, 
 ai.regstr_stus_id, ai.admin_stus_id, ai.cntxt_item_id, ai.cntxt_ver_nr, ai.item_long_nm
 from admin_item ai, nci_admin_item_rel air where ai.item_id = air.c_item_id and ai.ver_nr = air.c_item_ver_nr and air.rel_typ_id in (61,62);
 
+create or replace view vw_NCI_QUEST_VALID_VALUE as
+select NCI_PUB_ID, NCI_VER_NR, Q_VER_NR, Q_PUB_ID, VM_NM, VM_LNM, VM_DEF, VALUE, CMNTS, EDIT_IND, SEQ_NBR, NCI_IDSEQ,
+MEAN_TXT, DESC_TXT, CREAT_DT, CREAT_USR_ID, LST_UPD_USR_ID, 0 FLD_DELETE, LST_DEL_DT, S2P_TRN_DT, LST_UPD_DT, DISP_ORD, INSTR, VAL_MEAN_ITEM_ID, VAL_MEAN_VER_NR
+from NCI_QUEST_VALID_VALUE;
