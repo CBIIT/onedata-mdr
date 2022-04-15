73cb8673939c43f9636118c91b449ee3d8a17e25
trushi2 <irina.trush@nih.gov>
2022-04-15 11:24:20 -0400

    Add files via upload

--- od-db-sprints/sprint28/S28_SBREXT.sql
+++ od-db-sprints/sprint28/S28_SBREXT.sql
@@ -1,5 +1,5 @@
 
-alter table quest_vv_ext drop constraint QVT_QC_FK
+alter table quest_vv_ext drop constraint QVT_QC_FK;
 
 alter table sbrext.triggered_actions_ext drop constraint TAT_QC_FK;
 alter table sbrext.triggered_actions_ext drop constraint TAT_QC_FK2;
