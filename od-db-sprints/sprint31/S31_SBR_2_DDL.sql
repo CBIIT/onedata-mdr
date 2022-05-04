a416c7c4718e253a83ffcf925a215dec12096a8f
deepalimanathara <deepali.manathara@gmail.com>
2022-05-04 11:10:14 -0400

    Update S31_SBR_2_DDL.sql

--- od-db-sprints/sprint31/S31_SBR_2_DDL.sql
+++ od-db-sprints/sprint31/S31_SBR_2_DDL.sql
@@ -1,5 +1,18 @@
+-- Remove constraints and indexes on an old table that is no long er used but needs to stay for history
+-- Indexes and constraints deleted so that if the ref doc is deleted, it does not cause an issue.
 
-alter table sbrext.match_results_ext MRT_ASV_FK
-MRT_DET_FK
-MRT_RDT_FK
-MRT_VDN_FK
+alter table sbrext.match_results_ext DISABLE CONSTRAINT MRT_ASV_FK;
+alter table sbrext.match_results_ext DISABLE CONSTRAINT MRT_DET_FK;
+alter table sbrext.match_results_ext DISABLE CONSTRAINT MRT_RDT_FK;
+alter table sbrext.match_results_ext DISABLE CONSTRAINT MRT_VDN_FK;
+
+drop index sbrext.MRT_ASV_FK;
+drop index sbrext.MRT_DET_FK;
+drop index sbrext.MRT_PVE_FK;
+drop index sbrext.MRT_RDT_FK;
+drop index sbrext.MRT_VDN_FK;
+drop index sbrext.MRT_QC_CRF_FK;
+drop index sbrext.MRT_QC_MATCH_FK;
+drop index sbrext.MRT_QC_SUBMIT_FK;
+
+
