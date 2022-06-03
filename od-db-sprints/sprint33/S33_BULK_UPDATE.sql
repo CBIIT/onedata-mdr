2d093d8abe69c70dcc3c6c7dec6a58c9a0770584
deepalimanathara <deepali.manathara@gmail.com>
2022-06-03 10:03:20 -0400

    Update S33_BULK_UPDATE.sql

--- od-db-sprints/sprint33/S33_BULK_UPDATE.sql
+++ od-db-sprints/sprint33/S33_BULK_UPDATE.sql
@@ -1,7 +1,6 @@
 alter table ADMIN_ITEM DISABLE ALL TRIGGERS;
 
-update admin_item set ITEM_NM_ID_VER =  item_nm || '|' || ITEM_ID || '|' || ver_nr || '|' || obj_key_desc  from obj_key where obj_Key_id = admin_item.admin_item_typ_id;
+update admin_item set ITEM_NM_ID_VER =  (select item_nm || '|' || ITEM_ID || '|' || ver_nr || '|' || obj_key_desc  from obj_key where obj_Key_id = admin_item.admin_item_typ_id);
 commit;
 
 alter table ADMIN_ITEM ENABLE ALL TRIGGERS;
-
