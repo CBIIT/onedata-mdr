d933471dc932c85ad8a47e3d5bcbd01209d7ea76
asafieva <asafieva@gmail.com>
2022-06-06 17:39:11 -0400

    SAG_LOAD_CONCEPT_RETIRE procedure updates

--- od-db-sprints/sprint33/S33_LOAD_CONCEPT_UPDATES.sql
+++ od-db-sprints/sprint33/S33_LOAD_CONCEPT_UPDATES.sql
@@ -1,13 +1,15 @@
-create or replace Procedure SAG_LOAD_CONCEPT_RETIRE
+create or replace Procedure SAG_LOAD_CONCEPT_RETIRE (p_END_DATE IN date default sysdate)
 AS
-v_end_date DATE := sysdate;
+v_end_date DATE;
 BEGIN
+	v_end_date := p_END_DATE;
 update admin_item set admin_stus_id = 77, --WFS to 'RETIRED ARCHIVED'
 	LST_UPD_USR_ID = 'ONEDATA', 
-	UNTL_DT = v_end_date
-	where admin_item_typ_id = 49 and 
-    admin_stus_id <> 77 and 
-	item_long_nm in (
+	UNTL_DT = v_end_date,
+	REGSTR_STUS_ID = 11 -- 'Retired'
+	where admin_item_typ_id = 49 and -- Concept
+	admin_stus_id <> 77 and 
+	item_long_nm in ( -- EVS provided list - parents of retired concepts
 	select code from sag_load_concepts_evs where parents in(
 'C176957',
 'C167277',
