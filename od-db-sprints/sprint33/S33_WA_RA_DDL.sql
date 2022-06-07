82ee901cb6656f76c8c98729b8c228e1908ed318
deepalimanathara <deepali.manathara@gmail.com>
2022-06-07 12:33:23 -0400

    Update S33_WA_RA_DDL.sql

--- od-db-sprints/sprint33/S33_WA_RA_DDL.sql
+++ od-db-sprints/sprint33/S33_WA_RA_DDL.sql
@@ -1,3 +1,62 @@
 alter table ref_doc drop constraint UNI_REF_DOC_NAME;
 
 alter table ref modify lang_id default 1000
+
+
+  CREATE OR REPLACE  VIEW VW_NCI_FORM_FLAT_REP AS
+  select frm.item_long_nm frm_item_long_nm, frm.item_nm frm_item_nm, frm.item_id frm_item_id, frm.ver_nr frm_ver_nr, frm.item_desc  frm_item_def,
+  frmst.hdr_instr, frmst.ftr_instr,
+air.p_item_id mod_item_id, air.p_item_ver_nr mod_ver_nr, frm_mod.instr MOD_INSTR,
+ air.c_item_id de_item_id, air.c_item_ver_nr de_ver_nr, air.disp_ord quest_disp_ord,frm_mod.disp_ord mod_disp_ord, frm_mod.rep_no  mod_rep_no,
+aim.item_nm MOD_ITEM_NM, air.instr QUEST_INSTR, air.REQ_IND  QUEST_REQ_IND,
+vv.value REP_DEFLT_VAL_ENUM,
+       rep.EDIT_IND QUEST_EDIT_IND, rep.rep_seq,
+ aim.item_long_nm  mod_item_long_nm, aim.item_desc mod_item_desc,
+air.nci_pub_id QUEST_ITEM_ID,air.nci_ver_nr QUEST_VER_NR,
+de.CNTXT_NM_DN CDE_CNTXT_NM, de.item_nm CDE_ITEM_NM, de.cntxt_item_id CDE_CNTXT_ITEM_ID, de.cntxt_VER_NR CDE_CNTXT_VER_NR, de.REGSTR_STUS_NM_DN CDE_REGSTR_STUS_NM,
+de.ADMIN_STUS_NM_DN CDE_ADMIN_STUS_NM,
+air.CREAT_DT, air.CREAT_USR_ID, air.LST_UPD_USR_ID, air.FLD_DELETE, air.LST_DEL_DT, air.S2P_TRN_DT, air.LST_UPD_DT, air.item_long_nm QUEST_LONG_TXT, air.item_nm QUEST_TXT,
+rep.val REP_DEFLT_VAL
+from  nci_admin_item_rel_alt_key air, admin_item aim,
+admin_item frm, nci_admin_item_rel frm_mod , admin_item de, nci_quest_vv_rep rep, nci_form frmst, nci_quest_valid_value vv
+where  air.rel_typ_id = 63
+and aim.item_id = air.p_item_id and aim.ver_nr = air.p_item_ver_nr
+and frm.item_id = frm_mod.p_item_id and frm.ver_nr = frm_mod.p_item_ver_nr and aim.item_id = frm_mod.c_item_id and aim.ver_nr = frm_mod.c_item_ver_nr
+and frm.item_id = frmst.item_id and frm.ver_nr = frmst.ver_nr
+and frm_mod.rel_typ_id in (61,62)
+and frm.admin_item_typ_id in ( 54,55)
+and air.c_item_id = de.item_id (+)
+and air.c_item_ver_nr = de.ver_nr (+)
+and air.NCI_PUB_ID = rep.quest_pub_id 
+and air.nci_ver_nr = rep.quest_ver_nr 
+and rep.DEFLT_VAL_ID = vv.nci_pub_id (+)
+and (rep.deflt_val_id is not null or rep.val is not null)
+and nvl(air.fld_delete,0) = 0
+and nvl(frm_mod.fld_delete,0) = 0
+union
+  select frm.item_long_nm frm_item_long_nm, frm.item_nm frm_item_nm, frm.item_id frm_item_id, frm.ver_nr frm_ver_nr, frm.item_desc  frm_item_def,
+  frmst.hdr_instr, frmst.ftr_instr,
+air.p_item_id mod_item_id, air.p_item_ver_nr mod_ver_nr, frm_mod.instr MOD_INSTR,
+ air.c_item_id de_item_id, air.c_item_ver_nr de_ver_nr, air.disp_ord quest_disp_ord,frm_mod.disp_ord mod_disp_ord, frm_mod.rep_no  mod_rep_no,
+aim.item_nm MOD_ITEM_NM, air.instr QUEST_INSTR, air.REQ_IND  QUEST_REQ_IND,
+vv.value REP_DEFLT_VAL_ENUM,
+       air.EDIT_IND QUEST_EDIT_IND, 0 rep_seq,
+ aim.item_long_nm  mod_item_long_nm, aim.item_desc mod_item_desc,
+air.nci_pub_id QUEST_ITEM_ID,air.nci_ver_nr QUEST_VER_NR,
+de.CNTXT_NM_DN CDE_CNTXT_NM, de.item_nm CDE_ITEM_NM, de.cntxt_item_id CDE_CNTXT_ITEM_ID, de.cntxt_VER_NR CDE_CNTXT_VER_NR, de.REGSTR_STUS_NM_DN CDE_REGSTR_STUS_NM,
+de.ADMIN_STUS_NM_DN CDE_ADMIN_STUS_NM,
+air.CREAT_DT, air.CREAT_USR_ID, air.LST_UPD_USR_ID, air.FLD_DELETE, air.LST_DEL_DT, air.S2P_TRN_DT, air.LST_UPD_DT, air.item_long_nm QUEST_LONG_TXT, air.item_nm QUEST_TXT,
+air.deflt_val REP_DEFLT_VAL
+from  nci_admin_item_rel_alt_key air, admin_item aim,
+admin_item frm, nci_admin_item_rel frm_mod , admin_item de, nci_form frmst, nci_quest_valid_value vv
+where  air.rel_typ_id = 63
+and aim.item_id = air.p_item_id and aim.ver_nr = air.p_item_ver_nr
+and frm.item_id = frm_mod.p_item_id and frm.ver_nr = frm_mod.p_item_ver_nr and aim.item_id = frm_mod.c_item_id and aim.ver_nr = frm_mod.c_item_ver_nr
+and frm.item_id = frmst.item_id and frm.ver_nr = frmst.ver_nr
+and frm_mod.rel_typ_id in (61,62)
+and frm.admin_item_typ_id in ( 54,55)
+and (air.deflt_val_id is not null or air.deflt_val is not null)
+ and air.c_item_id = de.item_id (+)
+and air.c_item_ver_nr = de.ver_nr (+)
+and air.DEFLT_VAL_ID = vv.nci_pub_id (+);
+
