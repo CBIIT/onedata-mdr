e9f7fe985f385964f3b9f95ed4ff757ca304528f
deepalimanathara <deepali.manathara@gmail.com>
2022-06-14 14:18:34 -0400

    Update S33_BULK_UPDATE.sql

--- od-db-sprints/sprint33/S33_BULK_UPDATE.sql
+++ od-db-sprints/sprint33/S33_BULK_UPDATE.sql
@@ -4,3 +4,12 @@ update admin_item set ITEM_NM_ID_VER =  (select item_nm || '|' || ITEM_ID || '|'
 commit;
 
 alter table ADMIN_ITEM ENABLE ALL TRIGGERS;
+
+-- Tracker 1859
+
+insert into sbrext.qc_recs_ext (QR_IDSEQ,P_QC_IDSEQ,C_QC_IDSEQ,DISPLAY_ORDER,RL_NAME,CREATED_BY,DATE_CREATED,DATE_MODIFIED,MODIFIED_BY)
+select nci_11179.cmr_guid, p_val_idseq, qc_idseq, display_order,  'VALUE_INSTRUCTION', CREATED_BY, DATE_CREATED, DATE_MODIFiED,MODIFIED_BY
+from SBREXT.quest_contents_ext where QTL_NAME = 'VALUE_INSTR'
+and qc_idseq not in
+(select C_QC_IDSEQ from sbrext.QC_RECS_EXT where RL_NAME= 'VALUE_INSTRUCTION');
+commit;
