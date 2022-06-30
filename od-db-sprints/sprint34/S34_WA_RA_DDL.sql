fd992357ae4a32fe2c50737be4044d28d096d100
deepalimanathara <deepali.manathara@gmail.com>
2022-06-30 13:26:41 -0400

    Update S34_WA_RA_DDL.sql

--- od-db-sprints/sprint34/S34_WA_RA_DDL.sql
+++ od-db-sprints/sprint34/S34_WA_RA_DDL.sql
@@ -1,2 +1,6 @@
 alter table DATA_TYP add  (NCI_DTTYPE_MAP_ID integer);
 
+update data_TYP a set NCI_DTTYPE_MAP_ID = (select DTTYPE_ID from DATA_TYP where 	NCI_DTTYPE_TYP_ID = 2 and DTTYPE_NM = a.NCI_DTTYPE_MAP)
+where NCI_DTTYPE_TYP_ID = 1 and NCI_DTTYPE_MAP is not null;
+
+commit;
