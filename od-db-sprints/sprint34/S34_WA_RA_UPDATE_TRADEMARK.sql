0a63756b208198d32844a7c57997baa2108b031c
asafieva <asafieva@gmail.com>
2022-07-14 22:59:56 -0400

    DSRMWS-1257 Trademark symbols to UTF-8 Encoding

--- od-db-sprints/sprint34/S34_WA_RA_UPDATE_TRADEMARK.sql
+++ od-db-sprints/sprint34/S34_WA_RA_UPDATE_TRADEMARK.sql
@@ -1,6 +1,6 @@
 update admin_item set item_nm =  replace (item_nm, chr(49817), chr(14845090)),
 lst_upd_dt = sysdate, lst_upd_usr_id = 'ONEDATA', lst_upd_usr_id_x = 'ONEDATA',
-set CHNG_DESC_TXT =
-substrb('Trademark symbol not displayed properly in Long Name in UI is fixed on ' || to_char(sysdate, 'MON DD, YYYY HH:MI AM') || '. Updated by ONEDATA.' || DECODE(CHNG_DESC_TXT, NULL, '', ' ' || CHNG_DESC_TXT), 1, 2000)
-where admin_item_typ_id in (52, 54) and  instr(item_nm, chr(49817)) > 0;
+CHNG_DESC_TXT =
+substrb('Trademark symbol, not displayed properly in Long Name in UI, is fixed on ' || to_char(sysdate, 'MON DD, YYYY HH:MI AM') || '. Updated by ONEDATA.' || DECODE(CHNG_DESC_TXT, NULL, '', ' ' || CHNG_DESC_TXT), 1, 2000)
+where admin_item_typ_id in (52, 54) and instr(item_nm, chr(49817)) > 0;
 commit;
