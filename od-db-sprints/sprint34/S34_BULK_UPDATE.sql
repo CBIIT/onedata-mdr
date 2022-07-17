c2891aa17e3d3a86c83ef6f52563fc4bf37eee52
deepalimanathara <deepali.manathara@gmail.com>
2022-07-17 15:13:58 -0400

    Update S34_BULK_UPDATE.sql

--- od-db-sprints/sprint34/S34_BULK_UPDATE.sql
+++ od-db-sprints/sprint34/S34_BULK_UPDATE.sql
@@ -1,3 +1,4 @@
+-- Tracker 1704
 alter table ADMIN_ITEM DISABLE ALL TRIGGERS;
 
 update admin_item set ITEM_NM_ID_VER =  (select item_nm || '|' || ITEM_ID || '|' || ver_nr || '|' || obj_key_desc  from obj_key where obj_Key_id = admin_item.admin_item_typ_id);
@@ -5,6 +6,7 @@ commit;
 
 alter table ADMIN_ITEM ENABLE ALL TRIGGERS;
 
+-- Tracker 1843
 insert into sbr.ac_registrations (ar_idseq, ac_idseq, registration_status, date_created, created_by)
 select nci_11179.cmr_guid,nci_idseq, nci_stus, ai.creat_dt, ai.creat_usr_id from stus_mstr, admin_item ai where admin_item_typ_id <> 8 and
 stus_typ_id = 1 and stus_id = ai.regstr_stus_id
@@ -12,6 +14,7 @@ and ai.regstr_stus_id is not null  and nci_idseq not in (select ac_idseq from sb
 commit;
 
 
+-- Tracker 1843
 update sbr.ac_registrations r set Registration_Status = (select nci_stus from stus_mstr, admin_item ai where admin_item_typ_id <> 8 and
 stus_typ_id = 1 and stus_id = ai.regstr_stus_id
 and ai.regstr_stus_id is not null and r.ac_idseq = ai.nci_idseq)
