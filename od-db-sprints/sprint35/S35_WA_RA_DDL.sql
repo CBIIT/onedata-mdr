05eca937e8a6a0915f4d01dc219397ac2a5498f2
deepalimanathara <deepali.manathara@gmail.com>
2022-07-29 15:33:16 -0400

    Update S35_WA_RA_DDL.sql

--- od-db-sprints/sprint35/S35_WA_RA_DDL.sql
+++ od-db-sprints/sprint35/S35_WA_RA_DDL.sql
@@ -5,12 +5,12 @@ alter table NCI_STG_AI_CNCPT_CREAT add (CURATED_NM varchar2(2000), CURATED_DEF v
 -- Tracker
 alter table NCI_USR_CART add (CART_NM varchar2(255));
 
- CREATE OR REPLACE  VIEW VW_LIST_USR_CART_NM AS
+ CREATE OR REPLACE  VIEW VW_LIST_USR_CART_NM  CREATE OR REPLACE  VIEW VW_LIST_USR_CART_NM AS
   SELECT distinct CART_NM, CART_NM CART_NM_SPEC, CNTCT_SECU_ID,
            user CREAT_USR_ID,
             user LST_UPD_USR_ID,
-            '0' FLD_DELETE,
-            null LST_DEL_DT,
+            0 FLD_DELETE,
+           sysdate LST_DEL_DT,
            sysdate S2P_TRN_DT,
            sysdate LST_UPD_DT,
            sysdate CREAT_DT
