4067217ed7b948a9c435e5d84957994c90cfdbb1
deepalimanathara <deepali.manathara@gmail.com>
2022-08-05 14:25:31 -0400

    Update S35_SBR.sql

--- od-db-sprints/sprint35/S35_SBR.sql
+++ od-db-sprints/sprint35/S35_SBR.sql
@@ -6,3 +6,25 @@ EXEC DBMS_STATS.GATHER_TABLE_STATS ('SBREXT', 'QC_RECS_EXT');
 EXEC DBMS_STATS.GATHER_TABLE_STATS ('SBREXT', 'VALID_VALUES_ATT_EXT');
 
 EXEC DBMS_STATS.GATHER_TABLE_STATS ('SBR', 'ADMINISTERED_COMPONENTS');
+
+
+
+-- Tracker 1854
+
+delete from administered_components where ac_idseq in (select qc_idseq from sbrext.quest_contents_ext where qtl_name in ('VALID_VALUE') 
+and qc_idseq not in (select nci_idseq from onedata_wa.nci_quest_valid_value where nvl(fld_delete,0) = 0));
+
+commit;
+
+delete from administered_components where ac_idseq in (select qc_idseq from sbrext.quest_contents_ext where qtl_name in ('VALUE_INSTR') 
+and p_val_idseq not in (select nci_idseq from onedata_wa.nci_quest_valid_value) );
+commit;
+
+delete from sbrext.quest_contents_ext where qtl_name in ('VALUE_INSTR') 
+and p_val_idseq not in (select nci_idseq from onedata_wa.nci_quest_valid_value );
+
+commit;
+
+delete from sbrext.quest_contents_ext where qtl_name in ('VALID_VALUE') 
+and qc_idseq not in (select nci_idseq from onedata_wa.nci_quest_valid_value );
+commit;
