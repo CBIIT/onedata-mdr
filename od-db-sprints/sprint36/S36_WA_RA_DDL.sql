d5ca33a185676ad2d84295e95a00f2043611dc5e
deepalimanathara <deepali.manathara@gmail.com>
2022-08-23 12:10:47 -0400

    Create S36_WA_RA_DDL.sql

---
+++ od-db-sprints/sprint36/S36_WA_RA_DDL.sql
@@ -0,0 +1,27 @@
+-- Data Audit table
+
+create table NCI_DATA_AUDT 
+( DA_ID number not null primary key,
+ITEM_ID number not null,
+  VER_NR number not null,
+  ADMIN_ITEM_TYP_ID int not null,
+  ACTION_TYP  char(1) not null,
+  LVL varchar2(100) not null,
+  LVL_PK  varchar2(255) null,
+  ATTR_NM  varchar2(255) null,
+  FROM_VAL  varchar2(4000) null,
+  TO_VAL varchar2(4000) null,
+  CREAT_USR_ID_AUDT varchar2(255) null,
+  LST_UPD_USR_ID_AUDT varchar2(255) null,
+  CREAT_DT_AUDT date null,
+  LST_UPD_DT_AUDT date null,
+	"CREAT_DT" DATE DEFAULT sysdate, 
+	"CREAT_USR_ID" VARCHAR2(50 BYTE)  DEFAULT user, 
+	"LST_UPD_USR_ID" VARCHAR2(50 BYTE)  DEFAULT user, 
+	"FLD_DELETE" NUMBER(1,0) DEFAULT 0, 
+	"LST_DEL_DT" DATE DEFAULT sysdate, 
+	"S2P_TRN_DT" DATE DEFAULT sysdate, 
+	"LST_UPD_DT" DATE DEFAULT sysdate);
+  
+  
+  
