65aeccea1f12b783e551e910724cd93bc444532f
deepalimanathara <deepali.manathara@gmail.com>
2022-08-29 20:07:02 -0400

    Update S36_WA.sql

--- od-db-sprints/sprint36/S36_WA.sql
+++ od-db-sprints/sprint36/S36_WA.sql
@@ -38,6 +38,9 @@ commit;
 
 alter table admin_item disable all trigggers;
 
-update ITEM_RPT_URL set = PARAM_VAL || '/invoke/FormDownload/printerFriendly?item_id=' || item_id || '&version=' || ver_nr || '\Click_To_Download'
-from NCI_MDR_CNTRL where PARAM_NM = 'DOWNLOAD_HOST';
+update ADMIN_ITEM set ITEM_RPT_URL = (select PARAM_VAL || '/invoke/FormDownload/printerFriendly?item_id=' || item_id ||  chr(38) || 'version=' || ver_nr || '\Click_To_Download'
+from NCI_MDR_CNTRL where PARAM_NM = 'DOWNLOAD_HOST') where ADMIN_ITEM_TYP_ID = 54;
 commit;
+
+alter table admin_item enable all trigggers;
+
