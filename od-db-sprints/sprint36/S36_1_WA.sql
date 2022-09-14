c47c95c3947734e4ec654967c912e39400df5442
deepalimanathara <deepali.manathara@gmail.com>
2022-09-14 19:26:03 -0400

    Update S36_1_WA.sql

--- od-db-sprints/sprint36/S36_1_WA.sql
+++ od-db-sprints/sprint36/S36_1_WA.sql
@@ -1,6 +1,7 @@
-CREATE OR TRIGGER OD_TR_STG_ALT_NMS  BEFORE INSERT  on NCI_STG_ALT_NMS  for each row
+CREATE OR replace TRIGGER OD_TR_STG_ALT_NMS  BEFORE INSERT  on NCI_STG_ALT_NMS  for each row
      BEGIN    IF (:NEW.STG_AI_ID = -1  or :NEW.STG_AI_ID is null)  THEN select od_seq_CDE_IMPORT.nextval
  into :new.STG_AI_ID  from  dual ; 
  END IF;
  :new.DT_LAST_MODIFIED := TO_CHAR(SYSDATE, 'MM/DD/YY HH24:MI:SS');
  END;
+/
