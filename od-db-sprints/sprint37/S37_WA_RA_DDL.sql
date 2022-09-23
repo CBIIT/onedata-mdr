c2e2d702a35c90433cd1e1a5e1b343287c703727
deepalimanathara <deepali.manathara@gmail.com>
2022-09-23 13:10:50 -0400

    Update S37_WA_RA_DDL.sql

--- od-db-sprints/sprint37/S37_WA_RA_DDL.sql
+++ od-db-sprints/sprint37/S37_WA_RA_DDL.sql
@@ -1,4 +1,4 @@
---alter table nci_admin_item_rel add (CPY_FRM_ITEM_ID number, CPY_FRM_VER_NR number(4,2);
+alter table nci_admin_item_rel add (CPY_MOD_ITEM_ID number, CPY_MOD_VER_NR number(4,2));
 
 --Tracker 2129
 alter table NCI_ADMIN_ITEM_REL_ALT_KEY modify (EDIT_IND default 0, REQ_IND default 0);
