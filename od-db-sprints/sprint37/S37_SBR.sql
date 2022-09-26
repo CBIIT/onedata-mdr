bbc90f9c305d2bd30e3cdbca7674df883ca167be
deepalimanathara <deepali.manathara@gmail.com>
2022-09-26 14:38:33 -0400

    Update S37_SBR.sql

--- od-db-sprints/sprint37/S37_SBR.sql
+++ od-db-sprints/sprint37/S37_SBR.sql
@@ -3,5 +3,5 @@ delete from sbr.administered_components where ac_idseq in (select qc_idseq from 
 and qtl_name in ('QUESTION_INSTR', 'VALUE_INSTR','MODULE_INSTR','FOOTER', 'FORM_INSTR') );
 commit;
 delete from sbrext.quest_contents_ext where (preferred_definition is null or preferred_definition = ' ')
-and qtl_name in ('QUESTION_INSTR', 'VALUE_INSTR','MODULE_INSTR','FOOTER', 'FORM_INSTR') );
+and qtl_name in ('QUESTION_INSTR', 'VALUE_INSTR','MODULE_INSTR','FOOTER', 'FORM_INSTR') ;
 commit;
