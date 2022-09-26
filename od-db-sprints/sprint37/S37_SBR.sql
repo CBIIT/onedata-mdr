c80bfa358e4e2d3e678ec52df5b5b985de0c748e
deepalimanathara <deepali.manathara@gmail.com>
2022-09-26 14:36:56 -0400

    Create S37_SBR.sql

---
+++ od-db-sprints/sprint37/S37_SBR.sql
@@ -0,0 +1,7 @@
+
+delete from sbr.administered_components where ac_idseq in (select qc_idseq from sbrext.quest_contents_ext where (preferred_definition is null or preferred_definition = ' ')
+and qtl_name in ('QUESTION_INSTR', 'VALUE_INSTR','MODULE_INSTR','FOOTER', 'FORM_INSTR') );
+commit;
+delete from sbrext.quest_contents_ext where (preferred_definition is null or preferred_definition = ' ')
+and qtl_name in ('QUESTION_INSTR', 'VALUE_INSTR','MODULE_INSTR','FOOTER', 'FORM_INSTR') )
+commit;
