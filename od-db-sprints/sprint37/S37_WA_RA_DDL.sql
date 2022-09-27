a8d72da620fad567ff953c374db17149436129b5
deepalimanathara <deepali.manathara@gmail.com>
2022-09-27 13:14:40 -0400

    Update S37_WA_RA_DDL.sql

--- od-db-sprints/sprint37/S37_WA_RA_DDL.sql
+++ od-db-sprints/sprint37/S37_WA_RA_DDL.sql
@@ -21,3 +21,10 @@ FROM ADMIN_ITEM
 insert into obj_key (obj_typ_id, obj_key_Desc, obj_key_id, obj_key_def, NCI_CD) values (31, 'Printer Friendly Form', 114, 'Legacy Prior CDE Excel', 
 											'Legacy Prior CDE Excel');
 commit;
+
+-- Tracker 2147
+alter table NCI_STG_CDE_CREAT add PROCESS_DT_CHAR varchar2(20);
+
+update NCI_STG_CDE_CREAT set PROCESS_DT_CHAR = TO_CHAR(SYSDATE, 'MM/DD/YY HH24:MI:SS') where PROCESS_DT is not null;
+commit;
+
