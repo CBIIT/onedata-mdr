634fb2a843fd5191462744e0547d733cf805b622
deepalimanathara <deepali.manathara@gmail.com>
2022-10-04 17:18:56 -0400

    Update S38_WA_RA_DDL.sql

--- od-db-sprints/sprint38/S38_WA_RA_DDL.sql
+++ od-db-sprints/sprint38/S38_WA_RA_DDL.sql
@@ -44,6 +44,22 @@ AND AIR.C_ITEM_ID = DE.ITEM_ID
 AND AIR.C_ITEM_VER_NR = DE.VER_NR
 and FRM_MOD.P_ITEM_ID = -1;
 
+
+-- Stand-alone module
+
+
+  CREATE OR REPLACE  VIEW VW_NCI_FORM_MODULE AS
+  select air.p_item_id, air.p_item_ver_nr, ai.item_id, ai.ver_nr, ai.item_nm, ai.item_nm FORM_NM, ai.item_desc,
+'MODULE' admin_item_typ_nm,air.INSTR,
+ai.CREAT_DT, ai.CREAT_USR_ID, ai.LST_UPD_USR_ID, air.FLD_DELETE, ai.LST_DEL_DT, ai.S2P_TRN_DT, ai.LST_UPD_DT, air.disp_ord, air.rep_no,
+ai.regstr_stus_id, ai.admin_stus_id, ai.cntxt_item_id, ai.cntxt_ver_nr, ai.item_long_nm, air.CPY_MOD_ITEM_ID,
+air.CPY_MOD_VER_NR
+from admin_item ai, nci_admin_item_rel air where ai.item_id = air.c_item_id and ai.ver_nr = air.c_item_ver_nr and air.rel_typ_id in (61,62);
+
+
+  GRANT SELECT ON "ONEDATA_WA"."VW_NCI_FORM_MODULE" TO "ONEDATA_RO";
+
+
 -- New Form upload
 
 create table NCI_STG_FORM_IMPORT 
