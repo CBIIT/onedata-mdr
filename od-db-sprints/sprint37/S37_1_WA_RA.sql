4276bb70d91dcb5f43685a69d5916d079d9f005e
deepalimanathara <deepali.manathara@gmail.com>
2022-10-05 13:05:09 -0400

    Create S37_1_WA_RA.sql

---
+++ od-db-sprints/sprint37/S37_1_WA_RA.sql
@@ -0,0 +1,7 @@
+
+delete from obj_key where obj_key_id = 114;
+commit;
+
+insert into obj_key (obj_typ_id, obj_key_Desc, obj_key_id, obj_key_def, NCI_CD) values (31, 'Legacy Prior CDE Excel', 114, 'Legacy Prior CDE Excel', 
+											'Legacy Prior CDE Excel');
+commit;
