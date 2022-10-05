9481ec1387fd3a988cb113a8dbf23e86c3e93e52
deepalimanathara <deepali.manathara@gmail.com>
2022-10-05 13:04:16 -0400

    Update S37_WA_RA_DDL.sql

--- od-db-sprints/sprint37/S37_WA_RA_DDL.sql
+++ od-db-sprints/sprint37/S37_WA_RA_DDL.sql
@@ -20,12 +20,6 @@ FROM ADMIN_ITEM
        ADMIN_STUS_NM_DN = 'RELEASED';
 
 
-delete from obj_key where obj_key_id = 114;
-commit;
-
-insert into obj_key (obj_typ_id, obj_key_Desc, obj_key_id, obj_key_def, NCI_CD) values (31, 'Legacy Prior CDE Excel', 114, 'Legacy Prior CDE Excel', 
-											'Legacy Prior CDE Excel');
-commit;
 
 -- Tracker 2147
 alter table NCI_STG_CDE_CREAT add PROCESS_DT_CHAR varchar2(20);
