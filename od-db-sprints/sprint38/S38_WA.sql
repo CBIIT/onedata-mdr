81b425a55eab7024c9804ed282ea91b5ed135998
deepalimanathara <deepali.manathara@gmail.com>
2022-10-08 11:47:50 -0400

    Update S38_WA.sql

--- od-db-sprints/sprint38/S38_WA.sql
+++ od-db-sprints/sprint38/S38_WA.sql
@@ -1,10 +1,21 @@
+
+   CREATE SEQUENCE  OD_SEQ_FORM_IMPORT  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 1 CACHE 20 NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+
 create or replace TRIGGER OD_TR_FORM_IMPORT  BEFORE INSERT  on NCI_STG_FORM_IMPORT  for each row
-     BEGIN    IF (:NEW.STG_ID = -1  or :NEW.STG_ID is null)  THEN select od_seq_CDE_IMPORT.nextval
- into :new.STG_ID  from  dual ;   END IF;
+     BEGIN    IF (:NEW.FORM_IMP_ID = -1  or :NEW.FORM_IMP_ID is null)  THEN select od_seq_FORM_IMPORT.nextval
+ into :new.FORM_IMP_ID  from  dual ;   END IF;
  END;
 /
+
+
+create or replace TRIGGER TR_FORM_QUEST_IMPORT  BEFORE INSERT  on NCI_STG_FORM_QUEST_IMPORT  for each row
+     BEGIN    IF (:NEW.QUEST_IMP_ID = -1  or :NEW.QUEST_IMP_ID is null)  THEN select od_seq_FORM_IMPORT.nextval
+ into :new.QUEST_IMP_ID  from  dual ;   END IF;
+ END;
+/
+
 create or replace TRIGGER OD_TR_FORM_VV_IMPORT  BEFORE INSERT  on NCI_STG_FORM_VV_IMPORT  for each row
-     BEGIN    IF (:NEW.STG_VAL_ID = -1  or :NEW.STG_VAL_ID is null)  THEN select od_seq_CDE_IMPORT.nextval
- into :new.STG_VAL_ID  from  dual ;   END IF;
+     BEGIN    IF (:NEW.VAL_IMP_ID = -1  or :NEW.VAL_IMP_ID is null)  THEN select od_seq_FORM_IMPORT.nextval
+ into :new.VAL_IMP_ID  from  dual ;   END IF;
 END;
 /
