be60cc68357eb668a8a831f2857f1ae473d5fa5a
deepalimanathara <deepali.manathara@gmail.com>
2022-10-09 14:06:43 -0400

    Update S38_WA_RA_DDL.sql

--- od-db-sprints/sprint38/S38_WA_RA_DDL.sql
+++ od-db-sprints/sprint38/S38_WA_RA_DDL.sql
@@ -123,3 +123,10 @@ VV_ALT_DEF	varchar2(4000),
 	"LST_DEL_DT" DATE DEFAULT sysdate, 
 	"S2P_TRN_DT" DATE DEFAULT sysdate, 
 	"LST_UPD_DT" DATE DEFAULT sysdate);
+
+
+-- Form Match
+alter table NCI_DS_RSLT_DTL add user_id varchar2(255);
+alter table nci_ds_rslt add mtch_typ  varchar2(50);
+
+
