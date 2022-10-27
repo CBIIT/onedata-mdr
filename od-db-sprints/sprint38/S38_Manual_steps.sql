07d23c92a2884053d83ba275c47253b8854e3a95
deepalimanathara <deepali.manathara@gmail.com>
2022-10-27 11:14:28 -0400

    Create S38_Manual_steps.sql

---
+++ od-db-sprints/sprint38/S38_Manual_steps.sql
@@ -0,0 +1,5 @@
+
+-- Manual steps
+-- Project name - Tracker 2223
+-- There is one change to be made in S38_WA_RA_DDL.sql for each environment. Insert into NCI_MDR_CNTRL
+-- Delete Guest User filter - Tracker 2206
