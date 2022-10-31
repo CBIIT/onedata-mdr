ad6d2a556ad786a2bae6314b734ba2707ad0ee56
deepalimanathara <deepali.manathara@gmail.com>
2022-10-31 00:05:50 -0400

    Update S39_WA_RA_DDL.sql

--- od-db-sprints/sprint39/S39_WA_RA_DDL.sql
+++ od-db-sprints/sprint39/S39_WA_RA_DDL.sql
@@ -2,3 +2,14 @@ alter table CNCPT add (PRNT_ITEM_ID number, PRNT_ITEM_VER_NR number);
 
 alter table NCI_USR_CART add RETAIN_IND number(1);
 
+
+  CREATE OR REPLACE VIEW VW_LIST_USR_CART_NM AS
+  SELECT distinct CART_NM, CART_NM CART_NM_SPEC, CNTCT_SECU_ID, CART_NM || ' : ' || CNTCT_SECU_ID CART_USR_NM, 0 RETAIN_IND, 
+           user CREAT_USR_ID,
+            user LST_UPD_USR_ID,
+            0 FLD_DELETE,
+           sysdate LST_DEL_DT,
+           sysdate S2P_TRN_DT,
+           sysdate LST_UPD_DT,
+           sysdate CREAT_DT
+           from NCI_USR_CART where nvl(fld_delete,0)  =0;
