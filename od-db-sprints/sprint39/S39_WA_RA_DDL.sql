c21304b3fa3b3ea9202d2528fad14f20c133124a
deepalimanathara <deepali.manathara@gmail.com>
2022-11-03 23:51:47 -0400

    Update S39_WA_RA_DDL.sql

--- od-db-sprints/sprint39/S39_WA_RA_DDL.sql
+++ od-db-sprints/sprint39/S39_WA_RA_DDL.sql
@@ -25,10 +25,10 @@ commit;
            from NCI_USR_CART where nvl(fld_delete,0)  =0;
 	   
 create table NCI_CNCPT_REL
-   (	"PRNT_CNCPT_VER_NR" NUMBER(4,2) not null, 
-	"PRNT_CNCPT_ITEM_ID" NUMBER not null, 
-	"CHLD_CNCPT_VER_NR" NUMBER(4,2) not null, 
-	"CHLD_CNCPT_ITEM_ID" NUMBER not null, 
+   (	"P_ITEM_VER_NR" NUMBER(4,2) not null, 
+	"P_ITEM_ID" NUMBER not null, 
+	"C_ITEM_VER_NR" NUMBER(4,2) not null, 
+	"C_ITEM_ID" NUMBER not null, 
 	"CREAT_DT" DATE DEFAULT sysdate, 
 	"CREAT_USR_ID" VARCHAR2(50 BYTE) COLLATE "USING_NLS_COMP" DEFAULT user, 
 	"LST_UPD_USR_ID" VARCHAR2(50 BYTE) COLLATE "USING_NLS_COMP" DEFAULT user, 
@@ -38,5 +38,5 @@ create table NCI_CNCPT_REL
 	"LST_UPD_DT" DATE DEFAULT sysdate, 
 	"REL_TYP_ID" INTEGER not null,
 	"DISP_ORD" INTEGER,
-    primary key( PRNT_CNCPT_ITEM_ID, PRNT_CNCPT_VER_NR,CHLD_CNCPT_ITEM_ID, CHLD_cNCPT_VER_NR, REL_TYP_ID));
+    primary key( P_ITEM_ID, P_ITEM_VER_NR,C_ITEM_ID, C_ITEM_VER_NR, REL_TYP_ID));
    
