2446981ed97380b08818bff5d0be6ae12c3d5e33
deepalimanathara <deepali.manathara@gmail.com>
2022-11-28 11:32:26 -0500

    Update S39_SBR.sql

--- od-db-sprints/sprint39/S39_SBR.sql
+++ od-db-sprints/sprint39/S39_SBR.sql
@@ -1,9 +1,11 @@
 delete from sbr.administered_components where ac_idseq in (
-select  qc_idseq from sbrext.quest_contents_ext where date_created >= sysdate - 60 and qtl_name = 'VALID_VALUE' and 
+select  qc_idseq from sbrext.quest_contents_ext where date_created >= sysdate - (sysdate -  
+       TO_DATE('2022-05-01', 'YYYY-MM-DD')) and qtl_name = 'VALID_VALUE' and 
 qc_idseq not in (select nci_idseq from onedata_wa.nci_quest_Valid_Value));
 commit;
 delete from sbrext.quest_contents_ext where qc_idseq in (
-select  qc_idseq from sbrext.quest_contents_ext where date_created >= sysdate - 60 and qtl_name = 'VALID_VALUE' and 
+select  qc_idseq from sbrext.quest_contents_ext where date_created >= sysdate - (sysdate -  
+       TO_DATE('2022-05-01', 'YYYY-MM-DD')) and qtl_name = 'VALID_VALUE' and 
 qc_idseq not in (select nci_idseq from onedata_wa.nci_quest_Valid_Value));
 commit;
 
