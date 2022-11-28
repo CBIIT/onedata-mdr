6eac16464dd0927380b45341d1bd0bf3e403cf31
deepalimanathara <deepali.manathara@gmail.com>
2022-11-28 11:53:06 -0500

    Update S39_SBR.sql

--- od-db-sprints/sprint39/S39_SBR.sql
+++ od-db-sprints/sprint39/S39_SBR.sql
@@ -1,11 +1,10 @@
 delete from sbr.administered_components where ac_idseq in (
-select  qc_idseq from sbrext.quest_contents_ext where date_created >= sysdate - (sysdate -  
-       TO_DATE('2022-05-01', 'YYYY-MM-DD')) and qtl_name = 'VALID_VALUE' and 
-qc_idseq not in (select nci_idseq from onedata_wa.nci_quest_Valid_Value));
+select  qc_idseq from sbrext.quest_contents_ext where qtl_name = 'VALID_VALUE' and
+qc_idseq not in (select nci_idseq from onedata_wa.nci_quest_Valid_Value)
+and P_QST_IDSEQ in (select nci_idseq from onedata_wa.nci_admin_item_rel_alt_key where lst_upd_dt >= to_Date('05/01/2022','mm/dd/yyyy')));
 commit;
 delete from sbrext.quest_contents_ext where qc_idseq in (
-select  qc_idseq from sbrext.quest_contents_ext where date_created >= sysdate - (sysdate -  
-       TO_DATE('2022-05-01', 'YYYY-MM-DD')) and qtl_name = 'VALID_VALUE' and 
-qc_idseq not in (select nci_idseq from onedata_wa.nci_quest_Valid_Value));
+select  qc_idseq from sbrext.quest_contents_ext where qtl_name = 'VALID_VALUE' and 
+qc_idseq not in (select nci_idseq from onedata_wa.nci_quest_Valid_Value )
+and P_QST_IDSEQ in (select nci_idseq from onedata_wa.nci_admin_item_rel_alt_key where lst_upd_dt >= to_Date('05/01/2022','mm/dd/yyyy')));
 commit;
-
