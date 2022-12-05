4cfa005377cb99c9b5f4fcfc386761ac7a4fe80d
deepalimanathara <deepali.manathara@gmail.com>
2022-12-05 13:10:21 -0500

    Update S40_WA_RA_DDL.sql

--- od-db-sprints/sprint40/S40_WA_RA_DDL.sql
+++ od-db-sprints/sprint40/S40_WA_RA_DDL.sql
@@ -5,8 +5,6 @@ truncate table NCI_STG_FORM_VV_IMPORT;
 alter table NCI_STG_FORM_IMPORT
 add (BTCH_USR_NM varchar2(100) not null, BTCH_NM varchar2(100) not null, CREATED_FORM_ITEM_ID number);
 
-alter table NCI_STG_FORM_IMPORT
-add PROCESS_DT_CHAR  varchar2(20);
 
 alter table NCI_STG_FORM_QUEST_IMPORT
 add (BTCH_SEQ_NBR number not null);
