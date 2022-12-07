28f3e7100ef6f83286477a476952509eb6579740
deepalimanathara <deepali.manathara@gmail.com>
2022-12-07 11:02:47 -0500

    Update S40_WA.sql

--- od-db-sprints/sprint40/S40_WA.sql
+++ od-db-sprints/sprint40/S40_WA.sql
@@ -6,3 +6,30 @@ BEGIN
 :new.LST_UPD_DT := sysdate;
 END;
 /
+
+
+alter table admin_item disable all triggers;
+
+update admin_item set MTCH_TERM = regexp_replace(upper(item_nm),'\(|\)|\;|\-|\_|\||\:|\$|\[|\]|\''|\"|\%|\*|\&|\#|\@|\{|\}',''),
+MTCH_TERM_ADV = regexp_replace(upper(item_nm),'\(|\)|\;|\-|\_|\||\:|\$|\[|\]|\''|\"|\%|\*|\&|\#|\@|\{|\}','');
+commit;
+
+alter table admin_item enable all triggers;
+
+
+alter table alt_nms disable all triggers;
+
+update alt_nms set MTCH_TERM = regexp_replace(upper(nm_desc),'\(|\)|\;|\-|\_|\||\:|\$|\[|\]|\''|\"|\%|\*|\&|\#|\@|\{|\}',''),
+MTCH_TERM_ADV = regexp_replace(upper(nm_desc),'\(|\)|\;|\-|\_|\||\:|\$|\[|\]|\''|\"|\%|\*|\&|\#|\@|\{|\}','');
+commit;
+
+alter table alt_nms enable all triggers;
+
+
+alter table ref disable all triggers;
+
+update ref set MTCH_TERM = regexp_replace(upper(ref_desc),'\(|\)|\;|\-|\_|\||\:|\$|\[|\]|\''|\"|\%|\*|\&|\#|\@|\{|\}',''),
+MTCH_TERM_ADV = regexp_replace(upper(ref_desc),'\(|\)|\;|\-|\_|\||\:|\$|\[|\]|\''|\"|\%|\*|\&|\#|\@|\{|\}','');
+commit;
+
+alter table ref enable all triggers;
