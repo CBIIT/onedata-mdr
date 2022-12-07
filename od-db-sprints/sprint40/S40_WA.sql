cd0df1d82d2d20aa48212bc4bc60998a5801cb9a
deepalimanathara <deepali.manathara@gmail.com>
2022-12-07 11:05:33 -0500

    Update S40_WA.sql

--- od-db-sprints/sprint40/S40_WA.sql
+++ od-db-sprints/sprint40/S40_WA.sql
@@ -33,3 +33,40 @@ MTCH_TERM_ADV = regexp_replace(upper(ref_desc),'\(|\)|\;|\-|\_|\||\:|\$|\[|\]|\'
 commit;
 
 alter table ref enable all triggers;
+                                                 
+                                                 
+create or replace TRIGGER TR_AI_MTCH
+  BEFORE UPDATE or INSERT ON ADMIN_ITEM
+  FOR EACH ROW
+  BEGIN
+
+:new.MTCH_TERM := regexp_replace(upper(:new.item_nm),'\(|\)|\;|\-|\_|\||\:|\$|\[|\]|\''|\"|\%|\*|\&|\#|\@|\{|\}','');
+:new.MTCH_TERM_ADV := regexp_replace(upper(:new.item_nm),'\(|\)|\;|\-|\_|\||\:|\$|\[|\]|\''|\"|\%|\*|\&|\#|\@|\{|\}','');
+
+
+END;
+/
+
+
+CREATE OR REPLACE TRIGGER TR_ALT_NMS_BEFORE
+  BEFORE INSERT OR UPDATE
+  on ALT_NMS
+  for each row
+BEGIN
+   
+:new.MTCH_TERM := regexp_replace(upper(:new.nm_desc),'\(|\)|\;|\-|\_|\||\:|\$|\[|\]|\''|\"|\%|\*|\&|\#|\@|\{|\}','');
+:new.MTCH_TERM_ADV := regexp_replace(upper(:new.nm_desc),'\(|\)|\;|\-|\_|\||\:|\$|\[|\]|\''|\"|\%|\*|\&|\#|\@|\{|\}','');  
+END;
+/
+
+CREATE OR REPLACE TRIGGER TR_REF_BEFORE
+  BEFORE INSERT OR UPDATE
+  on REF
+  for each row
+BEGIN
+
+:new.MTCH_TERM := regexp_replace(upper(:new.ref_desc),'\(|\)|\;|\-|\_|\||\:|\$|\[|\]|\''|\"|\%|\*|\&|\#|\@|\{|\}','');
+:new.MTCH_TERM_ADV := regexp_replace(upper(:new.ref_desc),'\(|\)|\;|\-|\_|\||\:|\$|\[|\]|\''|\"|\%|\*|\&|\#|\@|\{|\}','');  
+END;
+/
+                                                 
