a2184795e96c373f2373087bdff2fb5d03046047
deepalimanathara <deepali.manathara@gmail.com>
2022-12-13 14:28:33 -0500

    Update S40_WA_RA_DDL.sql

--- od-db-sprints/sprint40/S40_WA_RA_DDL.sql
+++ od-db-sprints/sprint40/S40_WA_RA_DDL.sql
@@ -59,3 +59,23 @@ alter table NCI_CNCPT_REL add Primary KEY  (P_ITEM_ID, P_ITEM_VER_NR, C_ITEM_ID,
 insert into obj_key (obj_typ_id, obj_key_Desc, obj_key_id, obj_key_def, NCI_CD) values (17, 'Subset Relationship', 69, 'Subset Relationship', 
 											'Subset Relationship');
 commit;
+
+-- Add audt_desc column
+  CREATE OR REPLACE  VIEW VW_NCI_DATA_AUDT_FORM AS
+  select "DA_ID","ITEM_ID","VER_NR","ADMIN_ITEM_TYP_ID", ACTION_TYP,
+  "LVL","LVL_PK", ATTR_NM_STD ATTR_NM,"FROM_VAL","TO_VAL","CREAT_USR_ID_AUDT",LVL_1_ITEM_ID , LVL_1_VER_NR , LVL_1_ITEM_NM,
+  LVL_2_ITEM_ID , LVL_2_VER_NR , LVL_2_ITEM_NM,LVL_3_ITEM_ID , LVL_3_VER_NR , LVL_3_ITEM_NM,
+LVL_1_DISP_ORD ,LVL_2_DISP_ORD,LVL_3_DISP_ORD ,
+  "LST_UPD_USR_ID_AUDT","CREAT_DT_AUDT","LST_UPD_DT_AUDT","CREAT_DT","CREAT_USR_ID","LST_UPD_USR_ID","FLD_DELETE","LST_DEL_DT","S2P_TRN_DT","LST_UPD_DT","AUDT_CMNTS",
+  AUDT_DESC
+  from NCI_DATA_AUDT where (((item_id, ver_nr) in (Select item_id, ver_nr from nci_form)) or action_typ ='M');
+
+
+  CREATE OR REPLACE  VIEW VW_NCI_DATA_AUDT AS
+  select "DA_ID","ITEM_ID","VER_NR","ADMIN_ITEM_TYP_ID",ACTION_TYP,
+  "LVL","LVL_PK", ATTR_NM_STD ATTR_NM,"FROM_VAL","TO_VAL","CREAT_USR_ID_AUDT",
+  "LST_UPD_USR_ID_AUDT","CREAT_DT_AUDT","LST_UPD_DT_AUDT","CREAT_DT","CREAT_USR_ID","LST_UPD_USR_ID","FLD_DELETE","LST_DEL_DT","S2P_TRN_DT","LST_UPD_DT","AUDT_CMNTS",
+  AUDT_DESC from NCI_DATA_AUDT;
+
+
+
