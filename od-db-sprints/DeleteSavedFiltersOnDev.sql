8dea3c3ce2eb3d0a7a73dd87d240c3b36eb65d11
asafieva <asafieva@gmail.com>
2022-12-22 11:46:33 -0500

    Added a comment to use on DEV only

--- od-db-sprints/DeleteSavedFiltersOnDev.sql
+++ od-db-sprints/DeleteSavedFiltersOnDev.sql
@@ -1,4 +1,5 @@
-
+-- caution: this script when used "as is" deletes all search filters, and to be used on DEV tier only.
+-- to delete specific filters modify by adding constraints.
 delete from od_md_objrel where obj_depn_id in
 (select obj_id from od_md_obj where obj_typ_id = 71);
 commit;
