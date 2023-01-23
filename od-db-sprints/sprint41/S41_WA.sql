c182f781213021104f21562b083c1ba905c6a07f
deepalimanathara <deepali.manathara@gmail.com>
2023-01-23 16:09:44 -0500

    Update S41_WA.sql

--- od-db-sprints/sprint41/S41_WA.sql
+++ od-db-sprints/sprint41/S41_WA.sql
@@ -2,3 +2,8 @@
        insert into nci_mdr_cntrl (id, PARAM_NM, PARAM_VAL)
 values (12, 'MEDDRA', 'V 25.1');
 commit;
+
+
+
+insert into obj_key (OBJ_KEY_DESC, OBJ_TYP_ID, OBJ_KEY_DEF, OBJ_KEY_CMNTS, NCI_CD) values ('X',36,'Manual Insert','Manual Insert','X' );
+commit;
