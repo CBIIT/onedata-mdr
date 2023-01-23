059e20495d4c6d5b24c8c8e3e1db1f9233fdfb6b
asafieva <asafieva@gmail.com>
2023-01-23 13:41:45 -0500

    DSRMWS-2343 Lower tier links updates after refresh from Prod script

---
+++ od-db-sprints/UpdateDeepLinksLowerTier.sql
@@ -0,0 +1,18 @@
+-- DSRMWS-2343 After data refresh from Pro to lower tier, update Deep Links and Printer Friendly Links
+alter table admin_item disable all triggers;
+
+update ADMIN_ITEM set ITEM_DEEP_LINK = (Select param_val || '/CO/CDEDD?filter=CDEDD.ITEM_ID=' || item_id ||  '%20and%20ver_nr=' || ver_nr
+from NCI_MDR_CNTRL where PARAM_NM = 'DEEP_LINK')
+where admin_item_typ_id = 4 ;
+commit;
+
+update ADMIN_ITEM set ITEM_DEEP_LINK = (Select param_val || '/CO/FRMDD?filter=FRMDD.ITEM_ID=' || item_id || '%20and%20ver_nr=' || ver_nr
+from NCI_MDR_CNTRL where PARAM_NM = 'DEEP_LINK')
+where admin_item_typ_id = 54 ;
+commit;
+
+update ADMIN_ITEM set ITEM_RPT_URL = (select PARAM_VAL || '/invoke/FormDownload/printerFriendly?item_id=' || item_id ||  chr(38) || 'version=' || ver_nr || '\Click_to_View'    
+from NCI_MDR_CNTRL where PARAM_NM = 'DOWNLOAD_HOST') where ADMIN_ITEM_TYP_ID = 54;
+commit;
+
+alter table admin_item enable all triggers;
