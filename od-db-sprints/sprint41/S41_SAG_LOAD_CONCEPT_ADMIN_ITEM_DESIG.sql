e4423a4900c65b7dbb7e311908b2bd750fc235c0
asafieva <asafieva@gmail.com>
2023-01-30 19:26:18 -0500

    DSRMWS-2436 Added ENGLISH to prior names and definitions

--- od-db-sprints/sprint41/S41_SAG_LOAD_CONCEPT_ADMIN_ITEM_DESIG.sql
+++ od-db-sprints/sprint41/S41_SAG_LOAD_CONCEPT_ADMIN_ITEM_DESIG.sql
@@ -1,3 +1,11 @@
+CREATE OR REPLACE PROCEDURE CLEAR_INDEX(INDEX_NAME IN VARCHAR2) AS
+BEGIN
+    EXECUTE IMMEDIATE 'drop index ' || INDEX_NAME;
+EXCEPTION
+    WHEN OTHERS THEN
+        NULL;
+END CLEAR_INDEX;
+/
 create or replace Procedure SAG_LOAD_CONCEPT_ADMIN_ITEM_DESIG AS
 --DECLARE
 v_eff_date DATE := sysdate;
@@ -51,7 +59,7 @@ EVS_PREF_NAME, 1,
 from SAG_LOAD_CONCEPTS_EVS where
  code not in (select item_long_nm from ADMIN_ITEM where admin_item_typ_id = 49 and cntxt_item_id = 20000000024)-- and context restriction NCIP
  and (CONCEPT_STATUS is NULL and CON_IDSEQ is null);-- use an empty status concepts
-commit;
+-- commit;
 dbms_output.put_line('loaded concepts to admin_item !!!');
 
 --Add concept table records based on AI table
@@ -70,7 +78,7 @@ SELECT item_id,
      LST_UPD_DT,
      LST_UPD_USR_ID from admin_item where (item_id, ver_nr) in
 (select item_id, ver_nr from admin_item where admin_item_typ_id = 49 minus select item_id, ver_nr from cncpt);
-commit;
+commit; --commit only when both cncpt and admin_item updated
 dbms_output.put_line('loaded concepts to cncpt !!!'); 
 -- Add IDSEQ generated for concepts in AI, and ITEM_ID, VER_NR reference to EVS flat table
 
@@ -125,7 +133,8 @@ dbms_output.put_line('concept load updates done !!!');
 	EXECUTE IMMEDIATE 'ALTER TRIGGER TR_NCI_ALT_NMS_DENORM_INS ENABLE';
 	EXECUTE IMMEDIATE 'ALTER TRIGGER TR_ALT_NMS_POST ENABLE';-- this trigger updates AI audit info on designation updates
 	EXECUTE IMMEDIATE 'ALTER TRIGGER TR_ALT_DEF_POST ENABLE';
-	EXECUTE IMMEDIATE 'DROP INDEX IDX_SAG_LOAD_CNCPT_ID_VER';
+	--drop an auxilary index if there
+     CLEAR_INDEX('IDX_SAG_LOAD_CNCPT_ID_VER');
 
 EXCEPTION
     WHEN OTHERS THEN
@@ -136,6 +145,8 @@ EXCEPTION
         EXECUTE IMMEDIATE 'ALTER TRIGGER TR_NCI_ALT_NMS_DENORM_INS ENABLE';
         EXECUTE IMMEDIATE 'ALTER TRIGGER TR_ALT_NMS_POST ENABLE';-- this trigger updates AI audit info on designation updates
         EXECUTE IMMEDIATE 'ALTER TRIGGER TR_ALT_DEF_POST ENABLE';
+        --drop an auxilary index if there
+        CLEAR_INDEX('IDX_SAG_LOAD_CNCPT_ID_VER');
         RAISE_APPLICATION_ERROR (-20002, 'SAG_LOAD_CONCEPT_ADMIN_ITEM_DESIG error, code ' || SQLCODE || ': ' || SUBSTR(SQLERRM, 1 , 512));
   END;-- of EVS concepts data load
   
@@ -147,3 +158,171 @@ EXCEPTION
         DBMS_OUTPUT.PUT_LINE('nci_import.spLoadConceptRel error, code ' || SQLCODE || ': ' || SUBSTR(SQLERRM, 1 , 512));
   END;
 END;
+/
+create or replace Procedure SAG_LOAD_CONCEPT_PRIOR_DEF (P_CREAT_DT IN DATE DEFAULT sysdate) AS
+v_date DATE := P_CREAT_DT; --parameter
+V_IDSEQ CHAR(36 BYTE);
+v_cnt NUMBER(3);
+BEGIN
+-- assuming that sag_load_concepts_evs is preprocessed
+  FOR desig IN (SELECT item_id, ver_nr FROM sag_load_concepts_evs where CHANGED_DEF = 1)
+  LOOP --shall check not include if def is there
+   select count(*) into v_cnt from ALT_DEF
+   where NCI_DEF_TYP_ID = 1357
+   and CNTXT_ITEM_ID = 20000000024 and CNTXT_VER_NR = 1
+   and ITEM_ID = desig.ITEM_ID and VER_NR = desig.VER_NR
+   and DEF_DESC =
+   (select item_desc from admin_item where ITEM_ID = desig.ITEM_ID and VER_NR = desig.VER_NR);
+      --insert if not exist
+   IF (v_cnt  = 0) THEN
+    INSERT INTO /*+ APPEND */ ALT_DEF (DEF_DESC,
+    NCI_DEF_TYP_ID ,
+    CNTXT_ITEM_ID , --trigger 20000000024 NCIP
+    CNTXT_VER_NR ,
+    ITEM_ID , --AI reference
+    VER_NR , --AI reference
+    CREAT_USR_ID , CREAT_DT , LST_UPD_USR_ID , LST_UPD_DT )
+    SELECT
+        item_desc,
+        1357, -- Prior Preferred Definition
+        20000000024, --NCIP ID
+        1,
+        item_id, --concept to update name
+        ver_nr,
+        'ONEDATA',
+        v_date, --parameter
+        'ONEDATA',
+        v_date
+    FROM
+        admin_item
+    WHERE
+        admin_item_typ_id = 49
+        AND item_id = desig.item_id
+        AND ver_nr = desig.ver_nr
+        AND admin_stus_id = 75
+    ;
+   END IF;
+  END LOOP;
+  commit;
+  EXCEPTION
+    WHEN OTHERS THEN
+    DBMS_OUTPUT.PUT_LINE('SAG_LOAD_CONCEPT_PRIOR_DEF error, code ' || SQLCODE || ': ' || SUBSTR(SQLERRM, 1 , 512));
+        rollback;
+    RAISE_APPLICATION_ERROR (SQLCODE, 'SAG_LOAD_CONCEPT_PRIOR_DEF error, code ' || SQLCODE || ': ' || SUBSTR(SQLERRM, 1 , 512));
+END;
+/
+create or replace Procedure SAG_LOAD_CONCEPT_PRIOR_NAME (P_CREAT_DT IN DATE DEFAULT sysdate) AS
+v_date DATE := P_CREAT_DT; --parameter
+V_IDSEQ CHAR(36 BYTE);
+BEGIN
+-- assuming that sag_load_concepts_evs is preprocessed and item_id, item_nr is a unique combination
+  FOR desig IN (SELECT item_id, ver_nr FROM sag_load_concepts_evs where CHANGED_NAME = 1)
+  LOOP
+   BEGIN
+         --check that prior name like this does not exist
+   select NCI_IDSEQ into V_IDSEQ from ALT_NMS
+   where NM_TYP_ID = 1049
+   and CNTXT_ITEM_ID = 20000000024 and CNTXT_VER_NR = 1
+   and ITEM_ID = desig.ITEM_ID and VER_NR = desig.VER_NR
+   and NM_DESC =
+   (select item_nm from admin_item where ITEM_ID = desig.ITEM_ID and VER_NR = desig.VER_NR);
+   EXCEPTION
+   --insert if not exist
+      WHEN no_data_found THEN
+    INSERT INTO /*+ APPEND */ ALT_NMS (NM_DESC,
+    nm_typ_id ,
+    CNTXT_ITEM_ID , --trigger 20000000024 NCIP
+    CNTXT_VER_NR , CNTXT_NM_DN ,
+    LANG_ID ,
+    ITEM_ID , --AI reference
+    VER_NR , --AI reference
+    CREAT_USR_ID , CREAT_DT , LST_UPD_USR_ID , LST_UPD_DT )
+    SELECT
+        item_nm,
+        1049,  --Prior Preferred Name
+        20000000024, --NCIP ID
+        1,
+        'NCIP', --Context
+        1000, --DSRMWS-2436 ENGLISH
+        item_id, --concept to update name
+        ver_nr,
+        'ONEDATA',
+        v_date, --parameter
+        'ONEDATA',
+        v_date
+    FROM
+        admin_item
+    WHERE
+        admin_item_typ_id = 49
+        AND item_id = desig.item_id
+        AND ver_nr = desig.ver_nr
+        AND admin_stus_id = 75
+    ;
+   WHEN OTHERS THEN
+        DBMS_OUTPUT.PUT_LINE('SAG_LOAD_CONCEPT_PRIOR_NAME loop error, code ' || SQLCODE || ': ' || SUBSTR(SQLERRM, 1 , 512));
+        rollback;
+        RAISE_APPLICATION_ERROR (SQLCODE, 'SAG_LOAD_CONCEPT_PRIOR_NAME Loop error, code ' || SQLCODE || ': ' || SUBSTR(SQLERRM, 1 , 512));
+   END;
+  END LOOP;
+  commit;
+  EXCEPTION
+    WHEN OTHERS THEN
+    DBMS_OUTPUT.PUT_LINE('SAG_LOAD_CONCEPT_PRIOR_NAME error, code ' || SQLCODE || ': ' || SUBSTR(SQLERRM, 1 , 512));
+        rollback;
+    RAISE_APPLICATION_ERROR (SQLCODE, 'SAG_LOAD_CONCEPT_PRIOR_NAME error, code ' || SQLCODE || ': ' || SUBSTR(SQLERRM, 1 , 512));
+END;
+/
+create or replace Procedure SAG_LOAD_CONCEPT_PRIOR_DEF (P_CREAT_DT IN DATE DEFAULT sysdate) AS
+v_date DATE := P_CREAT_DT; --parameter
+V_IDSEQ CHAR(36 BYTE);
+v_cnt NUMBER(3);
+BEGIN
+-- assuming that sag_load_concepts_evs is preprocessed
+  FOR desig IN (SELECT item_id, ver_nr FROM sag_load_concepts_evs where CHANGED_DEF = 1)
+  LOOP --shall check not include if def is there
+   select count(*) into v_cnt from ALT_DEF
+   where NCI_DEF_TYP_ID = 1357
+   and CNTXT_ITEM_ID = 20000000024 and CNTXT_VER_NR = 1
+   and ITEM_ID = desig.ITEM_ID and VER_NR = desig.VER_NR
+   and DEF_DESC =
+   (select item_desc from admin_item where ITEM_ID = desig.ITEM_ID and VER_NR = desig.VER_NR);
+      --insert if not exist
+   IF (v_cnt  = 0) THEN
+    INSERT INTO /*+ APPEND */ ALT_DEF (DEF_DESC,
+    NCI_DEF_TYP_ID ,
+    CNTXT_ITEM_ID , --trigger 20000000024 NCIP
+    CNTXT_VER_NR ,
+    LANG_ID ,
+    ITEM_ID , --AI reference
+    VER_NR , --AI reference
+    CREAT_USR_ID , CREAT_DT , LST_UPD_USR_ID , LST_UPD_DT )
+    SELECT
+        item_desc,
+        1357, -- Prior Preferred Definition
+        20000000024, --NCIP ID
+        1,
+        1000, --DSRMWS-2436 ENGLISH
+        item_id, --concept to update name
+        ver_nr,
+        'ONEDATA',
+        v_date, --parameter
+        'ONEDATA',
+        v_date
+    FROM
+        admin_item
+    WHERE
+        admin_item_typ_id = 49
+        AND item_id = desig.item_id
+        AND ver_nr = desig.ver_nr
+        AND admin_stus_id = 75
+    ;
+   END IF;
+  END LOOP;
+  commit;
+  EXCEPTION
+    WHEN OTHERS THEN
+    DBMS_OUTPUT.PUT_LINE('SAG_LOAD_CONCEPT_PRIOR_DEF error, code ' || SQLCODE || ': ' || SUBSTR(SQLERRM, 1 , 512));
+        rollback;
+    RAISE_APPLICATION_ERROR (SQLCODE, 'SAG_LOAD_CONCEPT_PRIOR_DEF error, code ' || SQLCODE || ': ' || SUBSTR(SQLERRM, 1 , 512));
+END;
+/
