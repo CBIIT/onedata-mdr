5f4e92aacc1573d77fa28f3aeb16f99dd8bb66aa
deepalimanathara <deepali.manathara@gmail.com>
2023-02-15 13:19:18 -0500

    Update S42_WA_RA_DDL.sql

--- od-db-sprints/sprint42/S42_WA_RA_DDL.sql
+++ od-db-sprints/sprint42/S42_WA_RA_DDL.sql
@@ -44,7 +44,7 @@
 	      de, VALUE_DOM vd,
            (  SELECT item_id, ver_nr, ref_desc  FROM REF, OBJ_KEY WHERE REF_TYP_ID = OBJ_KEY.OBJ_KEY_ID   AND UPPER(OBJ_KEY_DESC)='EXAMPLE' order by REF.creat_dt desc fetch first row only)  EXMPL,
            (  SELECT item_id, ver_nr, ref_desc  FROM REF, OBJ_KEY WHERE REF_TYP_ID = OBJ_KEY.OBJ_KEY_ID   AND UPPER(OBJ_KEY_DESC)='CODING INSTRUCTIONS' order by REF.creat_dt desc fetch first row only)  CODE_INSTR,
-           (  SELECT item_id,ver_nr, nm_desc  FROM ALT_NMS, OBJ_KEY WHERE NM_TYP_ID = OBJ_KEY.OBJ_KEY_ID   AND UPPER(OBJ_KEY_DESC)='CRDC NAME' order by ALT_NMS.creat_dt desc fetch first row only)  CRDC_NM,
+           (  SELECT item_id,ver_nr, nm_desc  FROM ALT_NMS, OBJ_KEY WHERE NM_TYP_ID = OBJ_KEY.OBJ_KEY_ID   AND UPPER(OBJ_KEY_DESC)='CRDC ALT NAME' order by ALT_NMS.creat_dt desc fetch first row only)  CRDC_NM,
            (  SELECT item_id, ver_nr, def_desc  FROM ALT_DEF, OBJ_KEY WHERE NCI_DEF_TYP_ID = OBJ_KEY.OBJ_KEY_ID   AND UPPER(OBJ_KEY_DESC)='CRDC DEFINITION' order by ALT_DEF.creat_dt desc fetch first row only)  CRDC_DEF        
      WHERE     ADMIN_ITEM_TYP_ID = 4
            and ADMIN_ITEM.ITEM_Id = de.item_id
