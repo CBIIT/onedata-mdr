709f2dfdc59f1c385cd818873c1360c4dc03f2df
deepalimanathara <deepali.manathara@gmail.com>
2023-02-28 16:12:43 -0500

    Update S42_MD.sql

--- od-db-sprints/sprint42/S42_MD.sql
+++ od-db-sprints/sprint42/S42_MD.sql
@@ -1,2 +1,6 @@
 grant select on od_md_obj to onedata_Wa;
 grant select on od_md_objsecu to onedata_Wa;
+
+grant select on OD_MD_USAGE_LOG to onedata_Wa
+grant select on OD_MD_OBJSECUREL to onedata_Wa
+
