4cf60afa970150fe419c02b8ff2ad5ae9a6c28da
deepalimanathara <deepali.manathara@gmail.com>
2023-03-13 12:43:54 -0400

    Update S42_WA_RA_DDL.sql

--- od-db-sprints/sprint42/S42_WA_RA_DDL.sql
+++ od-db-sprints/sprint42/S42_WA_RA_DDL.sql
@@ -581,10 +581,10 @@ x.p_item_ver_nr) y, nci_mdr_cntrl c, nci_admin_item_rel r
            ak.FLD_DELETE,
            prot.P_ITEM_ID PROT_ITEM_ID,
            prot.P_ITEM_VER_NR PROT_VER_NR,
-           ak.c_item_id cde_item_id,
-           ak.c_item_ver_nr cde_ver_nr
+           ak.c_item_id item_id,
+           ak.c_item_ver_nr ver_nr
            FROM NCI_ADMIN_ITEM_REL r, NCI_ADMIN_ITEM_REL_ALT_KEY ak , nci_admin_item_rel prot
      WHERE  ak.P_ITEM_ID = r.C_ITEM_ID and ak.P_ITEM_VER_NR = r.C_ITEM_VER_NR and
 	   r.rel_typ_id = 61 
-  and r.p_item_id = prot.c_item_id and r.p_item_ver_nr = prot.c_item_ver_nr and prot.rel_typ_id=60
+  and r.p_item_id = prot.c_item_id and r.p_item_ver_nr = prot.c_item_ver_nr and prot.rel_typ_id=60;
 
