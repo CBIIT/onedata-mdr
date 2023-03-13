0698a0f0a8c1cf434007da4047b28d0fa22f40e3
deepalimanathara <deepali.manathara@gmail.com>
2023-03-13 12:04:23 -0400

    Create S42_RA.sql

---
+++ od-db-sprints/sprint42/S42_RA.sql
@@ -0,0 +1,9 @@
+--------------------------------------------------------
+--  DDL for Table NCI_MDR_CNTRL
+--------------------------------------------------------
+
+  CREATE TABLE NCI_MDR_CNTRL 
+   (	"ID" NUMBER(*,0), 
+	"PARAM_NM" VARCHAR2(100 BYTE) COLLATE "USING_NLS_COMP", 
+	"PARAM_VAL" VARCHAR2(100 BYTE) COLLATE "USING_NLS_COMP"
+   )  ;
