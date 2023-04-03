c2a5d56f9897e33795583148c5776ea718d32ab4
deepalimanathara <deepali.manathara@gmail.com>
2023-04-03 17:58:58 -0400

    Update S43_WA_RA_DDL.sql

--- od-db-sprints/sprint43/S43_WA_RA_DDL.sql
+++ od-db-sprints/sprint43/S43_WA_RA_DDL.sql
@@ -454,4 +454,18 @@ nvl(decode(trim(ADMIN_ITEM.DEF_SRC), 'NCI', '1-NCI', ADMIN_ITEM.DEF_SRC), 'No De
 	and ADMIN_ITEM.ITEM_ID = a.ITEM_ID (+) and ADMIN_ITEM.VER_NR = a.VER_NR (+);
 
 
+
+  CREATE OR REPLACE VIEW VW_VAL_MEAN
+  As SELECT ADMIN_ITEM.ITEM_ID, ADMIN_ITEM.VER_NR, ADMIN_ITEM.ITEM_NM, ADMIN_ITEM.ITEM_LONG_NM,  ADMIN_ITEM.ITEM_DESC,
+ADMIN_ITEM.CNTXT_NM_DN, ADMIN_ITEM.CURRNT_VER_IND, ADMIN_ITEM.REGSTR_STUS_NM_DN, ADMIN_ITEM.ADMIN_STUS_NM_DN, ADMIN_ITEM.CREAT_DT,
+ADMIN_ITEM.CREAT_USR_ID, ADMIN_ITEM.LST_UPD_USR_ID, ADMIN_ITEM.FLD_DELETE, ADMIN_ITEM.LST_DEL_DT, ADMIN_ITEM.S2P_TRN_DT,
+ADMIN_ITEM.LST_UPD_DT,
+decode(ext.cncpt_concat, ext.cncpt_concat_nm, null, admin_item.item_long_nm, null, admin_item.item_id, null, ext.cncpt_concat)  cncpt_concat,
+ext.cncpt_concat_nm, nvl(admin_item.origin_id_dn, origin) ORIGIN_NM,
+ext.cncpt_concat_src_typ, ext.cncpt_concat_with_int
+FROM ADMIN_ITEM, NCI_ADMIN_ITEM_EXT ext
+       WHERE ADMIN_ITEM_TYP_ID = 53 and admin_item.item_id = ext.item_id and admin_item.ver_nr = ext.ver_nr;
+
+
+
  
