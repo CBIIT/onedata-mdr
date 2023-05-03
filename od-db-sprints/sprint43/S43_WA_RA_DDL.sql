f5d3eecf8c698d63d470daeba0ba3d00aa362470
deepalimanathara <deepali.manathara@gmail.com>
2023-05-03 10:44:13 -0400

    Update S43_WA_RA_DDL.sql

--- od-db-sprints/sprint43/S43_WA_RA_DDL.sql
+++ od-db-sprints/sprint43/S43_WA_RA_DDL.sql
@@ -1,3 +1,14 @@
+--Emergency change 
+
+alter table admin_item disable all triggers;
+ 
+update ADMIN_ITEM set ITEM_RPT_URL = (select PARAM_VAL || '/invoke/FormDownload/printerFriendly?item_id=' || item_id ||  chr(38) || 'version=' || ver_nr 
+				      from NCI_MDR_CNTRL where PARAM_NM = 'DOWNLOAD_HOST') where ADMIN_ITEM_TYP_ID = 54;
+commit;
+ 
+alter table admin_item enable all triggers;
+
+
 drop MATERIALIZED VIEW MVW_FORM_NODE_DE_REL;
   
   CREATE MATERIALIZED VIEW MVW_FORM_NODE_DE_REL
