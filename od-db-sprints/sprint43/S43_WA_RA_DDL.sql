39ba620a9368522d9aa4ab9ead4a10136660262c
deepalimanathara <deepali.manathara@gmail.com>
2023-05-03 12:36:21 -0400

    Update S43_WA_RA_DDL.sql

--- od-db-sprints/sprint43/S43_WA_RA_DDL.sql
+++ od-db-sprints/sprint43/S43_WA_RA_DDL.sql
@@ -1,11 +1,4 @@
---Emergency change 
 
-alter table admin_item disable all triggers;
- 
-update ADMIN_ITEM set ITEM_RPT_URL = (select PARAM_VAL || '/invoke/FormDownload/printerFriendly?item_id=' || item_id ||  chr(38) || 'version=' || ver_nr 
-				      from NCI_MDR_CNTRL where PARAM_NM = 'DOWNLOAD_HOST') where ADMIN_ITEM_TYP_ID = 54;
-commit;
- 
 alter table admin_item enable all triggers;
 
 
@@ -443,7 +436,7 @@ alter table NCI_STG_FORM_VV_IMPORT add VM_LONG_NM varchar2(255);
 
 alter table admin_item disable all triggers;
 
-update ADMIN_ITEM set ITEM_RPT_URL = (select PARAM_VAL || '/invoke/downloads.form/printerFriendly?item_id=' || item_id ||  chr(38) || 'version=' || ver_nr || '\Click_to_View'
+update ADMIN_ITEM set ITEM_RPT_URL = (select PARAM_VAL || '/invoke/downloads.form/printerFriendly?item_id=' || item_id ||  chr(38) || 'version=' || ver_nr 
 from NCI_MDR_CNTRL where PARAM_NM = 'DOWNLOAD_HOST') where ADMIN_ITEM_TYP_ID = 54;
 commit;
 
