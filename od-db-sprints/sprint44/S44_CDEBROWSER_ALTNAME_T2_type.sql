125c53d3c71b7b01d8b867d76f65924026b2172a
trushi2 <irina.trush@nih.gov>
2023-05-31 10:10:55 -0400

    Related to JIRA865

--- od-db-sprints/sprint44/S44_CDEBROWSER_ALTNAME_T2_type.sql
+++ od-db-sprints/sprint44/S44_CDEBROWSER_ALTNAME_T2_type.sql
@@ -1,1 +1,2 @@
-ALTER TYPE CDEBROWSER_ALTNAME_T2 modify attribute "AlternateName"  varchar2(4000) CASCADE;
+ ALTER TYPE CDEBROWSER_ALTNAME_T2 modify attribute "AlternateName"  varchar2(4000) CASCADE
+/
