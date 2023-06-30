84f8e490e788786293d04831816dee69f738df8d
deepalimanathara <deepali.manathara@gmail.com>
2023-06-30 13:34:49 -0400

    Update S46_MDL_WA.sql

--- od-db-sprints/sprint46/S46_MDL_WA.sql
+++ od-db-sprints/sprint46/S46_MDL_WA.sql
@@ -1,1 +1,24 @@
-Test
+CREATE OR REPLACE TRIGGER TR_NCI_MDL_ELMNT  BEFORE INSERT  on NCI_MDL_ELMNT
+  for each row
+         BEGIN    IF (:NEW.ITEM_ID<= 0  or :NEW.ITEM_ID is null)  THEN
+         select od_seq_ADMIN_ITEM.nextval
+    into :new.ITEM_ID  from  dual ;
+:new.ver_nr := 1;
+
+END IF;
+
+END ;
+/
+create or replace TRIGGER TR_NCI_MDL_ELMNT
+  BEFORE  UPDATE
+  on NCI_MDL_ELMNT
+  for each row
+BEGIN
+   :new.LST_UPD_DT := SYSDATE;
+ 
+END;
+/
+
+
+
+
