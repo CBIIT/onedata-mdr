17aa191dd453bd8d99c03af6b833e86744f55ca6
deepalimanathara <deepali.manathara@gmail.com>
2023-07-17 08:08:49 -0400

    Update S46_WA_RA_DDL.sql

--- od-db-sprints/sprint46/S46_WA_RA_DDL.sql
+++ od-db-sprints/sprint46/S46_WA_RA_DDL.sql
@@ -508,3 +508,67 @@ ai.NCI_IDSEQ, ai.ADMIN_STUS_NM_DN, ai.CNTXT_NM_DN, ai.REGSTR_STUS_NM_DN, ai.ORIG
 decode(e.CNCPT_CONCAT,e.cncpt_concat_nm, null, e.cncpt_concat)  cncpt_concat, e.CNCPT_CONCAT_NM, e.CNCPT_CONCAT_DEF
 from admin_item ai, nci_admin_item_Ext e where ai.item_id = e.item_id and ai.ver_nr = e.ver_nr ;
 
+
+  CREATE OR REPLACE VIEW VW_NCI_GENERIC_DE AS
+  SELECT ADMIN_ITEM.ITEM_ID,
+           ADMIN_ITEM.VER_NR,
+          CAST('.' || ADMIN_ITEM.ITEM_ID || '.' AS VARCHAR2(4000))    ITEM_ID_STR,
+           ADMIN_ITEM.ITEM_NM,
+           ADMIN_ITEM.ITEM_LONG_NM,
+           ADMIN_ITEM.ITEM_DESC,
+           ADMIN_ITEM.ADMIN_NOTES,
+           ADMIN_ITEM.CHNG_DESC_TXT,
+           ADMIN_ITEM.CREATION_DT,
+           ADMIN_ITEM.EFF_DT,
+           ADMIN_ITEM.ORIGIN,
+           ADMIN_ITEM.ORIGIN_ID,
+           ADMIN_ITEM.ORIGIN_ID_DN,
+           ADMIN_ITEM.UNRSLVD_ISSUE,
+           ADMIN_ITEM.UNTL_DT,
+           ADMIN_ITEM.CURRNT_VER_IND,
+           ADMIN_ITEM.REGSTR_STUS_ID,
+           ADMIN_ITEM.ADMIN_STUS_ID,
+           ADMIN_ITEM.CNTXT_NM_DN,
+           ADMIN_ITEM.CNTXT_ITEM_ID,
+           ADMIN_ITEM.CNTXT_VER_NR,
+           ADMIN_ITEM.CREAT_USR_ID,
+           ADMIN_ITEM.CREAT_USR_ID             CREAT_USR_ID_X,
+           ADMIN_ITEM.LST_UPD_USR_ID,
+           ADMIN_ITEM.LST_UPD_USR_ID           LST_UPD_USR_ID_X,
+           ADMIN_ITEM.FLD_DELETE,
+           ADMIN_ITEM.LST_DEL_DT,
+           ADMIN_ITEM.S2P_TRN_DT,
+           ADMIN_ITEM.LST_UPD_DT,
+           ADMIN_ITEM.CREAT_DT,
+           ADMIN_ITEM.NCI_IDSEQ,
+	     ADMIN_ITEM.ADMIN_ITEM_TYP_ID,
+	   ADMIN_ITEM.ITEM_DEEP_LINK,
+           ext.USED_BY                         CNTXT_AGG,
+             vd.VAL_DOM_TYP_ID	 ,
+	     an.NM_DESC,
+	     an.nm_typ_id,
+	     adef.def_desc,
+	     adef.nci_def_typ_id,
+	    cntxt_item_id srch_cntxt_id ,
+	    cntxt_ver_nr srch_cntxt_ver_nr
+	     FROM ADMIN_ITEM,
+           NCI_ADMIN_ITEM_EXT  ext,
+	      de, VALUE_DOM vd, 
+	     (SELECT an.item_id, an.ver_nr, nm_typ_id, max(nm_desc) as NM_DESC
+	      from alt_nms an
+          group by an.item_id, an.ver_nr, nm_typ_id) an,
+	     (SELECT an.item_id, an.ver_nr, nci_def_typ_id, max(def_desc) as def_DESC
+	      from alt_def an
+          group by an.item_id, an.ver_nr, nci_def_typ_id) adef
+        WHERE     ADMIN_ITEM_TYP_ID = 4
+           and ADMIN_ITEM.ITEM_Id = de.item_id
+           and ADMIN_ITEM.VER_NR = DE.VER_NR
+           and de.VAL_DOM_ITEM_ID = vd.ITEM_ID
+	   and de.VAL_DOM_VER_NR = vd.VER_NR
+	   AND ADMIN_ITEM.ITEM_ID = EXT.ITEM_ID
+           AND ADMIN_ITEM.VER_NR = EXT.VER_NR
+	   and admin_item.item_id = an.item_id 
+	   and admin_item.ver_nr = an.ver_nr 
+	   and admin_item.item_id = adef.item_id (+)
+	   and admin_item.ver_nr = adef.ver_nr (+);
+
