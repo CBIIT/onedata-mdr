31c37593c82a05c770becf580b13959c69e14f54
deepalimanathara <deepali.manathara@gmail.com>
2023-08-25 14:06:17 -0400

    Update S48_WA_RA_DDL.sql

--- od-db-sprints/sprint48/S48_WA_RA_DDL.sql
+++ od-db-sprints/sprint48/S48_WA_RA_DDL.sql
@@ -1,3 +1,4 @@
 alter table NCI_STG_MDL_ELMNT add ME_TYP_ID integer;
 
-alter table NCI_MDL_ELMNT_CHAR add (MDL_ITEM_ID number, MDL_ITEM_VER_NR number(4,2));
+alter table NCI_MEC_MAP add (SRC_MDL_ITEM_ID number, SRC_MDL_VER_NR number(4,2),TGT_MDL_ITEM_ID number, TGT_MDL_VER_NR number(4,2));
+
