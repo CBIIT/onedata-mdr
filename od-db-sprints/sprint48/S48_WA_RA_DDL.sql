268dba38dbe37f4ca07ab5b97ca3a4fddcb500d2
deepalimanathara <deepali.manathara@gmail.com>
2023-08-31 21:46:21 -0400

    Update S48_WA_RA_DDL.sql

--- od-db-sprints/sprint48/S48_WA_RA_DDL.sql
+++ od-db-sprints/sprint48/S48_WA_RA_DDL.sql
@@ -38,6 +38,26 @@ validatedPlatform: free text */
 alter table NCI_MEC_MAP add ( MEC_MAP_NM varchar2(4000), MAP_DEG  integer, MEC_MAP_DESC varchar2(4000), DIRECT_TYP integer, TRANS_RUL_NOT integer, VALID_PLTFORM  varchar2(4000), PROV_ORG_ID number);
 alter table NCI_MEC_MAP add (MEC_GRP_RUL_NBR integer, MEC_GRP_RUL_DESC varchar2(4000));
 
+
+
+	
+	insert into obj_typ (obj_typ_id, obj_typ_desc) values (45,'Model Map Degree');
+	insert into obj_typ (obj_typ_id, obj_typ_desc) values (46,'Model Map Directionality');
+
+insert into obj_key (OBJ_KEY_ID, OBJ_KEY_DESC, OBJ_TYP_ID, OBJ_KEY_DEF, OBJ_KEY_CMNTS, NCI_CD) values (86,'Semantic Equivalent',45,'Semantic Equivalent','Semantic Equivalent','Semantic Equivalent' );
+commit;
+insert into obj_key (OBJ_KEY_ID, OBJ_KEY_DESC, OBJ_TYP_ID, OBJ_KEY_DEF, OBJ_KEY_CMNTS, NCI_CD) values (87,'Semantic Similar',45,'Semantic Similar','Semantic Similar','Semantic Similar' );
+commit;
+insert into obj_key (OBJ_KEY_ID, OBJ_KEY_DESC, OBJ_TYP_ID, OBJ_KEY_DEF, OBJ_KEY_CMNTS, NCI_CD) values (116,'Directional',46,'Directional','Directional','Directional' );
+commit;
+insert into obj_key (OBJ_KEY_ID, OBJ_KEY_DESC, OBJ_TYP_ID, OBJ_KEY_DEF, OBJ_KEY_CMNTS, NCI_CD) values (117,'Bi-Directional',46,'Bi-Directional','Bi-Directional','Bi-Directional' );
+commit;
+insert into obj_key (OBJ_KEY_ID, OBJ_KEY_DESC, OBJ_TYP_ID, OBJ_KEY_DEF, OBJ_KEY_CMNTS, NCI_CD) values (118,'1..*',46,'1..*','1..*','1..*' );
+commit;
+insert into obj_key (OBJ_KEY_ID, OBJ_KEY_DESC, OBJ_TYP_ID, OBJ_KEY_DEF, OBJ_KEY_CMNTS, NCI_CD) values (119,'*..1',46,'*..1','*..1','*..1' );
+commit;
+
+
 alter table NCI_STG_MDL_ELMNT add (DOM_ITEM_ID  number, DOM_VER_NR number(4,2));
 
 
