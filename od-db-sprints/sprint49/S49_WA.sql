b09624aa8d2cb080651eb77431f30290cb61727d
deepalimanathara <deepali.manathara@gmail.com>
2023-09-26 16:11:52 -0400

    Create S49_WA.sql

---
+++ od-db-sprints/sprint49/S49_WA.sql
@@ -0,0 +1,16 @@
+
+
+insert into obj_key (OBJ_KEY_DESC, OBJ_TYP_ID, OBJ_KEY_DEF) values ('Person',49,'Person' );
+commit;
+insert into obj_key (OBJ_KEY_DESC, OBJ_TYP_ID, OBJ_KEY_DEF) values ('Provider',49,'Provider' );
+insert into obj_key (OBJ_KEY_DESC, OBJ_TYP_ID, OBJ_KEY_DEF) values ('Procedure',49,'Procedure' );
+insert into obj_key (OBJ_KEY_DESC, OBJ_TYP_ID, OBJ_KEY_DEF) values ('Observation',49,'Observation' );
+insert into obj_key (OBJ_KEY_DESC, OBJ_TYP_ID, OBJ_KEY_DEF) values ('Diagnosis',49,'Diagnosis' );
+insert into obj_key (OBJ_KEY_DESC, OBJ_TYP_ID, OBJ_KEY_DEF) values ('Specimen',49,'Specimen' );
+insert into obj_key (OBJ_KEY_DESC, OBJ_TYP_ID, OBJ_KEY_DEF) values ('Healthcare Facility Site',49,'Healthcare Facility Site' );
+insert into obj_key (OBJ_KEY_DESC, OBJ_TYP_ID, OBJ_KEY_DEF) values ('Medication',49,'Medication' );
+insert into obj_key (OBJ_KEY_DESC, OBJ_TYP_ID, OBJ_KEY_DEF) values ('Note',49,'Note' );
+insert into obj_key (OBJ_KEY_DESC, OBJ_TYP_ID, OBJ_KEY_DEF) values ('Visit',49,'Visit' );
+insert into obj_key (OBJ_KEY_DESC, OBJ_TYP_ID, OBJ_KEY_DEF) values ('Test Results',49,'Test Results' );
+insert into obj_key (OBJ_KEY_DESC, OBJ_TYP_ID, OBJ_KEY_DEF) values ('Vital Signs',49,'Vital Signs' );
+commit;
