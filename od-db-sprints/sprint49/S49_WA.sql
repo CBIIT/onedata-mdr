35f45ff85a0192a38af5ca0bedfd90a2a54bd99e
deepalimanathara <deepali.manathara@gmail.com>
2023-10-06 12:11:56 -0400

    Update S49_WA.sql

--- od-db-sprints/sprint49/S49_WA.sql
+++ od-db-sprints/sprint49/S49_WA.sql
@@ -41,3 +41,22 @@ end if;
 
 END;
 /
+
+
+create or replace    TRIGGER TR_NCI_MDL_MAP_NM
+  AFTER INSERT
+  on NCI_MDL_MAP
+  for each row
+BEGIN
+   update ADMIN_ITEM set ITEM_NM_CURATED = (Select s.item_nm || 'v' || :new.src_mdl_ver_nr || ' mapped to ' || t.item_nm || 'v' || :new.tgt_mdl_ver_nr
+   from admin_item s, admin_item t where s.item_id = :new.src_mdl_item_id and s.ver_nr = :new.src_mdl_ver_nr and t.item_id = :new.tgt_mdl_item_id and t.ver_nr = :new.tgt_mdl_ver_nr)
+   where item_id = :new.item_id and ver_nr = :new.ver_nr;
+  -- commit;
+END;
+/
+
+update ADMIN_ITEM set ITEM_NM_CURATED = (Select s.item_nm || 'v' || s.ver_nr || ' mapped to ' || t.item_nm || 'v' || t.ver_nr
+   from admin_item s, admin_item t, nci_mdl_map m where s.item_id = m.src_mdl_item_id and s.ver_nr = m.src_mdl_ver_nr and t.item_id = m.tgt_mdl_item_id 
+   and t.ver_nr = m.tgt_mdl_ver_nr and m.item_id = admin_item.item_id and m.ver_nr = admin_item.ver_nr)
+   where admin_item_typ_id = 58
+   commit;
