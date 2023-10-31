0d9b45f1678e3fad06615bfef7efd0534da14735
deepalimanathara <deepali.manathara@gmail.com>
2023-10-31 10:43:01 -0400

    Update S50_WA.sql

--- od-db-sprints/sprint50/S50_WA.sql
+++ od-db-sprints/sprint50/S50_WA.sql
@@ -1,14 +1,3 @@
-create or replace TRIGGER TR_AI_MTCH
-  BEFORE UPDATE or INSERT ON ADMIN_ITEM
-  FOR EACH ROW
-  BEGIN
-
-:new.MTCH_TERM := regexp_replace(upper(:new.item_nm),'\(|\)|\;|\-|\_|\||\:|\$|\[|\]|\''|\"|\%|\*|\&|\#|\@|\{|\}','');
-:new.MTCH_TERM_ADV := regexp_replace(upper(:new.item_nm),'\(|\)|\;|\-|\_|\||\:|\$|\[|\]|\''|\"|\%|\*|\&|\#|\@|\{|\}|\s','');
-
-
-END;
-/
 
 alter table admin_item disable all triggers;
 alter table alt_nms disable all triggers;
@@ -17,14 +6,14 @@ set escape on
 
 update admin_item set 
 MTCH_TERM_ADV = regexp_replace(upper(item_nm),'[^A-Za-z0-9]', ''),
- MTCH_TERM= regexp_replace(upper(item_nm),'[^A-Za-z0-9]', '');
+ MTCH_TERM= regexp_replace(upper(item_nm),'[^ A-Za-z0-9]', '');
 commit;
 
 
 
 update alt_nms set 
 MTCH_TERM_ADV = regexp_replace(upper(nm_desc),'[^A-Za-z0-9]', ''),
- MTCH_TERM= regexp_replace(upper(nm_Desc),'[^A-Za-z0-9]', '');
+ MTCH_TERM= regexp_replace(upper(nm_Desc),'[^ A-Za-z0-9]', '');
 commit;
 alter table admin_item enable all triggers;
 alter table alt_nms enable all triggers;
@@ -34,7 +23,7 @@ create or replace TRIGGER TR_AI_MTCH
   FOR EACH ROW
   BEGIN
 
-:new.MTCH_TERM := regexp_replace(upper(:new.item_nm),'[^A-Za-z0-9]','');
+:new.MTCH_TERM := regexp_replace(upper(:new.item_nm),'[^ A-Za-z0-9]','');
 :new.MTCH_TERM_ADV := regexp_replace(upper(:new.item_nm),'[^A-Za-z0-9]','');
 
 
@@ -47,7 +36,7 @@ create or replace TRIGGER TR_ALT_NMS_BEFORE
   for each row
 BEGIN
 
-:new.MTCH_TERM := regexp_replace(upper(:new.nm_desc),'[^A-Za-z0-9]','');
+:new.MTCH_TERM := regexp_replace(upper(:new.nm_desc),'[^ A-Za-z0-9]','');
 :new.MTCH_TERM_ADV := regexp_replace(upper(:new.nm_desc),'[^A-Za-z0-9]','');  
 END;
 /
