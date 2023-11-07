7903fef3af241b5f7f6b160bcbfd1bb29e39e10e
matt-nash-nih <85249953+matt-nash-nih@users.noreply.github.com>
2023-11-07 11:51:22 -0500

    Update S50_RA.sql

--- od-db-sprints/sprint50/S50_RA.sql
+++ od-db-sprints/sprint50/S50_RA.sql
@@ -1,2 +1,2 @@
-insert into obj_key select * from onedata_wa.obj_key where obj_typ_id in (50,51) and obj_key_id not in (Select obj_key_id from obj_key)
+insert into obj_key select * from onedata_wa.obj_key where obj_typ_id in (50,51,52) and obj_key_id not in (Select obj_key_id from obj_key)
 commit;
