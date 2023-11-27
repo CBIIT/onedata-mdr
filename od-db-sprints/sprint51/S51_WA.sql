1e1cc2a5e60ff174f9e1ab0e28076218e94c1655
deepalimanathara <deepali.manathara@gmail.com>
2023-11-27 13:56:18 -0500

    Update S51_WA.sql

--- od-db-sprints/sprint51/S51_WA.sql
+++ od-db-sprints/sprint51/S51_WA.sql
@@ -9,3 +9,16 @@ commit;
 insert into obj_key (OBJ_KEY_DESC, OBJ_TYP_ID, OBJ_KEY_DEF) select 'ICD-O_CODE',23,'ICD-O_CODE'  from dual where ('ICD-O_CODE') not in 
 (Select  upper(obj_key_desc) from obj_key where obj_typ_id = 23) ;
 commit;
+
+create or replace TRIGGER TR_ADMIN_ITEM_REL_FOR_CONCAT
+  before INSERT 
+  on
+NCI_ADMIN_ITEM_REL
+  for each row
+BEGIN
+
+     :new.P_ITEM_ID_VER := :new.P_ITEM_ID || 'v'|| :new.P_ITEM_VER_NR;
+     :new.C_ITEM_ID_VER := :new.C_ITEM_ID || 'v' || :new.C_ITEM_VER_NR;
+
+END;
+/
