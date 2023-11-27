b45dd93faaf72ce151acdc24c0b82b497314edba
deepalimanathara <deepali.manathara@gmail.com>
2023-11-27 13:36:19 -0500

    Update S51_WA.sql

--- od-db-sprints/sprint51/S51_WA.sql
+++ od-db-sprints/sprint51/S51_WA.sql
@@ -1,3 +1,11 @@
 --jira 3015
 insert into obj_key (obj_key_desc, obj_typ_id, obj_key_def) values ('NIH CDE Submission Template', 31, 'NIH CDE Submission Template');
 commit;
+
+
+insert into obj_key (OBJ_KEY_ID, OBJ_KEY_DESC, OBJ_TYP_ID, OBJ_KEY_DEF) values (125,'NCIT-External Concept Relationship',39,'NCIT-External Concept Relationship' );
+commit;
+
+insert into obj_key (OBJ_KEY_DESC, OBJ_TYP_ID, OBJ_KEY_DEF) select 'ICD-O_CODE',23,'ICD-O_CODE'  from dual where ('ICD-O_CODE') not in 
+(Select  upper(obj_key_desc) from obj_key where obj_typ_id = 23) ;
+commit;
