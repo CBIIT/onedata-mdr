abc7034f9522ae5b6a5afcea0c03d3da8259917f
deepalimanathara <deepali.manathara@gmail.com>
2023-11-27 13:37:41 -0500

    Update S51_RA.sql

--- od-db-sprints/sprint51/S51_RA.sql
+++ od-db-sprints/sprint51/S51_RA.sql
@@ -1,4 +1,4 @@
 grant all on NCI_ADMIN_ITEM_XMAP to onedata_wa;
 
-insert into obj_key select * from onedata_wa.obj_key where obj_typ_id in (31) and obj_key_id not in (Select obj_key_id from obj_key);
+insert into obj_key select * from onedata_wa.obj_key where  obj_key_id not in (Select obj_key_id from obj_key);
 commit;
