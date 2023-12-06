9d53bdf25f14ef1206ff052812bfd13d7b1e75f8
deepalimanathara <deepali.manathara@gmail.com>
2023-12-06 14:16:40 -0500

    Update S51_WA_RA_DDL.sql

--- od-db-sprints/sprint51/S51_WA_RA_DDL.sql
+++ od-db-sprints/sprint51/S51_WA_RA_DDL.sql
@@ -1,4 +1,5 @@
-CREATE OR REPLACE  VIEW VW_NCI_AI_CNCPT AS
+
+  CREATE OR REPLACE  VIEW VW_NCI_AI_CNCPT AS
   SELECT  '5. Object Class' ALT_NMS_LVL, DE.ITEM_ID  ITEM_ID,
 		DE.VER_NR VER_NR,
 		a.ITEM_ID CORE_ITEM_ID, a.VER_NR CORE_VER_NR,
@@ -134,8 +135,53 @@ and nvl(pv.fld_delete,0) = 0
 		a.FLD_DELETE, a.LST_DEL_DT, a.S2P_TRN_DT,
 		a.LST_UPD_DT, 1 NCI_ORD, 1 NCI_PRMRY_IND , '1' NCI_CNCPT_VAL,
 		'1' NCI_PRMRY_IND_TXT, REL_TYP_ID
-       FROM NCI_CNCPT_REL a;
+       FROM NCI_CNCPT_REL a
+union
+	SELECT  'External Terminology Relationship' ALT_NMS_LVL, 
+		xmap.ITEM_ID ITEM_ID, xmap.VER_NR VER_NR,
+		xmap.ITEM_ID CORE_ITEM_ID, xmap.VER_NR CORE_VER_NR,
+		nonai.ITEM_ID CNCPT_ITEM_ID, nonai.VER_NR CNCPT_VER_NR,
+		xmap.CREAT_DT, xmap.CREAT_USR_ID, xmap.LST_UPD_USR_ID,
+		xmap.FLD_DELETE, xmap.LST_DEL_DT, xmap.S2P_TRN_DT,
+		xmap.LST_UPD_DT, 1 NCI_ORD, 1 NCI_PRMRY_IND , xmap.xmap_cd NCI_CNCPT_VAL,
+		'1' NCI_PRMRY_IND_TXT, 125
+       FROM admin_item nonai, cncpt noncncpt, NCI_ADMIN_ITEM_XMAP xmap
+where nonai.ITEM_LONG_NM = xmap.XMAP_CD and noncncpt.EVS_SRC_ID = xmap.EVS_SRC_ID
+and nonai.item_id = noncncpt.item_id and nonai.ver_nr = noncncpt.ver_nr
+union
+	SELECT  'External Terminology Relationship' ALT_NMS_LVL, 
+		nonai.ITEM_ID ITEM_ID, nonai.VER_NR VER_NR,
+		nonai.ITEM_ID CORE_ITEM_ID, nonai.VER_NR CORE_VER_NR,
+		xmap.ITEM_ID CNCPT_ITEM_ID, xmap.VER_NR CNCPT_VER_NR,
+		xmap.CREAT_DT, xmap.CREAT_USR_ID, xmap.LST_UPD_USR_ID,
+		xmap.FLD_DELETE, xmap.LST_DEL_DT, xmap.S2P_TRN_DT,
+		xmap.LST_UPD_DT, 1 NCI_ORD, 1 NCI_PRMRY_IND , xmap.xmap_cd NCI_CNCPT_VAL,
+		'1' NCI_PRMRY_IND_TXT, 125
+       FROM admin_item nonai, cncpt noncncpt, NCI_ADMIN_ITEM_XMAP xmap
+where nonai.ITEM_LONG_NM = xmap.XMAP_CD and noncncpt.EVS_SRC_ID = xmap.EVS_SRC_ID
+and nonai.item_id = noncncpt.item_id and nonai.ver_nr = noncncpt.ver_nr
+;
 
+drop materialized view vw_cncpt;
+
+  CREATE MATERIALIZED VIEW VW_CNCPT 
+  AS SELECT ADMIN_ITEM.ITEM_ID, ADMIN_ITEM.VER_NR, ADMIN_ITEM.ITEM_NM,  ADMIN_ITEM.ITEM_LONG_NM, ADMIN_ITEM.ITEM_DESC, ADMIN_ITEM.CREATION_DT, 
+ADMIN_ITEM.EFF_DT, ADMIN_ITEM.ORIGIN,  ADMIN_ITEM.UNTL_DT,  ADMIN_ITEM.CURRNT_VER_IND, ADMIN_ITEM.REGSTR_STUS_NM_DN, ADMIN_ITEM.ADMIN_STUS_NM_DN, ADMIN_ITEM.CNTXT_ITEM_ID,
+ADMIN_ITEM.CNTXT_VER_NR, ADMIN_ITEM.CNTXT_NM_DN,
+ ADMIN_ITEM.CREAT_DT, ADMIN_ITEM.CREAT_USR_ID, ADMIN_ITEM.LST_UPD_USR_ID, ADMIN_ITEM.FLD_DELETE, ADMIN_ITEM.LST_DEL_DT, ADMIN_ITEM.S2P_TRN_DT, ADMIN_ITEM.LST_UPD_DT, CNCPT.PRMRY_CNCPT_IND,
+nvl(decode(trim(ADMIN_ITEM.DEF_SRC), 'NCI', '1-NCI', ADMIN_ITEM.DEF_SRC), 'No Def Source') DEF_SRC, e.CNCPT_CONCAT_WITH_INT,
+		      decode(trim(OBJ_KEY.OBJ_KEY_DESC), 'NCI_CONCEPT_CODE', '1-NCC', 'NCI_META_CUI', '2-NMC', OBJ_KEY.OBJ_KEY_DESC) EVS_SRC,
+		      trim(OBJ_KEY.OBJ_KEY_DESC) EVS_SRC_ORI,
+			cncpt.evs_Src_id,
+		      nvl(decode(trim(ADMIN_ITEM.DEF_SRC), 'NCI', '1-NCI', ADMIN_ITEM.DEF_SRC), 'No Def Source') || '|' ||
+		      nvl(decode(trim(OBJ_KEY.OBJ_KEY_DESC), 'NCI_CONCEPT_CODE', '1-NCC', 'NCI_META_CUI', '2-NMC', OBJ_KEY.OBJ_KEY_DESC), 'No EVS Source') DEF_EVS_SRC,
+        substr(ADMIN_ITEM.ITEM_NM || ' | ' || a.SYN, 1, 4000) SYN,substr(a.SYN_MTCH_TERM,1,4000) SYN_MTCH_TERM, MTCH_TERM, MTCH_TERM_ADV
+              FROM ADMIN_ITEM,  CNCPT, nci_admin_item_ext e, OBJ_KEY, (select item_id, ver_nr,   LISTAGG(nm_desc, ' | ') WITHIN GROUP (ORDER by ITEM_ID) AS SYN,
+			        LISTAGG(MTCH_TERM_ADV , ' | ') WITHIN GROUP (ORDER by ITEM_ID) AS SYN_MTCH_TERM from ALT_NMS a group by item_id, ver_nr) a
+       WHERE ADMIN_ITEM_TYP_ID = 49 and ADMIN_ITEM.ITEM_ID = CNCPT.ITEM_ID and ADMIN_ITEM.VER_NR = CNCPT.VER_NR
+       and admin_item.item_id = e.item_id and admin_item.ver_nr = e.ver_nr
+	and cncpt.EVS_SRC_ID = OBJ_KEY.OBJ_KEY_ID (+) and admin_item.admin_stus_nm_dn = 'RELEASED' 
+	and ADMIN_ITEM.ITEM_ID = a.ITEM_ID (+) and ADMIN_ITEM.VER_NR = a.VER_NR (+);
 
 
   CREATE OR REPLACE  VIEW VW_NCI_DE_CNCPT AS
