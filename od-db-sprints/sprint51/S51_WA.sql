da1141d20d20a3e816d3596717628692033957e6
deepalimanathara <deepali.manathara@gmail.com>
2023-12-10 12:39:28 -0500

    Update S51_WA.sql

--- od-db-sprints/sprint51/S51_WA.sql
+++ od-db-sprints/sprint51/S51_WA.sql
@@ -9,3 +9,12 @@ commit;
 insert into obj_key (OBJ_KEY_DESC, OBJ_TYP_ID, OBJ_KEY_DEF) select 'ICD-O_CODE',23,'ICD-O_CODE'  from dual where ('ICD-O_CODE') not in 
 (Select  upper(obj_key_desc) from obj_key where obj_typ_id = 23) ;
 commit;
+
+CREATE OR REPLACE TRIGGER OD_TR_NCI_STG_MEC_MAP_UPD 
+BEFORE UPDATE ON NCI_STG_MEC_MAP
+for each row
+BEGIN
+  :new.DT_SORT := systimestamp();
+  :new.lst_upd_dt := sysdate();
+END;
+/
