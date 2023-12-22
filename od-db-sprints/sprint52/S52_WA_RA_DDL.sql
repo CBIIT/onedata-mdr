186608f67ad5dfe0092029f4e8457c21a06bb605
deepalimanathara <deepali.manathara@gmail.com>
2023-12-22 13:56:24 -0500

    Update S52_WA_RA_DDL.sql

--- od-db-sprints/sprint52/S52_WA_RA_DDL.sql
+++ od-db-sprints/sprint52/S52_WA_RA_DDL.sql
@@ -1,3 +1,5 @@
 alter table NCI_STG_MEC_MAP add (DT_LAST_MODIFIED varchar2(32));
 
 alter table NCI_MDL_ELMNT_CHAR add (CHAR_ORD integer);
+
+alter table NCI_STG_MDL_ELMNT_CHAR add (CHAR_ORD integer);
