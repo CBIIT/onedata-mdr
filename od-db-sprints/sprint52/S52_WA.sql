3be6b6a733b7f22cec146f4e5534feb6f5babd27
deepalimanathara <deepali.manathara@gmail.com>
2024-01-17 20:44:41 -0500

    Update S52_WA.sql

--- od-db-sprints/sprint52/S52_WA.sql
+++ od-db-sprints/sprint52/S52_WA.sql
@@ -78,3 +78,21 @@ insert into obj_key (obj_typ_Id, obj_key_desc) values (54, 'IGNORE');
 insert into obj_key (obj_typ_Id, obj_key_desc) values (54, 'DEFAULT');
 
 commit;
+
+
+drop view VW_ADMIN_ITEM_WITH_EXT
+
+CREATE MATERIALIZED VIEW VW_ADMIN_ITEM_WITH_EXT AS
+  select ai.ITEM_ID, ai.VER_NR, ai.ITEM_DESC, ai.ITEM_LONG_NM, ai.ITEM_NM, ai.CHNG_DESC_TXT, 
+ decode(ai.ADMIN_ITEM_TYP_ID , 49, 'Concept', 53, 'Value Meaning') ADMIN_ITEM_TYP_ID ,ai.CURRNT_VER_IND,
+ ai.CREAT_DT, ai.CREAT_USR_ID, ai.LST_UPD_USR_ID, ai.FLD_DELETE, ai.LST_DEL_DT, ai.S2P_TRN_DT, ai.LST_UPD_DT, 
+ ai.ADMIN_STUS_NM_DN, ai.CNTXT_NM_DN, ai.REGSTR_STUS_NM_DN, e.cncpt_concat_src_typ evs_src_ori,
+decode(e.CNCPT_CONCAT,e.cncpt_concat_nm, null, e.cncpt_concat)  cncpt_concat, e.CNCPT_CONCAT_NM, e.CNCPT_CONCAT_DEF
+from admin_item ai, nci_admin_item_Ext e where ai.item_id = e.item_id and ai.ver_nr = e.ver_nr
+and ai.admin_item_typ_id in (49,53)
+	  union
+	    select 0,1, 'No NCIt Concept Associated',  'NA', 'No NCIt Concept Associated', '',
+'Concept' ,1, 
+sysdate, 'ONEDATA', 'ONEDATA', 0,sysdate, sysdate ,sysdate,
+ 'DRAFT-NEW', 'NA', 'Application', 'NA',
+'NA'  , 'NA', 'NA' from dual;
