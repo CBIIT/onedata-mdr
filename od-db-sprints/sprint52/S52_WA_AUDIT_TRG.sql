2f608904304e603f2d009745c341f5c2016d05f5
deepalimanathara <deepali.manathara@gmail.com>
2024-01-22 13:04:55 -0500

    Update S52_WA_AUDIT_TRG.sql

--- od-db-sprints/sprint52/S52_WA_AUDIT_TRG.sql
+++ od-db-sprints/sprint52/S52_WA_AUDIT_TRG.sql
@@ -19,4 +19,60 @@ END;
 /
 
 
- 
+create or replace TRIGGER TR_NCI_STG_MDL_ELMNT_BEF_UPD
+  BEFORE  UPDATE
+  on NCI_STG_MDL_ELMNT
+  for each row
+BEGIN
+  :new.LST_UPD_DT := SYSDATE;
+
+END;
+/
+
+   
+create or replace TRIGGER TR_NCI_STG_MDL_ELMNT_CHAR_BEF_UPD
+  BEFORE  UPDATE
+  on NCI_STG_MDL_ELMNT_CHAR
+  for each row
+BEGIN
+  :new.LST_UPD_DT := SYSDATE;
+
+END;
+/
+   
+create or replace TRIGGER TR_NCI_PROC_EXEC_BEF_UPD
+  BEFORE  UPDATE
+  on NCI_PROC_EXEC
+  for each row
+BEGIN
+  :new.LST_UPD_DT := SYSDATE;
+  :new.START_DT := SYSDATE;
+
+END;
+/
+
+create or replace TRIGGER OD_TR_DS_UPD 
+BEFORE UPDATE ON NCI_DS_HDR
+for each row
+BEGIN
+  :new.DT_LAST_MODIFIED := TO_CHAR(SYSDATE, 'MM/DD/YY HH24:MI:SS');
+  :new.DT_SORT := systimestamp();
+  :new.LST_UPD_DT := SYSDATE;
+
+END;
+/
+   
+create or replace TRIGGER OD_TR_ALT_NMS_UPD 
+BEFORE UPDATE ON NCI_STG_ALT_NMS 
+for each row
+BEGIN
+:new.DT_LAST_MODIFIED := TO_CHAR(SYSDATE, 'MM/DD/YY HH24:MI:SS');
+:new.DT_SORT := systimestamp();
+  :new.LST_UPD_DT := SYSDATE;
+
+END;
+
+/
+
+
+
