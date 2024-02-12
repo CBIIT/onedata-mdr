d03a7c6e7b5eabf85dfd21f681863fd9afe1b165
deepalimanathara <deepali.manathara@gmail.com>
2024-02-12 13:54:22 -0500

    Update S52_WA.sql

--- od-db-sprints/sprint52/S52_WA.sql
+++ od-db-sprints/sprint52/S52_WA.sql
@@ -64,7 +64,7 @@ create or replace TRIGGER TR_NCI_MDL_MAP_NM
   for each row
 BEGIN
    update ADMIN_ITEM set ITEM_NM_CURATED = item_nm || ' | ' || (Select s.item_nm || 'v' || :new.src_mdl_ver_nr || ' -> ' || t.item_nm || 'v' || :new.tgt_mdl_ver_nr || '|' || :new.item_id || '|' || :new.ver_nr
-   from admin_item s, admin_item t where s.item_id = :new.src_mdl_item_id and s.ver_nr = :new.src_mdl_ver_nr and t.item_id = :new.tgt_mdl_item_id and t.ver_nr = :new.tgt_mdl_ver_nr) || ' | ' || item_id || 'v' || ver_nr
+   from admin_item s, admin_item t where s.item_id = :new.src_mdl_item_id and s.ver_nr = :new.src_mdl_ver_nr and t.item_id = :new.tgt_mdl_item_id and t.ver_nr = :new.tgt_mdl_ver_nr)
    where item_id = :new.item_id and ver_nr = :new.ver_nr;
   -- commit;
 
