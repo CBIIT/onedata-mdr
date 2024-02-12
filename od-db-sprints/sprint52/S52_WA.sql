4707a107d0b3551c5b662f5a99e2a1357f86b126
deepalimanathara <deepali.manathara@gmail.com>
2024-02-12 14:01:53 -0500

    Update S52_WA.sql

--- od-db-sprints/sprint52/S52_WA.sql
+++ od-db-sprints/sprint52/S52_WA.sql
@@ -63,7 +63,7 @@ create or replace TRIGGER TR_NCI_MDL_MAP_NM
   on NCI_MDL_MAP
   for each row
 BEGIN
-   update ADMIN_ITEM set ITEM_NM_CURATED = item_nm || ' | ' || (Select s.item_nm || 'v' || :new.src_mdl_ver_nr || ' -> ' || t.item_nm || 'v' || :new.tgt_mdl_ver_nr || '|' || :new.item_id || '|' || :new.ver_nr
+   update ADMIN_ITEM set ITEM_NM_CURATED = item_nm || ' | ' || (Select s.item_nm || 'v' || :new.src_mdl_ver_nr || ' -> ' || t.item_nm || 'v' || :new.tgt_mdl_ver_nr || ' | ' || :new.item_id || '|' || :new.ver_nr
    from admin_item s, admin_item t where s.item_id = :new.src_mdl_item_id and s.ver_nr = :new.src_mdl_ver_nr and t.item_id = :new.tgt_mdl_item_id and t.ver_nr = :new.tgt_mdl_ver_nr)
    where item_id = :new.item_id and ver_nr = :new.ver_nr;
   -- commit;
@@ -72,7 +72,7 @@ END;
 /
 
 
-update ADMIN_ITEM set ITEM_NM_CURATED = item_nm ||' | ' || (Select s.item_nm || 'v' || s.ver_nr || ' mapped to ' || t.item_nm || 'v' || t.ver_nr || '|' || m.item_id || '|' || m.ver_nr
+update ADMIN_ITEM set ITEM_NM_CURATED = item_nm ||' | ' || (Select s.item_nm || 'v' || s.ver_nr || ' mapped to ' || t.item_nm || 'v' || t.ver_nr || ' | ' || m.item_id || '|' || m.ver_nr
    from admin_item s, admin_item t, nci_mdl_map m where s.item_id = m.src_mdl_item_id and s.ver_nr = m.src_mdl_ver_nr and t.item_id = m.tgt_mdl_item_id 
    and t.ver_nr = m.tgt_mdl_ver_nr and m.item_id = admin_item.item_id and m.ver_nr = admin_item.ver_nr)
    where admin_item_typ_id = 58;
