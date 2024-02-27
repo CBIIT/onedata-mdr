61f62db0fd48b75f532b189bc81ec28755d179db
deepalimanathara <deepali.manathara@gmail.com>
2024-02-27 14:06:35 -0500

    Create S53_WA.sql

---
+++ od-db-sprints/sprint53/S53_WA.sql
@@ -0,0 +1,15 @@
+-- wa
+
+CREATE OR REPLACE TRIGGER TR_AI_WFS
+  BEFORE UPDATE or INSERT ON ADMIN_ITEM
+  FOR EACH ROW
+     WHEN ( -- WFS is like retired requires End Date --
+  ((DECODE(new.ADMIN_STUS_ID,old.ADMIN_STUS_ID,0,1)=1 and (new.ADMIN_STUS_ID > 76 and new.ADMIN_STUS_ID < 82)) and --WFS is changed to like Retired
+   (new.UNTL_DT is null)) --new End Date is null error
+    or --WFS was like Retired and is not changed, End Date shall not be removed
+  (DECODE(new.ADMIN_STUS_ID,old.ADMIN_STUS_ID,0,1)=0 and (old.ADMIN_STUS_ID is not null and old.ADMIN_STUS_ID > 76 and old.ADMIN_STUS_ID < 82) and
+  (DECODE(new.UNTL_DT, old.UNTL_DT,0,1)=1 and new.UNTL_DT is null) ---changed End Data to n errorull
+  )) BEGIN
+RAISE_APPLICATION_ERROR( -20001,'!!!! The item is not save "RETIRED" Workflow Status requires "End Date" value to be set !!!!');
+END TR_AI_WFS;
+/
