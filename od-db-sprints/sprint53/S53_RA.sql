e8eaccc1ea62d29ed00cf7273f7f84f36572d1fa
deepalimanathara <deepali.manathara@gmail.com>
2024-03-07 12:07:39 -0500

    Update S53_RA.sql

--- od-db-sprints/sprint53/S53_RA.sql
+++ od-db-sprints/sprint53/S53_RA.sql
@@ -3,3 +3,6 @@
 --RA
 insert into obj_key select * from onedata_wa.obj_key where obj_typ_id = 55;
 commit;
+
+insert into obj_key select * from onedata_wa.obj_key where obj_key_desc='OMOP_CODE';
+commit;
