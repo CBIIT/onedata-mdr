00daf2b11c3f13499721114bc283046eec73fc9d
deepalimanathara <deepali.manathara@gmail.com>
2024-03-22 12:34:26 -0400

    Create S54_WA.sql

---
+++ od-db-sprints/sprint54/S54_WA.sql
@@ -0,0 +1,20 @@
+create table SAG_LOAD_NCIt_TERM
+(
+ Subset_Code	varchar2(100) not null,
+ Subset_Name	varchar2(4000) null,
+ Concept_Code	varchar2(100) null,
+ NCIT_PT	varchar2(4000) null,
+ Rel_to_Target  varchar2(100) null,
+ Target_Code	varchar2(100) not null,
+ Target_Term	varchar2(4000) null,
+ Target_Term_type	varchar2(100) null,
+ Target_Terminology  	varchar2(100) not null,
+ Target_Terminology_Version  	varchar2(100) null,Target_term_Definition varchar2(8000),
+ Target_Synonym varchar2(8000));
+
+delete from nci_admin_item_xmap where dt_src='ICDO';
+commit;
+
+exec nci_ext_import.load_term('UBERON','UBERON');
+
+exec nci_ext_import.load_term('ICDO3','ICD-O');
