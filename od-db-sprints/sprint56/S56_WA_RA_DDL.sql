cceea3832d222dae03b9dd2e13fa965b1390f792
deepalimanathara <deepali.manathara@gmail.com>
2024-05-23 13:35:36 -0400

    Update S56_WA_RA_DDL.sql

--- od-db-sprints/sprint56/S56_WA_RA_DDL.sql
+++ od-db-sprints/sprint56/S56_WA_RA_DDL.sql
@@ -80,8 +80,8 @@ VALUE_DOM.VER_NR = TERM.VER_NR (+);
 	"PERM_VAL_END_DT" DATE, 
 	"PERM_VAL_NM" VARCHAR2(255 BYTE) COLLATE "USING_NLS_COMP" NOT NULL ENABLE, 
 	"PERM_VAL_DESC_TXT" VARCHAR2(2000 BYTE) COLLATE "USING_NLS_COMP", 
-	"VAL_DOM_ITEM_ID" NUMBER NOT NULL ENABLE, 
-	"VAL_DOM_VER_NR" NUMBER(4,2) NOT NULL ENABLE, 
+	"CDE_ITEM_ID" NUMBER NOT NULL ENABLE, 
+	"CDE_VER_NR" NUMBER(4,2) NOT NULL ENABLE, 
 	"VAL_ID" NUMBER NOT NULL ENABLE, 
 	"CREAT_DT" DATE DEFAULT sysdate, 
 	"CREAT_USR_ID" VARCHAR2(50 BYTE) COLLATE "USING_NLS_COMP" DEFAULT user, 
@@ -114,16 +114,15 @@ VALUE_DOM.VER_NR = TERM.VER_NR (+);
         VM.VER_NR = e.VER_NR and de.SUBSET_DESC <> 'Subset'
 	  union
 	 SELECT  pv.val_id,  PV.PERM_VAL_BEG_DT, PERM_VAL_END_DT, PERM_VAL_NM, PERM_VAL_DESC_TXT,
-              DE.ITEM_ID  DE_ITEM_ID,
-		DE.VER_NR DE_VER_NR, VM.ITEM_NM, VM.ITEM_LONG_NM,
+              pv.cde_ITEM_ID  DE_ITEM_ID,
+		pv.cde_VER_NR DE_VER_NR, VM.ITEM_NM, VM.ITEM_LONG_NM,
                 VM.ITEM_DESC, NCI_VAL_MEAN_ITEM_ID, NCI_VAL_MEAN_VER_NR,
                 decode(e.cncpt_concat, e.cncpt_concat_nm, null, vm.item_long_nm, null, vm.item_id, null, e.cncpt_concat) cncpt_concat, e.CNCPT_CONCAT_NM,
 		decode(e.cncpt_concat_with_int, e.cncpt_concat_nm, null, vm.item_long_nm, null, vm.item_id, null, e.cncpt_concat_with_int) cncpt_concat_with_int,
 		PV.CREAT_DT, PV.CREAT_USR_ID, PV.LST_UPD_USR_ID, PV.FLD_DELETE, PV.LST_DEL_DT, PV.S2P_TRN_DT, PV.LST_UPD_DT,
                 null, null, VM.ITEM_NM || ' ' || VM.ITEM_DESC  VM_SEARCH_STR ,'SUBSET' PV_TYP, de.VAL_DOM_ITEM_ID, de.val_dom_Ver_nr
        FROM  DE, PERM_VAL_SUBSET PV, ADMIN_ITEM VM, NCI_ADMIN_ITEM_EXT e where
-	PV.VAL_DOM_ITEM_ID = DE.VAL_DOM_ITEM_ID and
-	PV.VAL_DOM_VER_NR = DE.VAL_DOM_VER_NR and
+	  de.item_id = pv.cde_item_id and de.ver_nr = pv.cde_ver_nr and 
 	PV.NCI_VAL_MEAN_ITEM_ID = vm.ITEM_ID and
 	PV.NCI_VAL_MEAN_VER_NR = vm.VER_NR and
         VM.ITEM_ID = e.ITEM_ID and
