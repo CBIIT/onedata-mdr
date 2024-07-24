1b74452e191e2ca1e7f536cb5a58fb38e4bc817d
deepalimanathara <deepali.manathara@gmail.com>
2024-07-24 13:57:49 -0400

    Update S57_WA.sql

--- od-db-sprints/sprint57/S57_WA.sql
+++ od-db-sprints/sprint57/S57_WA.sql
@@ -7,3 +7,21 @@ BEGIN
 :new.DT_LST_MODIFIED := TO_CHAR(SYSDATE, 'MM/DD/YY HH24:MI:SS');
 END;
 /
+
+
+create or replace TRIGGER TR_NCI_MDL_ELMNT_CHAR
+  BEFORE  UPDATE
+  on NCI_MDL_ELMNT_CHAR
+  for each row
+BEGIN
+   :new.LST_UPD_DT := SYSDATE;
+   if (:new.CDE_ITEM_ID is null and :new.VAL_DOM_ITEM_ID is not null) then
+     :new.VAL_DOM_ITEM_ID := null;
+     :new.VAL_DOM_VER_NR := null;
+     :new.DE_CONC_ITEM_ID := null;
+     :new.DE_CONC_VER_NR := null;
+     
+   end if;
+
+END;
+/
