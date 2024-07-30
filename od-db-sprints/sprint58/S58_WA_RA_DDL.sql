3ad4d155bea19c619db59c1f879c1d7fbe0be2fb
deepalimanathara <deepali.manathara@gmail.com>
2024-07-30 10:59:06 -0400

    Update S58_WA_RA_DDL.sql

--- od-db-sprints/sprint58/S58_WA_RA_DDL.sql
+++ od-db-sprints/sprint58/S58_WA_RA_DDL.sql
@@ -9,3 +9,16 @@ insert into obj_key(obj_typ_id, obj_key_id, obj_key_desc) values (61,138, 'No Ma
 commit
 
 alter table NCI_MEC_MAP add (PCODE_SYSGEN varchar2(4000));
+
+
+  CREATE OR REPLACE  VIEW VW_NCI_MDL AS
+  select
+	ai."DATA_ID_STR",ai."ITEM_ID",ai."VER_NR",ai."CLSFCTN_SCHM_ID",
+    ai."ITEM_DESC",ai."CNTXT_ITEM_ID",ai."CNTXT_VER_NR",ai."ITEM_LONG_NM",ai."ITEM_NM",ai."ADMIN_NOTES",
+    ai."CHNG_DESC_TXT",ai."EFF_DT",ai."ORIGIN",ai."UNRSLVD_ISSUE",ai."UNTL_DT",ai."CLSFCTN_SCHM_VER_NR",ai."ADMIN_ITEM_TYP_ID",
+    ai."CURRNT_VER_IND",ai."ADMIN_STUS_ID",ai."REGSTR_STUS_ID",ai."CREAT_DT",
+    ai."CREAT_USR_ID",ai."LST_UPD_USR_ID",ai."FLD_DELETE",ai."LST_DEL_DT",ai."S2P_TRN_DT",ai."LST_UPD_DT",
+    ai."ADMIN_STUS_NM_DN",ai."CNTXT_NM_DN",ai."REGSTR_STUS_NM_DN",ai."ORIGIN_ID",ai."ORIGIN_ID_DN",ai."CREAT_USR_ID_X",ai."LST_UPD_USR_ID_X",
+    ai."ITEM_NM_CURATED",ai."ITEM_NM_ID_VER", m.prmry_mdl_lang_id ,
+    m.map_usg_Typ_id from admin_item ai, nci_mdl m where ai.item_id = m.item_id and ai.ver_nr = m.ver_nr and ai.admin_item_typ_id = 57;
+
