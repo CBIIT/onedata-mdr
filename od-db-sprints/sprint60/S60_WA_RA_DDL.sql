9c136199c55ee330980bdef866d97c6a12924b81
deepalimanathara <deepali.manathara@gmail.com>
2024-09-26 13:52:44 -0400

    Create S60_WA_RA_DDL.sql

---
+++ od-db-sprints/sprint60/S60_WA_RA_DDL.sql
@@ -0,0 +1,84 @@
+
+  CREATE OR REPLACE  VIEW VW_VAL_MAP_IMP_TEMPLATE AS
+  select  ' ' "DO_NOT_USE",
+       ' ' "BATCH_USER",
+       ' ' "BATCH_NAME",
+       rownum "SEQ_ID",
+       map.mdl_map_item_id "MODEL_MAP_ID",
+       map.mdl_map_ver_nr "MODEL_MAP_VERSION",
+        map.mecm_id "MECM_ID", 
+        smec.char_ord SRC_CHAR_ORD,
+	sme.ITEM_PHY_OBJ_NM "SRC_ELMNT_PHY_NAME", 
+	sme.ITEM_LONG_NM "SRC_ELMNT_NAME", 
+  smec."MEC_PHY_NM" "SRC_PHY_NAME", 
+	smec.MEC_LONG_NM "SRC_MEC_NAME", 
+	  tme.ITEM_PHY_OBJ_NM "TGT_ELMNT_PHY_NAME", 
+	 tme.ITEM_LONG_NM "TGT_ELMNT_NAME", 
+	  tmec."MEC_PHY_NM" "TGT_PHY_NAME", 
+   	tmec.MEC_LONG_NM "TGT_MEC_NAME", 
+       tmec.char_ord TGT_CHAR_ORD,
+          map.MEC_MAP_NM "MAPPING_GROUP_NAME",
+          map.MEC_MAP_DESC "MAPPING_GROUP_DESC",
+	  src_func.obj_key_Desc "SOURCE_FUNCTION",
+	  tgt_func.obj_key_Desc "TARGET_FUNCTION",
+	  deg.obj_key_Desc "MAPPING_DEGREE",
+	org.org_nm "PROV_ORG",
+' '	"PROV_CONTACT",
+	vmap.prov_rsn_txt "PROV_REVIEW_REASON",
+	vmap.prov_typ_rvw_txt "PROV_TYPE_OF_REVIEW",
+	vmap.PROV_RVW_DT "PROV_REVIEW_DATE",
+	vmap.prov_APRV_DT "PROV_APPROVAL_DATE",
+decode(nvl(map.VAL_MAP_CREATE_IND,0),1, 'Yes','No') "VALUE_MAP_GENERATED_IND",
+decode(svd.val_dom_typ_id,17, 'Enumerated',18, 'Non-enumerated',16, 'Enumerated by Reference')  "SOURCE_DOMAIN_TYPE",
+decode(tvd.val_dom_typ_id,17, 'Enumerated',18, 'Non-enumerated',16, 'Enumerated by Reference')  "TARGET_DOMAIN_TYPE",
+    vmap.SRC_PV,
+    vmap.TGT_PV,
+    vmap.SRC_LBL,
+    vmap.TGT_LBL, 
+    vmap.VM_CNCPT_CD,
+    vmap.VM_CNCPT_NM,
+	   map."CREAT_DT",
+	  map."CREAT_USR_ID",
+	  map."LST_UPD_USR_ID",
+	  map."FLD_DELETE",
+	  map."LST_DEL_DT",
+	  map."S2P_TRN_DT",
+	  map."LST_UPD_DT",
+      smec.MEC_ID SRC_MEC_ID,
+      smec.CDE_ITEM_ID  "SOURCE_CDE_ITEM_ID",
+      smec.CDE_VER_NR  "SOURCE_CDE_VER",
+      tmec.CDE_ITEM_ID  "TARGET_CDE_ITEM_ID",
+      tmec.CDE_VER_NR  "TARGET_CDE_VER",
+tmec.MEC_ID TGT_MEC_ID,
+svdrt.TERM_USE_NAME "SOURCE_REF_TERM_SOURCE",
+tvdrt.TERM_USE_NAME "TARGET_REF_TERM_SOURCE"
+    ' ' "CMNTS_DESC_TXT"
+	from  NCI_MDL_ELMNT sme,NCI_MDL_ELMNT_CHAR smec, NCI_MDL_ELMNT tme,NCI_MDL_ELMNT_CHAR tmec, nci_MEC_MAP map, 	NCI_MEC_VAL_MAP vmap
+	  NCI_MDL_ELMNT tmed,NCI_MDL_ELMNT_CHAR tmecd,
+	  obj_key deg,
+	  nci_org org,
+  	  value_dom svd,
+	  value_dom tvd,
+vw_val_dom_ref_term svdrt,
+vw_val_dom_ref_term tvdrt
+	where  sme.item_id (+)= smec.MDL_ELMNT_ITEM_ID
+	and sme.ver_nr (+)= smec.MDL_ELMNT_VER_NR and
+	 tme.item_id (+)= tmec.MDL_ELMNT_ITEM_ID
+	and tme.ver_nr (+)= tmec.MDL_ELMNT_VER_NR  and
+	 tmed.item_id (+)= tmecd.MDL_ELMNT_ITEM_ID
+	and tmed.ver_nr (+)= tmecd.MDL_ELMNT_VER_NR  and
+	   smec.MEC_ID (+)= map.SRC_MEC_ID and tmec.mec_id (+)= map.TGT_MEC_ID
+	  and tmecd.mec_id (+)= map.TGT_MEC_ID_DERV
+	  and map.map_deg = deg.obj_key_id (+)
+	  and map.prov_org_id = org.entty_id (+)
+	        and smec.val_dom_item_id = svd.item_id (+)
+	  and smec.val_dom_ver_nr = svd.ver_nr (+)
+	  and tmec.val_dom_item_id = tvd.item_id (+)
+ 	  and tmec.val_dom_ver_nr = tvd.ver_nr (+)
+          and svd.item_id = svdrt.item_id (+)
+          and svd.ver_nr = svdrt.ver_nr (+)
+          and tvd.item_id = tvdrt.item_id (+)
+          and tvd.ver_nr = tvdrt.ver_nr (+)
+    and map.MDL_MAP_ITEM_ID = vmap.MDL_MAP_ITEM_ID
+    and map.MDL_MAP_VER_NR = vmap.MDL_MAP_VER_NR;
+
