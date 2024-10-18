9974a5d761dc58de884262a6f7c8b155d64c0ae8
deepalimanathara <deepali.manathara@gmail.com>
2024-10-18 14:14:18 -0400

    Update S60_WA_RA_DDL.sql

--- od-db-sprints/sprint60/S60_WA_RA_DDL.sql
+++ od-db-sprints/sprint60/S60_WA_RA_DDL.sql
@@ -1,12 +1,12 @@
-  CREATE OR REPLACE  VIEW VW_VAL_MAP_IMP_TEMPLATE AS
+
+  CREATE OR REPLACE   VIEW "VW_VAL_MAP_IMP_TEMPLATE"  AS
   select  ' ' "DO_NOT_USE",
        ' ' "BATCH_USER",
        ' ' "BATCH_NAME",
        rownum "SEQ_ID",
-       map.mdl_map_item_id "MODEL_MAP_ID",
-       map.mdl_map_ver_nr "MODEL_MAP_VERSION",
+       vmap.mdl_map_item_id "MODEL_MAP_ID",
+       vmap.mdl_map_ver_nr "MODEL_MAP_VERSION",
         vmap.mecvm_id "MECVM_ID", 
-        map.mecm_id,
    	sme.ITEM_PHY_OBJ_NM "SRC_ELMNT_PHY_NAME", 
 	sme.ITEM_LONG_NM "SRC_ELMNT_NAME", 
   smec."MEC_PHY_NM" "SRC_PHY_NAME", 
@@ -22,7 +22,6 @@
 	vmap.prov_typ_rvw_txt "PROV_TYPE_OF_REVIEW",
 	vmap.PROV_RVW_DT "PROV_REVIEW_DATE",
 	vmap.prov_APRV_DT "PROV_APPROVAL_DATE",
-decode(nvl(map.VAL_MAP_CREATE_IND,0),1, 'Yes','No') "VALUE_MAP_GENERATED_IND",
 decode(svd.val_dom_typ_id,17, 'Enumerated',18, 'Non-enumerated',16, 'Enumerated by Reference')  "SOURCE_DOMAIN_TYPE",
 decode(tvd.val_dom_typ_id,17, 'Enumerated',18, 'Non-enumerated',16, 'Enumerated by Reference')  "TARGET_DOMAIN_TYPE",
     vmap.SRC_PV,
@@ -31,51 +30,52 @@ decode(tvd.val_dom_typ_id,17, 'Enumerated',18, 'Non-enumerated',16, 'Enumerated 
     vmap.TGT_LBL, 
     vmap.VM_CNCPT_CD,
     vmap.VM_CNCPT_NM,
-	   map."CREAT_DT",
-	  map."CREAT_USR_ID",
-	  map."LST_UPD_USR_ID",
-	  map."FLD_DELETE",
-	  map."LST_DEL_DT",
-	  map."S2P_TRN_DT",
-	  map."LST_UPD_DT",
+	   vmap."CREAT_DT",
+	  vmap."CREAT_USR_ID",
+	  vmap."LST_UPD_USR_ID",
+	  vmap."FLD_DELETE",
+	  vmap."LST_DEL_DT",
+	  vmap."S2P_TRN_DT",
+	  vmap."LST_UPD_DT",
       smec.MEC_ID SRC_MEC_ID,
       smec.CDE_ITEM_ID  "SOURCE_CDE_ITEM_ID",
       smec.CDE_VER_NR  "SOURCE_CDE_VER",
       tmec.CDE_ITEM_ID  "TARGET_CDE_ITEM_ID",
       tmec.CDE_VER_NR  "TARGET_CDE_VER",
+       svd.ITEM_ID  "SOURCE_VD_ITEM_ID",
+      svd.VER_NR  "SOURCE_VD_VER",
+      tvd.ITEM_ID  "TARGET_VD_ITEM_ID",
+      tvd.VER_NR  "TARGET_VD_VER",
 tmec.MEC_ID TGT_MEC_ID,
 svdrt.TERM_USE_NAME "SOURCE_REF_TERM_SOURCE",
 tvdrt.TERM_USE_NAME "TARGET_REF_TERM_SOURCE",
     ' ' "CMNTS_DESC_TXT"
-	from  NCI_MDL_ELMNT sme,NCI_MDL_ELMNT_CHAR smec, NCI_MDL_ELMNT tme,NCI_MDL_ELMNT_CHAR tmec, nci_MEC_MAP map, 	NCI_MEC_VAL_MAP vmap,
+	from  NCI_MDL_ELMNT sme,NCI_MDL_ELMNT_CHAR smec, NCI_MDL_ELMNT tme,NCI_MDL_ELMNT_CHAR tmec,	NCI_MEC_VAL_MAP vmap,
 	  obj_key deg,
 	  nci_org org,
   	  value_dom svd,
 	  value_dom tvd,
 vw_val_dom_ref_term svdrt,
 vw_val_dom_ref_term tvdrt
-	where  sme.item_id (+)= smec.MDL_ELMNT_ITEM_ID
-	and sme.ver_nr (+)= smec.MDL_ELMNT_VER_NR and
-	 tme.item_id (+)= tmec.MDL_ELMNT_ITEM_ID
-	and tme.ver_nr (+)= tmec.MDL_ELMNT_VER_NR  and
-	   smec.MEC_ID (+)= map.SRC_MEC_ID and tmec.mec_id (+)= map.TGT_MEC_ID
+	where  sme.item_id = smec.MDL_ELMNT_ITEM_ID
+	and sme.ver_nr = smec.MDL_ELMNT_VER_NR and
+	 tme.item_id = tmec.MDL_ELMNT_ITEM_ID
+	and tme.ver_nr = tmec.MDL_ELMNT_VER_NR  
 	  and vmap.map_deg = deg.obj_key_id (+)
-	  and map.prov_org_id = org.entty_id (+)
-	        and smec.val_dom_item_id = svd.item_id (+)
-	  and smec.val_dom_ver_nr = svd.ver_nr (+)
-	  and tmec.val_dom_item_id = tvd.item_id (+)
- 	  and tmec.val_dom_ver_nr = tvd.ver_nr (+)
+	  and vmap.prov_org_id = org.entty_id (+)
+	        and smec.val_dom_item_id = svd.item_id 
+	  and smec.val_dom_ver_nr = svd.ver_nr 
+	  and tmec.val_dom_item_id = tvd.item_id 
+ 	  and tmec.val_dom_ver_nr = tvd.ver_nr 
           and svd.item_id = svdrt.item_id (+)
           and svd.ver_nr = svdrt.ver_nr (+)
           and tvd.item_id = tvdrt.item_id (+)
           and tvd.ver_nr = tvdrt.ver_nr (+)
-    and map.MDL_MAP_ITEM_ID = vmap.MDL_MAP_ITEM_ID
-    and map.MDL_MAP_VER_NR = vmap.MDL_MAP_VER_NR
-    and map.SRC_MEC_ID = vmap.SRC_MEC_ID
-    and map.tgt_mec_id = vmap.tgt_mec_id;
-
-
-create table NCI_MDL_MAP_CNFG (
+          and vmap.src_mec_id = smec.mec_id
+          and vmap.tgt_mec_id = tmec.mec_id
+	  
+	  
+	  create table NCI_MDL_MAP_CNFG (
 CNFG_ID  number,
 MDL_MAP_ITEM_ID  number,
 MDL_MAP_VER_NR number(4,2),
