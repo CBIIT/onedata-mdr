bc616ae027e457f817d7a83a1f482b9bf84a721d
deepalimanathara <deepali.manathara@gmail.com>
2024-11-06 11:48:48 -0500

    Update S61_WA_RA_DDL.sql

--- od-db-sprints/sprint61/S61_WA_RA_DDL.sql
+++ od-db-sprints/sprint61/S61_WA_RA_DDL.sql
@@ -2,6 +2,6 @@ alter table sag_load_mt add (syn_match number(1));
 
 alter table sag_load_mt add (item_id number, ver_nr number(4,2));
 
-alter table nci_mec_map (add SRC_CDE_ITEM_ID number, SRC_CDE_VER_NR number(4,2), TGT_CDE_ITEM_ID number, TGT_CDE_VER_NR number(4,2));
+alter table nci_mec_map add (SRC_CDE_ITEM_ID number, SRC_CDE_VER_NR number(4,2), TGT_CDE_ITEM_ID number, TGT_CDE_VER_NR number(4,2));
 
 
