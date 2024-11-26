b93e3130be8ab1d191290fcc49194280795a1c42
matt-nash-nih <85249953+matt-nash-nih@users.noreply.github.com>
2024-11-26 10:58:23 -0500

    Update S61_WA_RA_DDL.sql

--- od-db-sprints/sprint61/S61_WA_RA_DDL.sql
+++ od-db-sprints/sprint61/S61_WA_RA_DDL.sql
@@ -96,3 +96,44 @@ create unique index idx_vw_cncpt on vw_cncpt (item_id, ver_nr);
 alter table nci_mec_val_map add (SRC_VM_CNCPT_CD varchar2(4000), SRC_VM_CNCPT_NM  varchar2(4000), TGT_VM_CNCPT_CD varchar2(4000), TGT_VM_CNCPT_NM  varchar2(4000), SRC_VD_ITEM_ID number, SRC_VD_VER_NR number(4,2), TGT_VD_ITEM_ID number,
 	TGT_VD_VER_NR number(4,2),  SRC_CDE_ITEM_ID number, SRC_CDE_VER_NR number(4,2), TGT_CDE_ITEM_ID number,
 	TGT_CDE_VER_NR number(4,2));
+
+--jira 3605
+update obj_key set obj_key_desc = 'Data Collection Excel File' where obj_key_id = 230;
+update obj_key set obj_key_def = 'Data Collection Excel File' where obj_key_id = 230;
+commit;
+
+--jira 3701
+alter table cncpt add (item_code varchar2(30));
+
+--jira 3703
+alter table nci_dload_cstm_data_coll add ALT_NM_TYP_ID number(*,0);
+
+--jira 3669
+alter table nci_stg_definition add IMP_ALT_DEF varchar(4000);
+alter table nci_stg_definition add IMP_ALT_DEF_TYP number;
+alter table nci_stg_definition add IMP_ALT_DEF_CNTXT_ID number;
+alter table nci_stg_definition add IMP_ALT_DEF_CNTXT_VER number(4,2);
+alter table nci_stg_definition add IMP_ALT_DEF_LANG_ID number;
+
+--jira 3658
+  CREATE TABLE NCI_DLOAD_NIH_SUB_TEMP 
+   (	"HDR_ID" NUMBER, 
+	"TYP_IND" NUMBER DEFAULT null, 
+	"CREAT_DT" DATE DEFAULT sysdate, 
+	"CREAT_USR_ID" VARCHAR2(50 BYTE) COLLATE "USING_NLS_COMP" DEFAULT user, 
+	"LST_UPD_USR_ID" VARCHAR2(50 BYTE) COLLATE "USING_NLS_COMP" DEFAULT user, 
+	"FLD_DELETE" NUMBER(1,0) DEFAULT 0, 
+	"LST_DEL_DT" DATE DEFAULT sysdate, 
+	"S2P_TRN_DT" DATE DEFAULT sysdate, 
+	"LST_UPD_DT" DATE DEFAULT sysdate, 
+	 PRIMARY KEY ("HDR_ID")
+);
+
+  GRANT SELECT ON NCI_DLOAD_NIH_SUB_TEMP TO "ONEDATA_RO";
+  
+insert into obj_typ (obj_typ_id, obj_typ_desc) values (62, 'UMLS-Concept Indicator');
+commit;
+
+insert into obj_key (obj_key_id, obj_typ_id, obj_key_desc, obj_key_def) values (231, 62, 'UMLS CUIs', 'UMLS CUIs');
+insert into obj_key (obj_key_id, obj_typ_id, obj_key_desc, obj_key_def) values (232, 62, 'Existing Concepts', 'Existing Concepts');
+commit;
