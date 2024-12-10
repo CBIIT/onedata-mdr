3c45e2d8716936645f390b183631bddb45c5a195
deepalimanathara <deepali.manathara@gmail.com>
2024-12-10 16:07:02 -0500

    Update S62_WA.sql

--- od-db-sprints/sprint62/S62_WA.sql
+++ od-db-sprints/sprint62/S62_WA.sql
@@ -18,20 +18,20 @@ if (:new.TGT_FUNC_PARAM = 'SOURCE') then
 end if;
 end if;
 end;
-/
-create or replace TRIGGER TR_MEC_MAP_UPD  BEFORE UPDATE ON NCI_MEC_MAP for each row
+/create or replace TRIGGER TR_MEC_MAP_UPD  BEFORE UPDATE ON NCI_MEC_MAP for each row
 declare 
 v_src_nm varchar2(1000);
 v_param_Val varchar2(255);
 BEGIN   
 
-if (:new.src_mec_id is not null and :new.src_mec_id <> :old.src_mec_id) then
+if (nvl(:new.src_mec_id,0)<> nvl(:old.src_mec_id,0)) then
 for cur in (select cde_item_id, cde_ver_nr from nci_mdl_elmnt_char mec where mec_id = :new.src_mec_id) loop
 :new.src_cde_item_id := cur.cde_item_id;
 :new.src_cde_ver_nr := cur.cde_ver_nr;
 end loop;
 end if;
-if (:new.tgt_mec_id is not null and :new.tgt_mec_id <> :old.tgt_mec_id) then
+if (nvl(:new.tgt_mec_id,0) <> nvl(:old.tgt_mec_id,0)) then
+--raise_application_error(-20000,'here');
 for cur in (select cde_item_id, cde_ver_nr from nci_mdl_elmnt_char mec where mec_id = :new.tgt_mec_id) loop
 :new.tgt_cde_item_id := cur.cde_item_id;
 :new.tgt_cde_ver_nr := cur.cde_ver_nr;
