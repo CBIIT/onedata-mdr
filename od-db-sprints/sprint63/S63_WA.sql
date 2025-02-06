66517a5bc59e71749cea9c280c396615e347e770
deepalimanathara <deepali.manathara@gmail.com>
2025-02-06 14:57:44 -0500

    Update S63_WA.sql

--- od-db-sprints/sprint63/S63_WA.sql
+++ od-db-sprints/sprint63/S63_WA.sql
@@ -31,3 +31,25 @@ end if;
 
 end;
 /
+create or replace TRIGGER TR_MEC_MAP_INS  BEFORE INSERT ON NCI_MEC_MAP for each row
+declare 
+v_src_nm varchar2(1000);
+v_param_Val varchar2(255);
+BEGIN   
+
+if (:new.src_mec_id is not null and :new.src_cde_item_id is null)  then
+for cur in (select cde_item_id, cde_ver_nr from nci_mdl_elmnt_char mec where mec_id = :new.src_mec_id) loop
+:new.src_cde_item_id := cur.cde_item_id;
+:new.src_cde_ver_nr := cur.cde_ver_nr;
+end loop;
+end if;
+
+if (:new.tgt_mec_id is not null and :new.tgt_cde_item_id is null)  then
+for cur in (select cde_item_id, cde_ver_nr from nci_mdl_elmnt_char mec where mec_id = :new.tgt_mec_id) loop
+:new.tgt_cde_item_id := cur.cde_item_id;
+:new.tgt_cde_ver_nr := cur.cde_ver_nr;
+end loop;
+end if;
+
+end;
+/
