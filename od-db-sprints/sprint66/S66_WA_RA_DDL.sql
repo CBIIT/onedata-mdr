c990959924ebb194dde4e310a1b0e1409d09db6f
deepalimanathara <deepali.manathara@gmail.com>
2025-04-18 21:32:18 -0400

    Create S66_WA_RA_DDL.sql

---
+++ od-db-sprints/sprint66/S66_WA_RA_DDL.sql
@@ -0,0 +1,7 @@
+alter table nci_mec_map add (MEC_SUB_GRP_NBR_DUP number(6,2));
+alter table nci_mec_map disable all triggers;
+update nci_mec_map set MEC_SUB_GRP_NBR_DUP= MEC_SUB_GRP_NBR;
+commit;
+alter table nci_mec_map enable all triggers;
+alter table nci_mec_Map drop column  MEC_SUB_GRP_NBR;
+alter table nci_mec_map rename column mec_sub_grp_nbr_dup to MEC_SUB_GRP_NBR;
