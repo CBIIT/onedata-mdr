e93e5c0a8ccacb6f149bd3fa80c81d8005adabdf
deepalimanathara <deepali.manathara@gmail.com>
2025-06-03 11:50:28 -0400

    Update S67_WA_RA_DDL.sql

--- od-db-sprints/sprint67/S67_WA_RA_DDL.sql
+++ od-db-sprints/sprint67/S67_WA_RA_DDL.sql
@@ -57,28 +57,4 @@ alter table NCI_MEC_VAL_MAP add (IMP_SEQ_NBR number);
 insert into obj_key( obj_key_id, obj_key_desc, obj_typ_id, obj_key_def) values (244, 'Value Mapping Export with validation', 31,'Value Mapping Export with validation');
 commit;
 
-create or replace TRIGGER TR_NCI_MDL_MAP_SHORT_NM
-  AFTER INSERT 
-  on NCI_MDL_MAP
-  for each row
-BEGIN
-   update ADMIN_ITEM set MDL_MAP_SNAME = (Select substr(s.item_nm ||' '|| :new.src_mdl_item_id || 'v' || :new.src_mdl_ver_nr || ' -> ' || t.item_nm || ' ' || :new.tgt_mdl_item_id || 'v' || :new.tgt_mdl_ver_nr ,1,255)
-   from admin_item s, admin_item t where s.item_id = :new.src_mdl_item_id and s.ver_nr = :new.src_mdl_ver_nr and t.item_id = :new.tgt_mdl_item_id and t.ver_nr = :new.tgt_mdl_ver_nr)
-   where item_id = :new.item_id and ver_nr = :new.ver_nr;
-  -- commit;
-
-END;
-/
-
-CREATE OR REPLACE TRIGGER OD_TR_MEC_VAL_MAP_UPD 
-BEFORE INSERT OR UPDATE ON NCI_STG_MEC_VAL_MAP
-for each row
-BEGIN
-:new.DT_LAST_MODIFIED := TO_CHAR(SYSDATE, 'MM/DD/YY HH24:MI:SS');
-:new.DT_SORT := systimestamp();
-  :new.LST_UPD_DT := SYSDATE;
-
-END;
-/
-
 
