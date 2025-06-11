bc21f1cd1090db0dee56e39976a2d60cd5e50314
matt-nash-nih <85249953+matt-nash-nih@users.noreply.github.com>
2025-06-11 10:32:07 -0400

    Create S67_RA.sql

---
+++ od-db-sprints/sprint67/S67_RA.sql
@@ -0,0 +1,5 @@
+--jira 4091,4092
+     grant all on nci_dload_cstm_dec_subtyp to onedata_wa;
+     grant all on nci_dload_cstm_vd_subtyp to onedata_wa;
+     grant all on nci_dload_cstm_col_dtl_dec to onedata_wa;
+     grant all on nci_dload_cstm_col_dtl_vd to onedata_wa;
