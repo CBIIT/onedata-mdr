8f7eb9fc653d51a57a4c7390754381f158cf5fb8
matt-nash-nih <85249953+matt-nash-nih@users.noreply.github.com>
2025-06-11 10:44:18 -0400

    Jira 4114 RA

--- od-db-sprints/sprint67/S67_RA.sql
+++ od-db-sprints/sprint67/S67_RA.sql
@@ -1,5 +1,9 @@
 --jira 4091,4092
-     grant all on nci_dload_cstm_dec_subtyp to onedata_wa;
-     grant all on nci_dload_cstm_vd_subtyp to onedata_wa;
-     grant all on nci_dload_cstm_col_dtl_dec to onedata_wa;
-     grant all on nci_dload_cstm_col_dtl_vd to onedata_wa;
+grant all on nci_dload_cstm_dec_subtyp to onedata_wa;
+grant all on nci_dload_cstm_vd_subtyp to onedata_wa;
+grant all on nci_dload_cstm_col_dtl_dec to onedata_wa;
+grant all on nci_dload_cstm_col_dtl_vd to onedata_wa;
+
+--jira 4114
+grant all on nci_ds_dtl to onedata_wa;
+grant all on nci_dload_cstm_col_dtl to onedata_wa;
