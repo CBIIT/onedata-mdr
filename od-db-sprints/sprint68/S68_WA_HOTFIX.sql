31d71d5aebc8da805b295a57d1f4c161d2239013
deepalimanathara <deepali.manathara@gmail.com>
2025-08-07 11:01:22 -0400

    Update S68_WA_HOTFIX.sql

--- od-db-sprints/sprint68/S68_WA_HOTFIX.sql
+++ od-db-sprints/sprint68/S68_WA_HOTFIX.sql
@@ -18,3 +18,22 @@ commit;
 alter table ref enable all triggers;
 
 alter table nci_ds_rslt_dtl add (mtch_desc_txt varchar2(4000));
+
+
+create or replace TRIGGER OD_TR_DS_UPD 
+BEFORE UPDATE ON NCI_DS_HDR
+for each row
+BEGIN
+  :new.DT_LAST_MODIFIED := TO_CHAR(SYSDATE, 'MM/DD/YY HH24:MI:SS');
+  :new.DT_SORT := systimestamp();
+  :new.LST_UPD_DT := SYSDATE;
+
+if (:new.cde_item_id is null and :new.rule_desc is not null) then
+:new.rule_desc := null;
+:new.mtch_desc_txt := null;
+  end if;
+
+
+END;
+/
+
