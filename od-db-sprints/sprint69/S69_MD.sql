776607d7d79eaa8b437f262e2530c7b6b9beccf7
deepalimanathara <deepali.manathara@gmail.com>
2025-08-11 19:38:30 -0400

    Update S69_MD.sql

--- od-db-sprints/sprint69/S69_MD.sql
+++ od-db-sprints/sprint69/S69_MD.sql
@@ -18,3 +18,24 @@ update od_md_objcol set prop_val= 103 where prop_id = 812 and (obj_id, schm_id, 
 (select obj_id, schm_id, col_id from od_mdv_objcolhort where col_nm = 'LST_UPD_DT')
 and prop_val <> 103;
 commit;
+
+
+update od_md_objcol set prop_val= 'Created By' where prop_id = 802 and (obj_id, schm_id, col_id) in 
+(select obj_id, schm_id, col_id from od_mdv_objcolhort where col_nm = 'CREAT_USR_ID')
+and prop_val <> 'Created By';
+
+
+update od_md_objcol set prop_val= 'Date Created' where prop_id = 802 and (obj_id, schm_id, col_id) in 
+(select obj_id, schm_id, col_id from od_mdv_objcolhort where col_nm = 'CREAT_DT')
+and prop_val <> 'Date Created';
+
+
+update od_md_objcol set prop_val= 'Last Modified By' where prop_id = 802 and (obj_id, schm_id, col_id) in 
+(select obj_id, schm_id, col_id from od_mdv_objcolhort where col_nm = 'LST_UPD_USR_ID')
+and prop_val <> 'Last Modified By';
+
+
+update od_md_objcol set prop_val= 'Date Last Modified' where prop_id = 802 and (obj_id, schm_id, col_id) in 
+(select obj_id, schm_id, col_id from od_mdv_objcolhort where col_nm = 'LST_UPD_DT')
+and prop_val <> 'Date Last Modified';
+commit;
