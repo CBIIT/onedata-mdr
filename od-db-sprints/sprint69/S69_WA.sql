d21af61cfa5fe5ad615c6d8d03ab6fc7ed7fab8e
deepalimanathara <deepali.manathara@gmail.com>
2025-09-03 12:14:04 -0400

    Create S69_WA.sql

---
+++ od-db-sprints/sprint69/S69_WA.sql
@@ -0,0 +1,29 @@
+create or update TRIGGER TR_NCI_MDL_ELMNT_CHAR
+  BEFORE  UPDATE
+  on NCI_MDL_ELMNT_CHAR
+  for each row
+BEGIN
+   :new.LST_UPD_DT := SYSDATE;
+   if (:new.CDE_ITEM_ID is null and (:new.VAL_DOM_ITEM_ID is not null or :new.DE_CONC_ITEM_ID is not null)) then
+     :new.VAL_DOM_ITEM_ID := null;
+     :new.VAL_DOM_VER_NR := null;
+     :new.DE_CONC_ITEM_ID := null;
+     :new.DE_CONC_VER_NR := null;
+   :new.VD_TYP_ID := null;
+
+   end if;
+      if (nvl(:new.CDE_ITEM_ID,0) <> nvl(:old.cde_item_id,0) or  nvl(:new.CDE_VER_NR,0) <> nvl(:old.cde_VER_NR,0) ) then
+     for cur in (select * from de where item_id = :new.CDE_ITEM_ID and ver_nr = :new.cde_ver_nr) loop
+     :new.VAL_DOM_ITEM_ID := cur.VAL_DOM_ITEM_ID;
+     :new.VAL_DOM_VER_NR := cur.VAL_DOM_VER_NR;
+     :new.DE_CONC_ITEM_ID := cur.DE_CONC_ITEM_ID;
+     :new.DE_CONC_VER_NR := cur.DE_CONC_VER_NR;
+for cur1 in (select * from value_dom where item_id = cur.val_dom_item_id and ver_nr = cur.val_dom_ver_nr) loop
+  :new.vd_typ_id = cur1.val_dom_typ_id;
+end loop;
+end loop;
+  
+   end if;
+
+END;
+/
