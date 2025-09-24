45d528f637765b802eebd983f2c49e6b56df831c
deepalimanathara <deepali.manathara@gmail.com>
2025-09-24 14:50:28 -0400

    Create S70_WA.sql

---
+++ od-db-sprints/sprint70/S70_WA.sql
@@ -0,0 +1,17 @@
+create or replace TRIGGER TR_MDL_POST
+  AFTER  UPDATE
+  on NCI_MDL
+  for each row
+BEGIN
+    update ADMIN_ITEM set LST_UPD_DT = sysdate, LST_UPD_USR_ID = :new.LST_UPD_USR_ID where ITEM_ID = :new.item_id and VER_NR = :new.VER_NR;
+END;
+/
+
+create or replace TRIGGER TR_MDL_MAP_POST
+  AFTER  UPDATE
+  on NCI_MDL_MAP
+  for each row
+BEGIN
+    update ADMIN_ITEM set LST_UPD_DT = sysdate, LST_UPD_USR_ID = :new.LST_UPD_USR_ID where ITEM_ID = :new.item_id and VER_NR = :new.VER_NR;
+END;
+/
