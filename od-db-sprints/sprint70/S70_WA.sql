7da632b276d33edeb7c0ad67027e058e7fbf3948
deepalimanathara <deepali.manathara@gmail.com>
2025-10-08 20:54:54 -0400

    Update S70_WA.sql

--- od-db-sprints/sprint70/S70_WA.sql
+++ od-db-sprints/sprint70/S70_WA.sql
@@ -28,3 +28,41 @@ create or replace TRIGGER TR_AI_END_EFF_DT
         RAISE_APPLICATION_ERROR( -20001,'!!!! The item is not saved. "End Date" must be set later than "Effective Date"  !!!!');
 END TR_AI_END_EFF_DT;
 /
+
+
+CREATE OR REPLACE TRIGGER TR_NCI_MDL_ELMNT_CHAR
+  BEFORE  UPDATE
+  on NCI_MDL_ELMNT_CHAR
+  for each row
+BEGIN
+   :new.LST_UPD_DT := SYSDATE;
+   if (:new.CDE_ITEM_ID is null and (:new.VAL_DOM_ITEM_ID is not null or :new.DE_CONC_ITEM_ID is not null)) then
+     :new.VAL_DOM_ITEM_ID := null;
+     :new.VAL_DOM_VER_NR := null;
+     :new.DE_CONC_ITEM_ID := null;
+     :new.DE_CONC_VER_NR := null;
+  :new.OC_CNCPT_CD := null;
+  :new.PROP_CNCPT_CD := null;
+   :new.VD_TYP_ID := null;
+
+   end if;
+      if (nvl(:new.CDE_ITEM_ID,0) <> nvl(:old.cde_item_id,0) or  nvl(:new.CDE_VER_NR,0) <> nvl(:old.cde_VER_NR,0) ) then
+     for cur in (select * from de where item_id = :new.CDE_ITEM_ID and ver_nr = :new.cde_ver_nr) loop
+     :new.VAL_DOM_ITEM_ID := cur.VAL_DOM_ITEM_ID;
+     :new.VAL_DOM_VER_NR := cur.VAL_DOM_VER_NR;
+     :new.DE_CONC_ITEM_ID := cur.DE_CONC_ITEM_ID;
+     :new.DE_CONC_VER_NR := cur.DE_CONC_VER_NR;
+for cur1 in (select * from value_dom where item_id = cur.val_dom_item_id and ver_nr = cur.val_dom_ver_nr) loop
+  :new.vd_typ_id := cur1.val_dom_typ_id;
+end loop;
+for cur1 in (select e.cncpt_concat from nci_admin_item_ext e, de_conc dec where e.item_id = dec.obj_cls_item_id and e.ver_nr = dec.obj_cls_ver_nr and dec.item_id = cur.de_conc_item_id and dec.ver_nr = cur.de_conc_ver_nr) loop
+  :new.oc_cncpt_cd := cur1.cncpt_concat;
+end loop;
+for cur1 in (select e.cncpt_concat from nci_admin_item_ext e, de_conc dec where e.item_id = dec.prop_item_id and e.ver_nr = dec.prop_ver_nr and dec.item_id = cur.de_conc_item_id and dec.ver_nr = cur.de_conc_ver_nr) loop
+  :new.prop_cncpt_cd := cur1.cncpt_concat;
+end loop;
+end loop;
+   end if;
+
+END;
+/
