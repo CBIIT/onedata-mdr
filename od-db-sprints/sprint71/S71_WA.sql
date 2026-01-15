7cbcd50fca432fbeb482ce4aba1f8606c0662d2f
deepalimanathara <deepali.manathara@gmail.com>
2026-01-15 14:39:01 -0500

    Update S71_WA.sql

--- od-db-sprints/sprint71/S71_WA.sql
+++ od-db-sprints/sprint71/S71_WA.sql
@@ -95,6 +95,7 @@ BEGIN    IF (:NEW.NCI_PUB_ID<= 0  or :NEW.NCI_PUB_ID is null)  THEN
 drop trigger tr_nci_ref;
 drop trigger TR_NCI_ALT_NMS;
 drop trigger TR_NCI_ALT_DEF;
+drop trigger TR_NCI_PERM_VAL;
 
 CREATE OR REPLACE TRIGGER OD_TR_REF_DOC  BEFORE INSERT  on REF_DOC  for each row
                   BEGIN    IF (:NEW.REF_DOC_ID<= 0  or :NEW.REF_DOC_ID is null)  THEN 
@@ -102,6 +103,16 @@ CREATE OR REPLACE TRIGGER OD_TR_REF_DOC  BEFORE INSERT  on REF_DOC  for each row
             into :new.REF_DOC_ID  from  dual ;   END IF; 
 end;
 /
+
+CREATE OR REPLACE TRIGGER NCI_TR_ENTTY  BEFORE INSERT  on NCI_ENTTY  for each row
+     BEGIN    IF (:NEW.ENTTY_ID = -1  or :NEW.ENTTY_ID is null)  THEN select nci_seq_ENTTY.nextval
+ into :new.ENTTY_ID  from  dual ;   END IF;
+END ;
+/
+
+
+
+
 
 
         
