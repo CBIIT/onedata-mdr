f8b99f558e28d115b47ebc183c50e864c26426b5
deepalimanathara <deepali.manathara@gmail.com>
2026-01-15 14:21:39 -0500

    Update S71_WA.sql

--- od-db-sprints/sprint71/S71_WA.sql
+++ od-db-sprints/sprint71/S71_WA.sql
@@ -99,7 +99,7 @@ CREATE OR REPLACE TRIGGER OD_TR_REF_DOC  BEFORE INSERT  on REF_DOC  for each row
                   BEGIN    IF (:NEW.REF_DOC_ID<= 0  or :NEW.REF_DOC_ID is null)  THEN 
                   select od_seq_REF_DOC.nextval
             into :new.REF_DOC_ID  from  dual ;   END IF; 
-
+end;
 /
 
 
