d621229142f21e11b702bedc2dd9b40737c2a99d
deepalimanathara <deepali.manathara@gmail.com>
2026-02-26 22:07:03 -0500

    Update S71_EMERGENCY_FIX_2.sql

--- od-db-sprints/sprint71/S71_EMERGENCY_FIX_2.sql
+++ od-db-sprints/sprint71/S71_EMERGENCY_FIX_2.sql
@@ -1,18 +1,57 @@
 
-drop trigger TR_NCI_ALT_NMS;
-drop trigger TR_NCI_ALT_DEF;
-drop trigger TR_NCI_PERM_VAL;
+create or replace trigger TR_NCI_REF
+  BEFORE INSERT 
+  on REF
+  for each row
+BEGIN
+  if (:new.NCI_IDSEQ is null) then
+ :new.NCI_IDSEQ := nci_11179.cmr_guid();
+end if;
+END;
+/
+
+
+
+
+create or replace trigger TR_NCI_ALT_DEF
+  BEFORE INSERT 
+  on ALT_DEF
+  for each row
+BEGIN
+  if (:new.NCI_IDSEQ is null) then
+ :new.NCI_IDSEQ := nci_11179.cmr_guid();
+end if;
+END;
+/
+
+
+create or replace trigger TR_NCI_ALT_NMS
+  BEFORE INSERT  on ALT_NMS
+  for each row
+BEGIN
+   if (:new.NCI_IDSEQ is null) then
+ :new.NCI_IDSEQ := nci_11179.cmr_guid();
+end if;
+END;
+/
+
+create or replace trigger TR_NCI_PERM_VAL
+  BEFORE INSERT  on PERM_VAL
+  for each row
+BEGIN
+   if (:new.NCI_IDSEQ is null) then
+ :new.NCI_IDSEQ := nci_11179.cmr_guid();
+end if;
+END;
+/
+
 
 CREATE OR REPLACE TRIGGER OD_TR_REF_DOC  BEFORE INSERT  on REF_DOC  for each row
                   BEGIN    IF (:NEW.REF_DOC_ID<= 0  or :NEW.REF_DOC_ID is null)  THEN 
                   select od_seq_REF_DOC.nextval
             into :new.REF_DOC_ID  from  dual ;   END IF; 
-
-end;
+  
+                  :new.nci_idseq := nci_11179.cmr_guid(); 
+  END ;          
 /
 
-CREATE OR REPLACE TRIGGER NCI_TR_ENTTY  BEFORE INSERT  on NCI_ENTTY  for each row
-     BEGIN    IF (:NEW.ENTTY_ID = -1  or :NEW.ENTTY_ID is null)  THEN select nci_seq_ENTTY.nextval
- into :new.ENTTY_ID  from  dual ;   END IF;
-END ;
-/
