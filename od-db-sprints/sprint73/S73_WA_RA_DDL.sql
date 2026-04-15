be5e1c0e074a85ec612de513fa8fee4093fae92b
matt-nash-nih <85249953+matt-nash-nih@users.noreply.github.com>
2026-04-15 09:28:40 -0400

    Update S73_WA_RA_DDL.sql

--- od-db-sprints/sprint73/S73_WA_RA_DDL.sql
+++ od-db-sprints/sprint73/S73_WA_RA_DDL.sql
@@ -18,3 +18,31 @@ commit;
 alter table nci_ds_hdr modify (ENTTY_NM varchar2(4000), ENTTY_NM_USR varchar2(4000));
 
 alter table  nci_ds_prmtr modify (ENTTY_NM varchar2(4000), ENTTY_NM_USR varchar2(4000));
+
+insert into obj_typ (obj_typ_id, obj_typ_desc) values (65, 'AI Model Variant - CDE');
+insert into obj_typ (obj_typ_id , obj_typ_desc) values (66, 'AI Model Variant - PV');
+commit;
+
+insert into obj_key (obj_typ_id, obj_key_desc, obj_key_id) values (65, '',252);
+insert into obj_key (obj_typ_id, obj_key_desc, obj_key_id) values (65, 'All-mpnet-base-v2-FT', 252);
+insert into obj_key (obj_typ_id, obj_key_desc, obj_key_id) values (65, 'All-MiniLM-L6-v2-FT',253);
+insert into obj_key (obj_typ_id, obj_key_desc, obj_key_id) values (65, 'SapBERT-FT',254);
+insert into obj_key (obj_typ_id, obj_key_desc, obj_key_id) values (65, 'PubMedBERT-FT',255);
+insert into obj_key (obj_typ_id, obj_key_desc, obj_key_id) values (65, 'All-mpnet-base-v2',256);
+insert into obj_key (obj_typ_id, obj_key_desc, obj_key_id) values (65, 'All-MiniLM-L6-v2',257);
+insert into obj_key (obj_typ_id, obj_key_desc, obj_key_id) values (65, 'SapBERT',258);
+insert into obj_key (obj_typ_id, obj_key_desc, obj_key_id) values (65, 'LongName-sapBERT',259);
+insert into obj_key (obj_typ_id, obj_key_desc, obj_key_id) values (65, 'LongName-MiniLM-L6-v2',260);
+insert into obj_key (obj_typ_id, obj_key_desc, obj_key_id) values (65, 'Definition-MiniLM-L6-v2',262);
+
+insert into obj_key (obj_typ_id, obj_key_desc, obj_key_id) values (66, 'PV-MiniLM-L6-v2',263);
+insert into obj_key (obj_typ_id, obj_key_desc, obj_key_id) values (66, 'PV-Expanded-MiniLM-L6-v2',264);
+insert into obj_key (obj_typ_id, obj_key_desc, obj_key_id) values (66, 'PV-sapBERT',265);
+insert into obj_key (obj_typ_id, obj_key_desc, obj_key_id) values (66, 'PV-Expanded-sapBERT',266);
+--insert into obj_key (obj_typ_id, obj_key_desc, obj_key_id) values (66, '(None: CDE_Only)',267);
+commit;
+
+alter table nci_ds_prmtr add VARIANT_1_NM varchar2(255);
+alter table nci_ds_prmtr add VARIANT_2_NM varchar2(255);
+
+
