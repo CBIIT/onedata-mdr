a9c7ba18f0bbe70d33d70b44a36ee0388548d9e8
biggersbb <87334463+biggersbb@users.noreply.github.com>
2026-04-17 10:31:48 -0400

    JIRA 9352

---
+++ od-db-sprints/sprint73/S73_RA_WA_DDL2.sql
@@ -0,0 +1,3 @@
+Insert into OBJ_KEY (OBJ_KEY_ID, OBJ_KEY_DESC, OBJ_TYP_ID, OBJ_KEY_DEF) values (267,'REDCap DD CDE Zipfile',31,'REDCap DD CDE Zipfile');
+Insert into OBJ_KEY (OBJ_KEY_ID, OBJ_KEY_DESC, OBJ_TYP_ID, OBJ_KEY_DEF) values (268,'REDCap DD Form ZipFile',31,'REDCap DD Form Zipfile');
+commit;
