1f4dd8dbdc6a48bdf4296d3c4e3ab4363db7981d
matt-nash-nih <85249953+matt-nash-nih@users.noreply.github.com>
2026-04-21 13:49:13 -0400

    Combine WA RA DDL scripts into one

--- od-db-sprints/sprint73/S73_WA_RA_DDL.sql
+++ od-db-sprints/sprint73/S73_WA_RA_DDL.sql
@@ -252,6 +252,10 @@ set define off;
 	"VARIANT_2_NM" VARCHAR2(255 BYTE) COLLATE "USING_NLS_COMP"
    )  ;
 
+Insert into OBJ_KEY (OBJ_KEY_ID, OBJ_KEY_DESC, OBJ_TYP_ID, OBJ_KEY_DEF) values (267,'REDCap DD CDE Zipfile',31,'REDCap DD CDE Zipfile');
+Insert into OBJ_KEY (OBJ_KEY_ID, OBJ_KEY_DESC, OBJ_TYP_ID, OBJ_KEY_DEF) values (268,'REDCap DD Form ZipFile',31,'REDCap DD Form Zipfile');
+commit;
+
 
 
 
