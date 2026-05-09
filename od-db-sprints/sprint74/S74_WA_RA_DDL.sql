dc83ab4736d8fa426acbcd465c828070ab7c3467
deepalimanathara <deepali.manathara@gmail.com>
2026-05-08 22:19:45 -0400

    Update S74_WA_RA_DDL.sql

--- od-db-sprints/sprint74/S74_WA_RA_DDL.sql
+++ od-db-sprints/sprint74/S74_WA_RA_DDL.sql
@@ -2,6 +2,6 @@ alter table CNCPT add (PRMRY_RT_ORI_NM varchar2(255));
 
 alter table cncpt disable all triggers;
 update CNCPT set PRMRY_RT_ORI_NM = (select ITEM_NM from ADMIN_ITEM ai where ai.item_id = cncpt.item_id and ai.ver_nr = cncpt.ver_nr)
-where cncpt.prmry_Rt_ind = 1;
+where cncpt.PRMRY_CNCPT_IND = 1;
 commit;
 alter table cncpt enable all triggers;
