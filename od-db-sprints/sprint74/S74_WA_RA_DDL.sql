ed3c9616c46e1c9aea1accc486e30344ec37d10a
deepalimanathara <deepali.manathara@gmail.com>
2026-05-08 22:24:07 -0400

    Update S74_WA_RA_DDL.sql

--- od-db-sprints/sprint74/S74_WA_RA_DDL.sql
+++ od-db-sprints/sprint74/S74_WA_RA_DDL.sql
@@ -4,4 +4,7 @@ alter table cncpt disable all triggers;
 update CNCPT set PRMRY_RT_ORI_NM = (select ITEM_NM from ADMIN_ITEM ai where ai.item_id = cncpt.item_id and ai.ver_nr = cncpt.ver_nr)
 where cncpt.PRMRY_CNCPT_IND = 1;
 commit;
+update cncpt set prmry_rt_ori_nm = 'Stage' where PRMRY_CNCPT_IND = 1 and prmry_rt_ori_nm like '% Stage%';
+commit;
+
 alter table cncpt enable all triggers;
