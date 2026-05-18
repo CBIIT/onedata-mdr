363a67c50d8831859665254410961f146685c507
deepalimanathara <deepali.manathara@gmail.com>
2026-05-18 13:26:41 -0400

    Update S74_WA_RA_DDL.sql

--- od-db-sprints/sprint74/S74_WA_RA_DDL.sql
+++ od-db-sprints/sprint74/S74_WA_RA_DDL.sql
@@ -102,3 +102,67 @@ and value_dom.ver_nr = term.ver_nr (+)
 and admin_item.item_id = e.item_id and admin_item.ver_nr = e.ver_nr;
 
 
+
+  CREATE OR REPLACE  VIEW VW_MDL_FLAT_VAL_MAP AS
+  select ai.item_id MDL_ITEM_ID, 
+	ai.ver_nr  MDL_VER_NR, 
+	ai.item_nm  MDL_NM, 
+        ai.cntxt_nm_dn  MDL_CNTXT_NM, 
+	ai.admin_stus_nm_dn  MDL_ADMIN_STUS_NM, 
+	ai.regstr_stus_nm_dn  MDL_REGSTR_STUS_NM,
+	ai.currnt_ver_ind MDL_CURRNT_VER_IND, 
+	me.item_long_nm  ME_LONG_NM, 
+	me.item_phy_obj_nm ME_PHY_NM ,
+	me.item_desc ME_DESC,
+	me.item_id ME_ITEM_ID, 
+	me.ver_nr ME_VER_NR, 
+	mec.creat_dt, 
+	mec.creat_usr_id, 
+	mec.lst_upd_dt, 
+	mec.lst_upd_usr_id, 
+	mec.s2p_trn_dt, 
+	mec.fld_delete, 
+	mec.lst_del_dt,
+--	mec.src_dttype MEC_DTTYP, 
+--	mec.CHAR_ORD MEC_CHAR_ORD, 
+--	mec.src_max_char MEC_MAX_CHAR, 
+--	mec.src_min_char MEC_MIN_CHAR, 
+--	mec.src_uom MEC_UOM, 
+--	mec.src_deflt_val MEC_DEFLT_VAL, 
+--	  mec.cmnts_desc_txt,
+--	mec.de_conc_item_id, 
+--	mec.de_conc_ver_nr,
+--	mec.val_dom_item_id,
+--	mec.val_dom_ver_nr, 
+	mec.mec_long_nm , 
+	mec.mec_phy_nm , 
+	mec.mec_desc , 
+	mec.cde_item_id , 
+	  mec.val_dom_item_id, 
+	  mec.val_dom_ver_nr,
+   	mec.cde_ver_nr ,
+	cde.Item_nm CDE_NM,
+	cde.cntxt_nm_dn CDE_CNTXT_NM,
+	  mec.mec_id,
+	  mec.req_ind,
+	  mec.pk_ind,
+	  mec.fk_ind,
+	  mec.mdl_pk_ind,
+	  mec.FK_ELMNT_PHY_NM,
+	  mec.FK_ELMNT_CHAR_PHY_NM,
+	  PV.PERM_VAL_BEG_DT, PERM_VAL_END_DT, PERM_VAL_NM, PERM_VAL_DESC_TXT,
+             -- PV.VAL_DOM_ITEM_ID,
+		--PV.VAL_DOM_VER_NR , 
+        VM.ITEM_NM VM_ITEM_NM, VM.ITEM_LONG_NM VM_ITEM_LONG_NM,
+                VM.ITEM_DESC VM_ITEM_DESC, NCI_VAL_MEAN_ITEM_ID, NCI_VAL_MEAN_VER_NR, vm.admin_stus_nm_dn VM_ADMIN_STUS_NM_DN
+from admin_item ai, nci_mdl mdl, nci_mdl_elmnt me, nci_mdl_elmnt_char mec, admin_item cde, PERM_VAL PV, ADMIN_ITEM VM	 
+where ai.item_id = mdl.item_id and ai.ver_nr = mdl.ver_nr and ai.admin_item_typ_id = 57 and mdl.item_id = me.mdl_item_id
+and mdl.ver_nr = me.mdl_item_ver_nr and me.item_id = mec.mdl_elmnt_item_id and me.ver_nr = mec.mdl_elmnt_ver_nr
+	and mec.cde_item_id = cde.item_id (+)
+	and mec.cde_ver_nr = cde.ver_nr (+)
+	and cde.admin_item_typ_id (+)= 4
+	  and PV.NCI_VAL_MEAN_ITEM_ID = vm.ITEM_ID and
+	PV.NCI_VAL_MEAN_VER_NR = vm.VER_NR and
+	  pv.val_dom_item_id = mec.val_dom_item_id and 
+	  pv.val_dom_Ver_nr = mec.val_dom_ver_nr
+	  
