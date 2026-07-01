7854a6b5eccf06e34a7e0e2544886c99e571459a
deepalimanathara <deepali.manathara@gmail.com>
2026-07-01 16:56:22 -0400

    Update S74_EP_WA_RA_DDL.sql

--- od-db-sprints/sprint74/S74_EP_WA_RA_DDL.sql
+++ od-db-sprints/sprint74/S74_EP_WA_RA_DDL.sql
@@ -1,24 +1,15 @@
 
   CREATE or replace view VW_CNCPT_ALL_2 
-  AS SELECT ADMIN_ITEM.ITEM_ID, ADMIN_ITEM.VER_NR, ADMIN_ITEM.ITEM_NM,  ADMIN_ITEM.ITEM_LONG_NM, '.' ||ADMIN_ITEM.ITEM_NM ||'.' ITEM_NM_PERIOD,  '.' ||ADMIN_ITEM.ITEM_LONG_NM || '.' ITEM_LONG_NM_PERIOD, ADMIN_ITEM.ITEM_DESC, ADMIN_ITEM.CREATION_DT, 
-ADMIN_ITEM.EFF_DT, ADMIN_ITEM.ORIGIN,  ADMIN_ITEM.UNTL_DT,  ADMIN_ITEM.CURRNT_VER_IND, ADMIN_ITEM.REGSTR_STUS_NM_DN, ADMIN_ITEM.ADMIN_STUS_NM_DN, ADMIN_ITEM.CNTXT_ITEM_ID,
-ADMIN_ITEM.CNTXT_VER_NR, ADMIN_ITEM.CNTXT_NM_DN,
- ADMIN_ITEM.CREAT_DT, ADMIN_ITEM.CREAT_USR_ID, ADMIN_ITEM.LST_UPD_USR_ID, ADMIN_ITEM.FLD_DELETE, ADMIN_ITEM.LST_DEL_DT, ADMIN_ITEM.S2P_TRN_DT, ADMIN_ITEM.LST_UPD_DT, CNCPT.PRMRY_CNCPT_IND,
-nvl(decode(trim(ADMIN_ITEM.DEF_SRC), 'NCI', '1-NCI', ADMIN_ITEM.DEF_SRC), 'No Def Source') DEF_SRC, e.CNCPT_CONCAT_WITH_INT,
+  AS SELECT ADMIN_ITEM.ITEM_ID, ADMIN_ITEM.VER_NR, ADMIN_ITEM.ITEM_NM,  item_long_nm, ADMIN_ITEM.ITEM_DESC, ADMIN_ITEM.CREATION_DT, 
+ CNCPT.PRMRY_CNCPT_IND,
+nvl(decode(trim(ADMIN_ITEM.DEF_SRC), 'NCI', '1-NCI', ADMIN_ITEM.DEF_SRC), 'No Def Source') DEF_SRC,
 		      decode(trim(OBJ_KEY.OBJ_KEY_DESC), 'NCI_CONCEPT_CODE', '1-NCC', 'NCI_META_CUI', '2-NMC', OBJ_KEY.OBJ_KEY_DESC) EVS_SRC,
 		      trim(OBJ_KEY.OBJ_KEY_DESC) EVS_SRC_ORI,
 			cncpt.evs_Src_id,nvl(cncpt.ref_term_ind,0) REF_TERM_IND,
 		      nvl(decode(trim(ADMIN_ITEM.DEF_SRC), 'NCI', '1-NCI', ADMIN_ITEM.DEF_SRC), 'No Def Source') || '|' ||
 		      nvl(decode(trim(OBJ_KEY.OBJ_KEY_DESC), 'NCI_CONCEPT_CODE', '1-NCC', 'NCI_META_CUI', '2-NMC', OBJ_KEY.OBJ_KEY_DESC), 'No EVS Source') DEF_EVS_SRC,
     nvl(PRMRY_OBJ_CLS_IND,0) PRMRY_OBJ_CLS_IND,  decode(PRMRY_OBJ_CLS_IND,1,'Yes','') PRMRY_OBJ_CLS_IND_NM ,
-    b.sys_path_final,
-	  b.lvl,
-    nvl(cncpt.PRMRY_RT_ORI_NM, admin_item.item_nm) CNCPT_ORI_NM,
- 'TEST' NCI_META_CUI
-  --  xm.xmap_cd NCI_META_CUI
-              FROM ADMIN_ITEM,  CNCPT, nci_admin_item_ext e, OBJ_KEY, 
-                vw_prmry_oc_cncpt_rel b
+    nvl(cncpt.PRMRY_RT_ORI_NM, admin_item.item_nm) CNCPT_ORI_NM
+              FROM ADMIN_ITEM,  CNCPT, OBJ_KEY
        WHERE ADMIN_ITEM_TYP_ID = 49 and ADMIN_ITEM.ITEM_ID = CNCPT.ITEM_ID and ADMIN_ITEM.VER_NR = CNCPT.VER_NR
-       and admin_item.item_id = e.item_id and admin_item.ver_nr = e.ver_nr
-	and cncpt.EVS_SRC_ID = OBJ_KEY.OBJ_KEY_ID (+) --and admin_item.admin_stus_nm_dn = 'RELEASED' 
-	and admin_item.item_id = b.c_item_id  (+) and admin_item.ver_nr = b.c_item_ver_nr (+);
+   	and cncpt.EVS_SRC_ID = OBJ_KEY.OBJ_KEY_ID (+) --and admin_item.admin_stus_nm_dn = 'RELEASED' 
