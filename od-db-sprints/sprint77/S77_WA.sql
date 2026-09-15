2b17a285a7c4eb47051a25ad7581895cecaa2112
deepalimanathara <deepali.manathara@gmail.com>
2026-09-15 13:02:32 -0400

    Update S77_WA.sql

--- od-db-sprints/sprint77/S77_WA.sql
+++ od-db-sprints/sprint77/S77_WA.sql
@@ -27,3 +27,47 @@ CREATE INDEX "ONEDATA_WA"."IDX_ALTNMS_MTCH_TERM_ADV" ON "ONEDATA_WA"."ALT_NMS" (
   TABLESPACE "USERS" ;
 
 commit work;
+
+
+create index idx_nci_mec_val_map_mm
+on nci_mec_val_map (mdl_map_item_id, mdl_map_ver_nr, fld_delete);
+
+
+analyze table nci_mec_val_map compute statistics;
+
+
+create index idx_nci_mec_map_mm
+on nci_mec_map (mdl_map_item_id, mdl_map_ver_nr, fld_delete);
+
+
+analyze table nci_mec_map compute statistics;
+
+
+create index idx_admin_item_fld
+on admin_Item (item_id, ver_nr, fld_delete);
+
+
+analyze table admin_item compute statistics;
+
+
+create or replace TRIGGER TR_REF_BEFORE
+  BEFORE INSERT OR UPDATE
+  on REF
+  for each row
+BEGIN
+
+
+:new.MTCH_TERM := regexp_replace(upper(:new.ref_desc),'[^ A-Za-z0-9]','');
+:new.MTCH_TERM_ADV := regexp_replace(upper(:new.ref_desc),'[^A-Za-z0-9]','');  
+END;
+/
+
+alter table ref disable all triggers;
+
+update ref set mtch_term = regexp_replace(upper(ref_desc),'[^ A-Za-z0-9]',''),
+mtch_term_adv =  regexp_replace(upper(ref_desc),'[^A-Za-z0-9]','');
+commit;
+
+alter table ref enable all triggers;
+
+
