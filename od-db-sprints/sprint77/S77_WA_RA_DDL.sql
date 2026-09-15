98d87b6a47302c22a4b2e52f8866e237c944a609
matt-nash-nih <85249953+matt-nash-nih@users.noreply.github.com>
2026-09-15 14:43:38 -0400

    Modify tables and update records in DDL script
    Added new columns and updated existing records in nci_ds_hdr and obj_key tables. Disabled and re-enabled triggers during updates.

--- od-db-sprints/sprint77/S77_WA_RA_DDL.sql
+++ od-db-sprints/sprint77/S77_WA_RA_DDL.sql
@@ -9,5 +9,19 @@ FROM VALUE_DOM;
 alter table ref add mtch_scr number(10,5);
 
 alter table alt_nms add mtch_scr number(10,5);
+update obj_key set obj_key_desc = 'CDEMeta-SapBERT' where obj_key_id = 253;
+alter table nci_ds_prmtr_temp add MS_CNTXT varchar2(1000);
+alter table nci_ds_hdr add VARIANT_1 number default 253;
+
+alter table nci_ds_hdr disable all triggers;
+update nci_ds_hdr set VARIANT_1 = 253;
+commit;
+alter table nci_ds_hdr enable all triggers;
+
+alter table nci_ds_hdr add CDE_MDL_VARIANTS varchar2(100) default 'CDEMeta-SapBERT';
+alter table nci_ds_hdr disable all triggers;
+update nci_ds_hdr set CDE_MDL_VARIANTS = 'CDEMeta-SapBERT';
+commit;
+alter table nci_ds_hdr enable all triggers;
 
      
