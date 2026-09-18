da5088693bef0c5f426a92bf7eaf99380a6e7f38
matt-nash-nih <85249953+matt-nash-nih@users.noreply.github.com>
2026-09-18 13:08:56 -0400

    Update default values and thresholds in database tables
    Modified MTCH_SCR default values and updated thresholds in nci_ds_hdr and nci_ds_prmtr tables. Disabled and enabled triggers during updates.

--- od-db-sprints/sprint77/S77_1_WA_RA_DDL.sql
+++ od-db-sprints/sprint77/S77_1_WA_RA_DDL.sql
@@ -1,2 +1,24 @@
 ALTER TABLE ALT_NMS MODIFY MTCH_SCR DEFAULT 1;
 ALTER TABLE REF MODIFY MTCH_SCR DEFAULT 1;
+
+alter table nci_ds_hdr disable all triggers;
+update nci_ds_hdr set threshold_1 = null, threshold_2 = null;
+alter table nci_ds_hdr modify THRESHOLD_1 number(4,2) default .5;
+alter table nci_ds_hdr modify THRESHOLD_2 number(4,2) default .5;
+update nci_ds_hdr set threshold_1 = .5, threshold_2 = .5;
+commit;
+alter table nci_ds_hdr enable all triggers;
+
+alter table nci_ds_prmtr disable all triggers;
+alter table nci_ds_prmtr_temp disable all triggers;
+update nci_ds_prmtr_temp set threshold_1 = null, threshold_2 =null;
+commit;
+alter table nci_ds_prmtr_temp modify threshold_1 number(4,2);
+alter table nci_ds_prmtr_temp modify threshold_2 number(4,2);
+update nci_ds_prmtr set threshold_1 = null, threshold_2 =null;
+commit;
+alter table nci_ds_prmtr modify threshold_1 number(4,2);
+alter table nci_ds_prmtr modify threshold_2 number(4,2);
+
+alter table nci_ds_prmtr enable all triggers;
+alter table nci_ds_prmtr_temp enable all triggers;
