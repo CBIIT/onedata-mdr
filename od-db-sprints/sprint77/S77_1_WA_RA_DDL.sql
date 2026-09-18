db23850646a5c53930fb2bf28111cd9473b261ca
matt-nash-nih <85249953+matt-nash-nih@users.noreply.github.com>
2026-09-18 16:43:14 -0400

    Update nci_ds_hdr thresholds and disable triggers
    Modified thresholds and committed changes in nci_ds_hdr table.

--- od-db-sprints/sprint77/S77_1_WA_RA_DDL.sql
+++ od-db-sprints/sprint77/S77_1_WA_RA_DDL.sql
@@ -3,6 +3,7 @@ ALTER TABLE REF MODIFY MTCH_SCR DEFAULT 1;
 
 alter table nci_ds_hdr disable all triggers;
 update nci_ds_hdr set threshold_1 = null, threshold_2 = null;
+commit;
 alter table nci_ds_hdr modify THRESHOLD_1 number(4,2) default .5;
 alter table nci_ds_hdr modify THRESHOLD_2 number(4,2) default .5;
 update nci_ds_hdr set threshold_1 = .5, threshold_2 = .5;
