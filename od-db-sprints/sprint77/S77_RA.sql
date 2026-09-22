4fa6a0f0ff5ed13e56e09c30a30bf87cc0a8a8c4
matt-nash-nih <85249953+matt-nash-nih@users.noreply.github.com>
2026-09-22 10:52:07 -0400

    Modify nci_ds_rslt table

--- od-db-sprints/sprint77/S77_RA.sql
+++ od-db-sprints/sprint77/S77_RA.sql
@@ -1,1 +1,4 @@
 grant all on nci_ds_btch_hdr to onedata_wa;
+alter table nci_ds_rslt disable all triggers;
+alter table nci_ds_rslt modify score number(38,4);
+alter table nci_ds_rslt enable all triggers;
